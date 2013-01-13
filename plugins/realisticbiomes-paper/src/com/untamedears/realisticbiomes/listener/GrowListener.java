962ff9ef5be063ac7a378f95917d4b4b7eaa2983
Travis Christian <travis.w.christian@gmail.com>
2013-01-13 10:57:16 -0800

    populated initial rules

--- plugins/realisticbiomes-paper/src/com/untamedears/realisticbiomes/listener/GrowListener.java
+++ plugins/realisticbiomes-paper/src/com/untamedears/realisticbiomes/listener/GrowListener.java
@@ -21,9 +21,16 @@ public class GrowListener implements Listener {
 	private static Map<Material, Set<Biome>> allowedGrowth = new HashMap<Material, Set<Biome>>();
 
 	static {
-		allowedGrowth.put(Material.CROPS, new HashSet<Biome>( Arrays.asList(new Biome[]{ Biome.PLAINS }) ));
-		allowedGrowth.put(Material.MELON_STEM, new HashSet<Biome>( Arrays.asList(new Biome[]{ Biome.PLAINS }) ));
-		allowedGrowth.put(Material.PUMPKIN_STEM, new HashSet<Biome>( Arrays.asList(new Biome[]{ Biome.PLAINS }) ));
+		allowedGrowth.put(Material.CROPS, new HashSet<Biome>( Arrays.asList(new Biome[]{ Biome.PLAINS, Biome.FOREST, Biome.FOREST_HILLS, Biome.MUSHROOM_ISLAND, Biome.RIVER, Biome.SKY, Biome.SMALL_MOUNTAINS, Biome.SWAMPLAND, Biome.TAIGA, Biome.TAIGA_HILLS, Biome.EXTREME_HILLS }) ));
+		allowedGrowth.put(Material.MELON_STEM, new HashSet<Biome>( Arrays.asList(new Biome[]{ Biome.PLAINS, Biome.FOREST, Biome.FOREST_HILLS, Biome.MUSHROOM_ISLAND, Biome.RIVER, Biome.SMALL_MOUNTAINS, Biome.SWAMPLAND, Biome.JUNGLE, Biome.JUNGLE_HILLS }) ));
+		allowedGrowth.put(Material.PUMPKIN_STEM, new HashSet<Biome>( Arrays.asList(new Biome[]{ Biome.PLAINS, Biome.FOREST, Biome.FOREST_HILLS, Biome.MUSHROOM_ISLAND, Biome.RIVER, Biome.SKY, Biome.SMALL_MOUNTAINS, Biome.SWAMPLAND }) ));
+		allowedGrowth.put(Material.CARROT, new HashSet<Biome>( Arrays.asList(new Biome[]{ Biome.PLAINS, Biome.FOREST, Biome.FOREST_HILLS, Biome.MUSHROOM_ISLAND, Biome.RIVER }) ));
+		allowedGrowth.put(Material.POTATO, new HashSet<Biome>( Arrays.asList(new Biome[]{ Biome.PLAINS, Biome.FOREST, Biome.FOREST_HILLS, Biome.MUSHROOM_ISLAND, Biome.RIVER }) ));
+		allowedGrowth.put(Material.COCOA, new HashSet<Biome>( Arrays.asList(new Biome[]{ Biome.JUNGLE, Biome.JUNGLE_HILLS, Biome.MUSHROOM_ISLAND }) ));
+		allowedGrowth.put(Material.CACTUS, new HashSet<Biome>( Arrays.asList(new Biome[]{ Biome.DESERT, Biome.DESERT_HILLS, Biome.MUSHROOM_ISLAND }) ));
+		allowedGrowth.put(Material.SUGAR_CANE_BLOCK, new HashSet<Biome>( Arrays.asList(new Biome[]{ Biome.DESERT, Biome.DESERT_HILLS, Biome.SWAMPLAND, Biome.JUNGLE, Biome.JUNGLE_HILLS, Biome.BEACH, Biome.MUSHROOM_SHORE, Biome.RIVER }) ));
+		allowedGrowth.put(Material.SAPLING, new HashSet<Biome>( Arrays.asList(new Biome[]{ Biome.FOREST, Biome.FOREST_HILLS, Biome.MUSHROOM_ISLAND, Biome.SMALL_MOUNTAINS, Biome.SWAMPLAND, Biome.TAIGA, Biome.TAIGA_HILLS, Biome.EXTREME_HILLS, Biome.JUNGLE, Biome.JUNGLE_HILLS }) ));
+		allowedGrowth.put(Material.NETHER_STALK, new HashSet<Biome>( Arrays.asList(new Biome[]{ Biome.HELL }) ));
 	}
 
 	@EventHandler(ignoreCancelled = true)
