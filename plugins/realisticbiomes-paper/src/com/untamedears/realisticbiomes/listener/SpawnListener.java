028d6abf849da2e1c0fbe1e0fa56337c2f8dd728
Travis Christian <travis.w.christian@gmail.com>
2013-01-26 19:07:11 -0800

    Added biome filter for catching fish. 90% reduction in chicken eggs.

--- plugins/realisticbiomes-paper/src/com/untamedears/realisticbiomes/listener/SpawnListener.java
+++ plugins/realisticbiomes-paper/src/com/untamedears/realisticbiomes/listener/SpawnListener.java
@@ -5,7 +5,9 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.Logger;
 
+import org.bukkit.Material;
 import org.bukkit.block.Biome;
 import org.bukkit.block.Block;
 import org.bukkit.entity.EntityType;
@@ -13,9 +15,21 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
+import org.bukkit.event.entity.ItemSpawnEvent;
+import org.bukkit.event.player.PlayerFishEvent;
 
+/**
+ * Event listeners for animal spawn related events. Whenever animals breed or a fish is caught, the species is checked against
+ * allowed biomes. If the biome is not found, the event is cancelled and nothing happens. Also, chicken egg spawns are cancelled
+ * randomly to reduce the average time between laying eggs.
+ * @author WildWeazel
+ *
+ */
 public class SpawnListener implements Listener {
 
+	/**
+	 *  Maps a {@link EntityType} to the {@link Biome}s in which it is permitted to spawn
+	 */
 	private static Map<EntityType, Set<Biome>> allowedGrowth = new HashMap<EntityType, Set<Biome>>();
 
 	static {
@@ -81,8 +95,21 @@ public class SpawnListener implements Listener {
 				Biome.EXTREME_HILLS
 			}))
 		);
+		allowedGrowth.put(	EntityType.FISHING_HOOK,
+				new HashSet<Biome>( Arrays.asList(new Biome[]{
+					Biome.RIVER,
+					Biome.OCEAN,
+					Biome.BEACH,
+					Biome.FROZEN_RIVER,
+					Biome.FROZEN_OCEAN
+				}))
+			);
 	}
 
+	/**
+	 *  Event handler for {@link CreatureSpawnEvent}. Checks animal breeding for proper biomes.
+	 * @param event The {@link CreatureSpawnEvent} being handled
+	 */
 	@EventHandler(ignoreCancelled = true)
 	public void spawnEntity(CreatureSpawnEvent event) {
 		if(event.getSpawnReason() == SpawnReason.BREEDING) {
@@ -92,9 +119,40 @@ public class SpawnListener implements Listener {
 		}
 	}
 
-	private boolean canSpawnHere(EntityType m, Block b) {
-		if(allowedGrowth.containsKey(m)) {
-			return allowedGrowth.get(m).contains(b.getBiome());
+	public static Logger LOG = Logger.getLogger("RealisticBiomes");
+	/**
+	 *  Event handler for {@link ItemSpawnEvent}. Reduces the chance of a chicken egg being spawned.
+	 * @param event The {@link ItemSpawnEvent} being handled
+	 */
+	@EventHandler(ignoreCancelled = true)
+	public void spawnItem(ItemSpawnEvent event) {
+		if(event.getEntity().getItemStack().getType() == Material.EGG) {
+			event.setCancelled(Math.random() > 0.1);
+		}
+	}
+
+	/**
+	 *  Event handler for {@link PlayerFishEvent}. Checks caught fish for proper biomes.
+	 * @param event The {@link PlayerFishEvent} being handled
+	 */
+	@EventHandler(ignoreCancelled = true)
+	public void fishing(PlayerFishEvent event) {
+		if( event.getState() == PlayerFishEvent.State.CAUGHT_FISH && event.getCaught() != null ) {
+			EntityType type = EntityType.FISHING_HOOK;
+			Block block = event.getCaught().getLocation().getBlock();
+			event.setCancelled(!canSpawnHere(type, block));
+		}
+	}
+
+	/**
+	 * Determines if a plant {@link EntityType} type can spawn in a {@link Biome}
+	 * @param t The entity type being spawned
+	 * @param b The biome in which the spawn occurs
+	 * @return Whether the entity is allowed to spawn in the biome
+	 */
+	private boolean canSpawnHere(EntityType t, Block b) {
+		if(allowedGrowth.containsKey(t)) {
+			return allowedGrowth.get(t).contains(b.getBiome());
 		}
 		return true;
 	}
