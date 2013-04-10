018dcd14684d1d064fb08167a758846aecfb6bba
rourke rabinowitz <rourke7501@gmail.com>
2013-04-10 11:46:50 -0400

    started block listener

---
+++ plugins/jukealert-paper/src/com/untamedears/JukeAlert/JukeAlertListening.java
@@ -0,0 +1,14 @@
+package com.untamedears.JukeAlert;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+public class JukeAlertListening {
+	  public void placeSnitchBlock(BlockPlaceEvent bpe) {
+		 Block block= bpe.getBlock();
+		 if (block.getType()== Material.JUKEBOX){
+			 
+		 }
+	  }
+}
