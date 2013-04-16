bfd999815a69b6bc8c0d9bf0984d57e418d6f1a5
Rourke750 <Rourke7501@gmail.com>
2013-04-16 14:22:41 -0400

    it goofed

--- plugins/jukealert-paper/src/com/untamedears/JukeAlert/JukeAlertListener.java
+++ plugins/jukealert-paper/src/com/untamedears/JukeAlert/JukeAlertListener.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.untamedears.JukeAlert;
 
 import com.untamedears.JukeAlert.sql.JukeAlertLogger;
@@ -21,82 +21,7 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
-public class JukeAlertListener implements Listener {
 
-	private JukeAlert ja;
-	private JukeAlertLogger jal;
-	public JukeAlertListener(JukeAlert ja) {
-		this.ja = ja;
-	}
-
-	@EventHandler(priority = EventPriority.NORMAL)
-	public void placeSnitchBlock(BlockPlaceEvent event) {
-		if (event.isCancelled()) {
-			return;
-		}
-		Block block = event.getBlock();
-		if (!block.getType().equals(Material.JUKEBOX)) {
-			return;
-		}
-		Player player = event.getPlayer();
-		Location loc = player.getLocation();
-		if (Utility.isReinforced(loc)) {
-			AccessDelegate access = AccessDelegate.getDelegate(block);
-			IReinforcement rei = access.getReinforcement();
-			if (rei instanceof PlayerReinforcement) {
-				PlayerReinforcement reinforcement = (PlayerReinforcement) rei;
-				Faction owner = reinforcement.getOwner();
-				if (reinforcement.getSecurityLevel().equals(SecurityLevel.GROUP)) {
-					ja.getJaLogger().logSnitchPlace(player.getWorld().getName(), owner.getName(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
-					player.sendMessage(ChatColor.AQUA + "You've created a snitch block registered to the group " + owner.getName() + ".");
-				} else {
-					ja.getJaLogger().logSnitchPlace(player.getWorld().getName(), "p:" + owner.getFounder(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
-					player.sendMessage(ChatColor.AQUA + "You've created a private snitch block; Reinforce it with a group to register others.");
-				}
-			}
-
-			ja.getJaLogger().logSnitchPlace(player.getWorld().getName(), "p:" + player.getName(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
-			player.sendMessage(ChatColor.AQUA + "You've created a private snitch; reinforce it to add others to it.");
-			return;
-		}
-		player.sendMessage(ChatColor.YELLOW + "You've placed a jukebox reinforce it to register it as a snitch.");
-	}
-
-	@EventHandler(priority = EventPriority.NORMAL)
-	public void breakSnitchBlock(BlockBreakEvent event) {
-		if (event.isCancelled()) {
-			return;
-		}
-		Block block = event.getBlock();
-		if (!block.getType().equals(Material.JUKEBOX)) {
-			return;
-		}
-		Player player = event.getPlayer();
-		Location loc = player.getLocation();
-		ja.getJaLogger().logSnitchBreak(player.getWorld().getName(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
-		//TODO: Make sure this is 100% complete. Also make it remove from the List in JukeAlert.java
-	}
-	
-	@EventHandler(priority = EventPriority.HIGH)
-	public void enterSnitchProximity(PlayerMoveEvent event) {
-		Location loc= event.getPlayer().getLocation();
-		double x=0,y=0,z=0;
-		jal.updateCubiodSize(x, y, z);//gets snitch field.
-		if (loc.getX() == x || loc.getY() == y || loc.getZ() == z){ //if snitch field and player have the same cords.
-			// todo: send player message.
-		}
-		
-		
-		//TODO: Add/remove players to/from the JukeAlertSnitch's list and notify the player's who own the snitch if they have entered.
-	}
-	 
-	//Registers the events in this to JukeAlert.java
-	public void registerEvents() {
-		Bukkit.getServer().getPluginManager().registerEvents(this, ja);
-	}
-}
-=======
-package com.untamedears.JukeAlert;
 
 import com.untamedears.citadel.SecurityLevel;
 import com.untamedears.citadel.Utility;
@@ -192,4 +117,4 @@ public class JukeAlertListener implements Listener {
 		Bukkit.getServer().getPluginManager().registerEvents(this, ja);
 	}
 }
->>>>>>> 283796ff88b49865ceefb333e7872259208f978f
+
--- plugins/jukealert-paper/src/com/untamedears/JukeAlert/sql/JukeAlertLogger.java
+++ plugins/jukealert-paper/src/com/untamedears/JukeAlert/sql/JukeAlertLogger.java
@@ -8,10 +8,7 @@ import com.untamedears.JukeAlert.JukeAlert;
 
 import java.awt.Event;
 import java.util.logging.Level;
-<<<<<<< HEAD
 
-=======
->>>>>>> 283796ff88b49865ceefb333e7872259208f978f
 import org.bukkit.Location;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.entity.Player;
@@ -25,7 +22,7 @@ import org.bukkit.event.player.PlayerMoveEvent;
  */
 public class JukeAlertLogger {
 
-<<<<<<< HEAD
+
 	private JukeAlert plugin;
 	private Database db;
 
@@ -161,114 +158,5 @@ public class JukeAlertLogger {
 		}
 		//Calculates whether or not a player enters a snitch field: unfinished.
 	}
-=======
-    private JukeAlert plugin;
-    private Database db;
 
-    public JukeAlertLogger(JukeAlert plugin) {
-        this.plugin = plugin;
-
-        Configuration c = plugin.getConfig();
-
-        String host = c.getString("db.host");
-        String dbname = c.getString("db.name");
-        String user = c.getString("db.user");
-        String pass = c.getString("db.pass");
-        String prefix = c.getString("db.prefix");
-
-        if (host == null) {
-            host = "localhost";
-            c.set("db.host", host);
-        }
-
-        if (dbname == null) {
-            dbname = "mydb";
-            c.set("db.name", dbname);
-        }
-
-        if (user == null) {
-            user = "root";
-            c.set("db.user", user);
-        }
-
-        if (pass == null) {
-            pass = "admin";
-            c.set("db.pass", pass);
-        }
-
-        if (prefix == null) {
-            prefix = "pvp_";
-            c.set("db.prefix", prefix);
-        }
-
-        plugin.saveConfig();
-
-        db = new Database(host, dbname, user, pass, prefix);
-        boolean connected = db.connect();
-        if (connected) {
-            genTables();
-        } else {
-            JukeAlert.log(Level.SEVERE, "Could not connect to the database! Fill out your config.yml!");
-        }
-    }
-
-    public Database getDb() {
-        return db;
-    }
-
-    /**
-     * Table generator
-     */
-    private void genTables() {
-        db.execute("CREATE TABLE IF NOT EXISTS `" + db.getPrefix() + "snitches` ("
-                + "`snitch_id` int(10) unsigned NOT NULL AUTO_INCREMENT,"
-                + "`snitch_world` varchar(40) NOT NULL,"
-                + "`snitch_x` int(10) NOT NULL,"
-                + "`snitch_y` int(10) NOT NULL,"
-                + "`snitch_z` int(10) NOT NULL,"
-                + "`snitch_group` varchar(40) NOT NULL,"
-                + "`snitch_cuboid_x` int(10) NOT NULL,"
-                + "`snitch_cuboid_y` int(10) NOT NULL,"
-                + "`snitch_cuboid_z` int(10) NOT NULL,"
-                + "PRIMARY KEY (`snitch_id`));");
-    }
-    
-    //Logs the snitch being placed at World, x, y, z in the database.
-    public void logSnitchPlace(String world, String group, double x, double y, double z) {
-        db.execute("INSERT INTO " + db.getPrefix() + "snitches "
-                + "(snitch_world, snitch_x, snitch_y, snitch_z, snitch_group, snitch_cuboid_x, snitch_cuboid_y, snitch_cuboid_z) "
-                + "VALUES("
-                + "'" + world + "',"
-                + "'" + x + "',"
-                + "'" + y + "',"
-                + "'" + z + "',"
-                + "'" + group + "',"
-                + "'" + 11 + "',"
-                + "'" + 11 + "',"
-                + "'" + 11 + "'"
-                + ")");
-    }
-    //Removes the snitch at the location of World, X, Y, Z from the database.
-    public void logSnitchBreak(String world, double x, double y, double z) {
-        db.execute("DELETE FROM " + db.getPrefix() + "snitches WHERE snitch_world='" + world + "' AND snitch_x='" + x + "' AND snitch_y='" + y + "' AND snitch_z='" + z + "'");
-    }
-    
-    //Changes the group of which the snitch is registered to at the location of loc in the database.
-    public void updateGroupSnitch(Location loc, String group) {
-        int lX = loc.getBlockX();
-        int lY = loc.getBlockY();
-        int lZ = loc.getBlockZ();
-        db.execute("UPDATE " + db.getPrefix() + "snitches SET snitch_group='" + group + "' WHERE snitch_world='"
-                + loc.getWorld().getName() + "' AND snitch_x='" + lX + "' AND snitch_y='" + lY + "' AND snitch_z='" + lZ + "'");
-    }
-    
-    //Updates the cuboid size of the snitch in the database.
-    public void updateCubiodSize(Location loc, int x, int y, int z) {
-        int lX = loc.getBlockX();
-        int lY = loc.getBlockY();
-        int lZ = loc.getBlockZ();
-        db.execute("UPDATE " + db.getPrefix() + "snitches SET snitch_cuboid_x='" + x + "', snitch_cuboid_y='" + y
-                + "', snitch_cuboid_z='" + z + "' WHERE snitch_world='" + loc.getWorld().getName() + "' AND snitch_x='" + lX + "' AND snitch_y='" + lY + "' AND snitch_z='" + lZ + "'");
-    }
->>>>>>> 283796ff88b49865ceefb333e7872259208f978f
 }
