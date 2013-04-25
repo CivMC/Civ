4829f108a4a1941ba0d812c2008c9ea9847ae6b8
Mark Grandi <markgrandi@gmail.com>
2013-04-24 18:53:59 -0700

    more sql work + finished Logger methods working with new schema
    finished updating the sql schema and rewriting the log* methods
    (logBlockBreak, etc) to pass the correct stuff to logSnitchInfo(). Also
    changed the parameter of logSnitchInfo() to be a Material rather then a
    Block since Material essentially represents 'an item' conceptually
    (including blocks), but there are a couple of places where im not sure
    if we need to use a ItemStack, Block or Material (most notably logUsed()
    and logSnichBucketFill() and logSnitchBucketEmpty() )

--- plugins/jukealert-paper/src/com/untamedears/JukeAlert/storage/JukeAlertLogger.java
+++ plugins/jukealert-paper/src/com/untamedears/JukeAlert/storage/JukeAlertLogger.java
@@ -23,6 +23,7 @@ import java.util.Map;
 import java.util.logging.Level;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.configuration.Configuration;
@@ -108,12 +109,12 @@ public class JukeAlertLogger {
         		+ "`snitch_id` int(10) unsigned NOT NULL," // reference to the column in the main snitches table
                 + "`snitch_log_time` datetime,"
                 + "`snitch_logged_action` tinyint unsigned NOT NULL,"
-                + "`snitch_logged_initiated_user`  char(16) NOT NULL,"
-                + "`snitch_logged_victim_user` char(16), "
+                + "`snitch_logged_initiated_user` varchar(16) NOT NULL,"
+                + "`snitch_logged_victim_user` varchar(16), "
                 + "`snitch_logged_x` int(10), "
                 + "`snitch_logged_Y` int(10), "
                 + "`snitch_logged_z` int(10), "
-                + "`snitch_logged_blockid` smallint unsigned,"
+                + "`snitch_logged_materialid` smallint unsigned," // can be either a block, item, etc
                 + "PRIMARY KEY (`snitch_details_id`));");
     }
 
@@ -137,7 +138,7 @@ public class JukeAlertLogger {
         // statement to insert a log entry into the snitchesDetailsTable
         insertSnitchLogStmt = db.prepareStatement(String.format(
             "INSERT INTO %s (snitch_id, snitch_log_time, snitch_logged_action, snitch_logged_initated_user," +
-            " snitch_logged_victim_user, snitch_logged_x, snitch_logged_y, snitch_logged_z, snitch_logged_blockid) " +
+            " snitch_logged_victim_user, snitch_logged_x, snitch_logged_y, snitch_logged_z, snitch_logged_materialid) " +
             " VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)",
             snitchDetailsTbl));
         
@@ -273,112 +274,152 @@ public class JukeAlertLogger {
      * ----Type /ja more to see more----
      * 
      * @param snitch - the snitch that recorded this event, required
-     * @param block - the block that was part of the event, if there was one , null if no block was part of the event
+     * @param material - the block/item/whatever that was part of the event, if there was one , null if no material was part of the event
+     * @param loc - the location where this event occured, if any
      * @param date - the date this event occurred , required
      * @param action - the action that took place in this event
      * @param initiatedUser - the user who initiated the event, required
      * @param victimUser - the user who was victim of the event, can be null
      */
-    public void logSnitchInfo(Snitch snitch, Block block, Date date, LoggedAction action, String initiatedUser, String victimUser) {
+    public void logSnitchInfo(Snitch snitch, Material material, Location loc, Date date, LoggedAction action, String initiatedUser, String victimUser) {
     	
         try {
+        	// snitchid
         	insertSnitchLogStmt.setInt(1,  snitch.getId());
+        	// snitch log time
         	insertSnitchLogStmt.setDate(2, new java.sql.Date(date.getTime()));
+        	// snitch logged action
         	insertSnitchLogStmt.setByte(3,  (byte)action.getLoggedActionId());
+        	// initiated user
         	insertSnitchLogStmt.setString(4,  initiatedUser);
-        	insertSnitchLogStmt.setString(5, victimUser);
-        	insertSnitchLogStmt.setInt(6,  block.getLocation().getBlockX());
-        	insertSnitchLogStmt.setInt(7,  block.getLocation().getBlockY());
-        	insertSnitchLogStmt.setInt(8,  block.getLocation().getBlockZ());
-        	insertSnitchLogStmt.setShort(9,  (short) block.getTypeId());
+        	
+        	// These columns, victimUser, location and materialid can all be null so check if it is an insert SQL null if it is
+        	
+        	// victim user
+        	if (victimUser != null) {
+        		insertSnitchLogStmt.setString(5, victimUser);
+        	} else {
+        		insertSnitchLogStmt.setNull(5, java.sql.Types.VARCHAR);
+        	}
+        	
+        	// location, x, y, z
+        	if (loc != null) {
+	        	insertSnitchLogStmt.setInt(6,  loc.getBlockX());
+	        	insertSnitchLogStmt.setInt(7,  loc.getBlockY());
+	        	insertSnitchLogStmt.setInt(8,  loc.getBlockZ());
+        	} else {
+        		insertSnitchLogStmt.setNull(6, java.sql.Types.INTEGER);
+        		insertSnitchLogStmt.setNull(7, java.sql.Types.INTEGER);
+        		insertSnitchLogStmt.setNull(8, java.sql.Types.INTEGER);
+        	}
+        	
+        	// materialid
+        	if (material != null) {
+        		insertSnitchLogStmt.setShort(9,  (short) material.getId());
+        	} else {
+        		insertSnitchLogStmt.setNull(9, java.sql.Types.SMALLINT);
+        	}
         	
         	insertSnitchLogStmt.execute();
         } catch (SQLException ex) {
         	this.plugin.getLogger().log(Level.SEVERE, String.format("Could not create snitch log entry! with snitch %s, " +
-        			"block %s, date %s, initiatedUser %s, victimUser %s", snitch, block, date, initiatedUser, victimUser), ex);
+        			"material %s, date %s, initiatedUser %s, victimUser %s", snitch, material, date, initiatedUser, victimUser), ex);
         }
     }
 
+    /**
+     * logs a message that someone killed an entity
+     * @param snitch - the snitch that recorded this event
+     * @param player - the player that did the killing
+     * @param entity - the entity that died
+     */
     public void logSnitchEntityKill(Snitch snitch, Player player, Entity entity) {
-        logSnitchInfo(String.format("%s killed a %s.", player.getName(), entity.toString()), snitch);
+    	
+    	// There is no material or location involved in this event
+    	this.logSnitchInfo(snitch, null, null, new Date(), LoggedAction.KILL, player.getPlayerListName(), entity.getClass().getName());
     }
 
     /**
-     * @param player
-     * @param victim
+     * Logs a message that someone killed another player
+     * @param snitch - the snitch that recorded this event
+     * @param player - the player that did the killing
+     * @param victim - the player that died
      */
     public void logSnitchPlayerKill(Snitch snitch, Player player, Player victim) {
-        logSnitchInfo(String.format("%s killed %s.", player.getName(), victim.getName()), snitch);
+    	// There is no material or location involved in this event
+    	this.logSnitchInfo(snitch, null, null, new Date(), LoggedAction.KILL, player.getPlayerListName(), victim.getPlayerListName());
     }
 
     /**
-     * @param player
-     * @param field
+     * Logs a message that someone entered the snitch's field
+     * @param snitch - the snitch that recorded this event
+     * @param player - the player that entered the snitch's field
+     * @param loc - the location of where the player entered
      */
     public void logSnitchEntry(Snitch snitch, Location loc, Player player) {
-        logSnitchInfo(String.format(
-            "%s made an entry at [%s%s %s(%sX: %d Y: %d Z: %d%s)]", player.getName(), ChatColor.AQUA, loc.getWorld().getName(),
-                ChatColor.RESET, ChatColor.RED, loc.getBlockX(), loc.getBlockY(), loc.getBlockZ(), ChatColor.RESET),
-            snitch);
+
+    	// no material or victimUser for this event
+    	this.logSnitchInfo(snitch, null, loc, new Date(), LoggedAction.ENTRY, player.getPlayerListName(), null);
     }
 
     /**
-     * @param player
-     * @param block
+     * Logs a message that someone broke a block within the snitch's field
+     * @param snitch - the snitch that recorded this event
+     * @param player - the player that broke the block
+     * @param block - the block that was broken
      */
     public void logSnitchBlockBreak(Snitch snitch, Player player, Block block) {
-        Location loc = block.getLocation();
-        logSnitchInfo(String.format(
-            "%s broke a %s at X: %d Y: %d Z: %d.", player.getName(), block.getType().toString(), loc.getBlockX(),
-                loc.getBlockY(), loc.getBlockZ()),
-            snitch);
-    }
 
-    /**
-     * @param player
-     * @param block
-     */
-    public void logSnitchBucketEmpty(Snitch snitch, Player player, Location loc, ItemStack item) {
-        logSnitchInfo(String.format(
-            "%s emptied a %s at X: %d Y: %d Z: %d.", player.getName(), item.getType().toString(), loc.getBlockX(),
-                loc.getBlockY(), loc.getBlockZ()),
-            snitch);
+    	// no victim user in this event
+    	this.logSnitchInfo(snitch, block.getType(), block.getLocation(), new Date(), LoggedAction.BLOCK_BREAK, player.getPlayerListName(), null);
     }
-
+    
     /**
-     * @param player
-     * @param block
-     */
-    public void logSnitchBucketFill(Snitch snitch, Player player, Block block) {
-        Location loc = block.getLocation();
-        logSnitchInfo(String.format(
-            "%s filled a bucket of %s at X: %d Y: %d Z: %d.", player.getName(), block.getType().toString(),
-                loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()),
-            snitch);
-    }
-
-    /**
-     * @param player
-     * @param block
+     * Logs a message that someone placed a block within the snitch's field
+     * @param snitch - the snitch that recorded this event
+     * @param player - the player that placed the block
+     * @param block - the block that was placed
      */
     public void logSnitchBlockPlace(Snitch snitch, Player player, Block block) {
-        Location loc = block.getLocation();
-        logSnitchInfo(String.format(
-            "%s placed a %s at X: %d Y: %d Z: %d.", player.getName(), block.getType().toString(),
-                loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()),
-            snitch);
+    	// no victim user in this event
+        this.logSnitchInfo(snitch, block.getType(), block.getLocation(), new Date(), LoggedAction.BLOCK_PLACE, player.getPlayerListName(), null);
     }
 
     /**
-     * @param player
-     * @param block
+     * Logs a message that someone emptied a bucket within the snitch's field
+     * @param snitch - the snitch that recorded this event
+     * @param player - the player that emptied the bucket
+     * @param loc - the location of where the bucket empty occurred
+     * @param item - the ItemStack representing the bucket that the player emptied
      */
-    public void logSnitchUsed(Snitch snitch, Player player, Block block) {
-        Location loc = block.getLocation();
-        logSnitchInfo(String.format(
-            "%s used a snitch at X: %d Y: %d Z: %d.", player.getName(), loc.getBlockX(),
-                loc.getBlockY(), loc.getBlockZ()),
-            snitch);
+    public void logSnitchBucketEmpty(Snitch snitch, Player player, Location loc, ItemStack item) {
+    	// no victim user in this event
+        this.logSnitchInfo(snitch, item.getType(), loc, new Date(), LoggedAction.BUCKET_EMPTY, player.getPlayerListName(), null);
+    }
+
+    /**
+     * Logs a message that someone filled a bucket within the snitch's field
+     * @param snitch - the snitch that recorded this event
+     * @param player - the player that filled the bucket
+     * @param block - the block that was 'put into' the bucket
+     */
+    public void logSnitchBucketFill(Snitch snitch, Player player, Block block) {
+    	// TODO: should we take a block or a ItemStack as a parameter here? 
+    	
+    	// no victim user in this event
+        this.logSnitchInfo(snitch, block.getType(), block.getLocation(), new Date(), LoggedAction.BUCKET_FILL, player.getPlayerListName(), null);
+    }
+
+
+
+    /**
+     * Logs a message that someone used a block within the snitch's field
+     * @param snitch - the snitch that recorded this event
+     * @param player - the player that used something
+     * @param block - the block that was used
+     */
+    public void logUsed(Snitch snitch, Player player, Block block) {
+        // TODO: what should we use to identify what was used? Block? Material? 
     }
 
     //Logs the snitch being placed at World, x, y, z in the database.
