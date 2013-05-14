e34af532301ee8c854ef89426da3eb9d20e81b16
Travis Christian <travis.w.christian@gmail.com>
2013-05-13 19:46:05 -0700

    added option for flat growth rate on crops under max glowstone light

--- plugins/realisticbiomes-paper/src/com/untamedears/realisticbiomes/RealisticBiomes.java
+++ plugins/realisticbiomes-paper/src/com/untamedears/realisticbiomes/RealisticBiomes.java
@@ -5,17 +5,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Logger;
 
-import org.bukkit.Chunk;
 import org.bukkit.Material;
 import org.bukkit.TreeType;
-import org.bukkit.World;
 import org.bukkit.block.Biome;
 import org.bukkit.block.Block;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.plugin.PluginManager;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import com.untamedears.realisticbiomes.listener.GrowListener;
 import com.untamedears.realisticbiomes.listener.PlayerListener;
