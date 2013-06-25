76d587c6a97a91f29bc3d57663c0f10c86b2dc62
erocs <github@erocs.org>
2013-06-24 22:17:11 -0700

    Whitespace

--- plugins/jukealert-paper/src/com/untamedears/JukeAlert/command/commands/LookupCommand.java
+++ plugins/jukealert-paper/src/com/untamedears/JukeAlert/command/commands/LookupCommand.java
@@ -15,56 +15,55 @@ import com.untamedears.JukeAlert.model.Snitch;
 
 public class LookupCommand extends PlayerCommand {
 
-	private SnitchManager snitchManager;
-	
-	public LookupCommand() {
+    private SnitchManager snitchManager;
+
+    public LookupCommand() {
         super("Lookup");
         setDescription("Lookup a snitch's group by its coordinates");
         setUsage("/jalookup <x> <y> <z> [world]");
         setArgumentRange(3, 4);
         setIdentifier("jalookup");
         snitchManager = plugin.getSnitchManager();
-	}
+    }
 
-	@Override
-	public boolean execute(CommandSender sender, String[] args) {
-		boolean canLookupAny = (sender instanceof ConsoleCommandSender || sender.hasPermission("jukealert.admin.lookupany"));
+    @Override
+    public boolean execute(CommandSender sender, String[] args) {
+        boolean canLookupAny = (sender instanceof ConsoleCommandSender || sender.hasPermission("jukealert.admin.lookupany"));
         if (sender instanceof Player || canLookupAny) {
-        	int x, y, z;
-        	String world;
-        	try {
-        		x = Integer.parseInt(args[0]);
-        		y = Integer.parseInt(args[1]);
-        		z = Integer.parseInt(args[2]);
-        		if(args.length == 3) {
-        			world = "world";
-        		} else {
-        			world = args[3];
-        		}
-        	} catch (NumberFormatException e) {
-        		sender.sendMessage(ChatColor.RED + "Invalid coordinates.");
-        		return false;
-        	}
-    		if(Bukkit.getWorld(world) == null) {
-        		sender.sendMessage(ChatColor.RED + "Invalid world.");
-    			return false;
-    		}
-        	Location loc = new Location(Bukkit.getWorld(world), x, y, z);
-        	Snitch match = snitchManager.getSnitch(loc.getWorld(), loc);
-        	if(match == null) {
-        		sender.sendMessage(ChatColor.RED + "You do not own a snitch at those coordinates!");
-        		return false;
-        	}
-        	if(canLookupAny || isOnSnitch(match, sender.getName())) {
-        		sender.sendMessage(ChatColor.AQUA + "The snitch at [" + x + " " + y + " " + z + "] is owned by " + match.getGroup().getName());
-        	} else {
-        		sender.sendMessage(ChatColor.RED + "You do not own a snitch at those coordinates!");       		
-        	}
-        	return true;
+            int x, y, z;
+            String world;
+            try {
+                x = Integer.parseInt(args[0]);
+                y = Integer.parseInt(args[1]);
+                z = Integer.parseInt(args[2]);
+                if(args.length == 3) {
+                    world = "world";
+                } else {
+                    world = args[3];
+                }
+            } catch (NumberFormatException e) {
+                sender.sendMessage(ChatColor.RED + "Invalid coordinates.");
+                return false;
+            }
+            if(Bukkit.getWorld(world) == null) {
+                sender.sendMessage(ChatColor.RED + "Invalid world.");
+                return false;
+            }
+            Location loc = new Location(Bukkit.getWorld(world), x, y, z);
+            Snitch match = snitchManager.getSnitch(loc.getWorld(), loc);
+            if(match == null) {
+                sender.sendMessage(ChatColor.RED + "You do not own a snitch at those coordinates!");
+                return false;
+            }
+            if(canLookupAny || isOnSnitch(match, sender.getName())) {
+                sender.sendMessage(ChatColor.AQUA + "The snitch at [" + x + " " + y + " " + z + "] is owned by " + match.getGroup().getName());
+            } else {
+                sender.sendMessage(ChatColor.RED + "You do not own a snitch at those coordinates!");               
+            }
+            return true;
         } else {
-        	sender.sendMessage(ChatColor.RED + "You do not own a snitch at those coordinates!");  
+            sender.sendMessage(ChatColor.RED + "You do not own a snitch at those coordinates!");  
             return false;
         }
-	}
-	
+    }
 }
