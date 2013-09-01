760128c7311de77d26bdd2be9f6e6f3d4c373f18
MCL0stman <MinecraftL0stman@gmail.com>
2013-09-01 00:48:00 -0400

    JukeAlert - Bug #74 - Removed juke alert requirement for citadel tables Updated juke alert to pass a guid seperated list of values for the group names to a new procedure.  This allows us to inject the faction groups to the juke alert procedure as opposed to getting those at runtime. Because there are no character strings disallowed in citadel, we have to safely inject multiple data values with no known delimiter.  As such, the safest way to handle is to simply specify a guid as the delimiter. At that point, we can safely delimit our citadel group strings.  Moved prepared statement to a call to the procedure instead of the direct sql.

--- plugins/jukealert-paper/src/com/untamedears/JukeAlert/group/GroupMediator.java
+++ plugins/jukealert-paper/src/com/untamedears/JukeAlert/group/GroupMediator.java
@@ -1,5 +1,8 @@
 package com.untamedears.JukeAlert.group;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import com.untamedears.citadel.Citadel;
 import com.untamedears.citadel.GroupManager;
 import com.untamedears.citadel.entity.Faction;
@@ -14,5 +17,35 @@ public class GroupMediator {
 
     public Faction getGroupByName(String groupName) {
         return groupManager.getGroup(groupName);
+    }
+    
+    public List<String> getGroupsByPlayerName(String playerName, boolean includeFounders, boolean includeModerators, boolean includeMembers) {
+    	List<String> returnValue = new ArrayList<String>();
+    	
+    	if (includeFounders) {
+    		for(Faction f : groupManager.getGroupsByFounder(playerName)) {
+    			if (!returnValue.contains(f.getName())) {
+    				returnValue.add(f.getName());
+    			}
+    		}
+    	}
+    	
+    	if (includeModerators) {
+    		for(Faction f : groupManager.getGroupsByModerator(playerName)) {
+    			if (!returnValue.contains(f.getName())) {
+    				returnValue.add(f.getName());
+    			}
+    		}
+    	}
+    	
+    	if (includeMembers) {
+    		for(Faction f : groupManager.getGroupsByMember(playerName)) {
+    			if (!returnValue.contains(f.getName())) {
+    				returnValue.add(f.getName());
+    			}
+    		}
+    	}
+    	
+    	return returnValue;
     }
 }
