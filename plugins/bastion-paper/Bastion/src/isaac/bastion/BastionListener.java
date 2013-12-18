3b05c72c0f31fb60d7e27490db95d6daa140000a
space_fountain <spacefountain@gmail.com>
2013-12-18 15:53:04 -0500

    Oh I forgot dispensers.

--- plugins/bastion-paper/Bastion/src/isaac/bastion/BastionListener.java
+++ plugins/bastion-paper/Bastion/src/isaac/bastion/BastionListener.java
@@ -8,15 +8,19 @@ import com.untamedears.citadel.entity.PlayerReinforcement;
 import com.untamedears.citadel.events.CreateReinforcementEvent;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.event.block.BlockPistonExtendEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.block.EntityBlockFormEvent;
 import org.bukkit.event.player.PlayerBucketEmptyEvent;
+import org.bukkit.material.Dispenser;
+import org.bukkit.material.MaterialData;
 
 public final class BastionListener
 implements Listener
@@ -68,6 +72,26 @@ implements Listener
 		Block placedOn=event.getBlockClicked().getRelative(event.getBlockFace());
 		Location placed=placedOn.getLocation();
 		boolean shouldCancel = bastionManager.handleBlockPlace(placed,event.getPlayer(),true);
+		event.setCancelled(shouldCancel);
+
+	}
+	@EventHandler
+	public void dispensed(BlockDispenseEvent  event){
+		if(event.isCancelled()){
+			return;
+		}
+		Material mat=event.getItem().getType();
+		if(mat!=Material.FLINT_AND_STEEL&&mat!=Material.WATER_BUCKET&&mat!=Material.LAVA_BUCKET){
+			return;
+		}
+		
+		MaterialData blockData = event.getBlock().getState().getData();
+		Dispenser dispenser = (Dispenser) blockData;
+		BlockFace facing=dispenser.getFacing();
+		Block block=event.getBlock().getRelative(facing);
+		
+		boolean shouldCancel = bastionManager.handleBlockPlace(block.getLocation(),false);
+		Bastion.getPlugin().getLogger().info("shouldCancel="+shouldCancel);
 		event.setCancelled(shouldCancel);
 
 	}
