06b54e928192a310410e58aef6071fd4238922d6
jjj5311 <jjj5311@gmail.com>
2015-03-04 17:46:34 -0600

    remove unused imports

--- plugins/namelayer-paper/src/vg/civcraft/mc/namelayer/NameLayerPlugin.java
+++ plugins/namelayer-paper/src/vg/civcraft/mc/namelayer/NameLayerPlugin.java
@@ -1,10 +1,7 @@
 package vg.civcraft.mc.namelayer;
 
 import java.io.File;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
