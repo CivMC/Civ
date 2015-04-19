df8f02ec338a2222b5cbdfc36d420bf38c1bddc7
Rourke750 <Rourke7501@gmail.com>
2015-04-19 12:34:52 -0400

    Fix issue with associationListener

--- plugins/namelayer-paper/src/vg/civcraft/mc/namelayer/listeners/AssociationListener.java
+++ plugins/namelayer-paper/src/vg/civcraft/mc/namelayer/listeners/AssociationListener.java
@@ -2,6 +2,7 @@ package vg.civcraft.mc.namelayer.listeners;
 
 import java.util.UUID;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -9,19 +10,28 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import vg.civcraft.mc.namelayer.NameAPI;
+import vg.civcraft.mc.namelayer.NameLayerPlugin;
 import vg.civcraft.mc.namelayer.database.AssociationList;
 import vg.civcraft.mc.namelayer.misc.ClassHandler;
 import vg.civcraft.mc.namelayer.misc.ProfileInterface;
 
 public class AssociationListener implements Listener{
 	
-	private AssociationList associations = NameAPI.getAssociationList();
+	private AssociationList associations;
 	private ClassHandler ch = ClassHandler.ch;
 	private ProfileInterface game;
 	
 	public AssociationListener(){
 		if (ch != null)
 			game = ch.getProfileClass();
+		Bukkit.getScheduler().runTaskLater(NameLayerPlugin.getInstance(), new Runnable(){
+
+			@Override
+			public void run() {
+				associations =  NameAPI.getAssociationList();
+			}
+			
+		}, 1);
 	}
 	
 	private String packageName = getClass().getPackage().getName();
