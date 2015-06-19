45cdaa17c3bc2da710a4dd938159a975fd7c696d
gipsy <ste3ls@gmail.com>
2015-06-19 21:37:46 +0200

    PlayerInteract untangle ifs, fix handle cocoa info block offset
    also remove unused import somewhere.
    when trees are done, change all getData() magic numbers to enums and
    most improtantly make the growthConfig-map typesafe

--- plugins/realisticbiomes-paper/src/com/untamedears/realisticbiomes/persist/Fruits.java
+++ plugins/realisticbiomes-paper/src/com/untamedears/realisticbiomes/persist/Fruits.java
@@ -3,7 +3,6 @@ package com.untamedears.realisticbiomes.persist;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
