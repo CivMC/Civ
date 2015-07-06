b84d1bf49e710aad693989919050874c53852197
Rourke750 <Rourke7501@gmail.com>
2015-07-05 21:25:47 -0400

    Added QTBox to Civmodcore

---
+++ plugins/civmodcore-paper/src/vg/civcraft/mc/civmodcore/locations/QTBox.java
@@ -0,0 +1,10 @@
+package vg.civcraft.mc.civmodcore.locations;
+
+public interface QTBox {
+  public int qtXMin();
+  public int qtXMid();
+  public int qtXMax();
+  public int qtYMin();
+  public int qtYMid();
+  public int qtYMax();
+}
