f4d0a251336636bc8e00c90a6a166d1267ae3929
maxopoly <maxseinbot@gmx.de>
2015-11-05 21:17:01 +0100

    Giving the mercury listener it's own event to use

--- plugins/namelayer-paper/src/vg/civcraft/mc/namelayer/listeners/MercuryMessageListener.java
+++ plugins/namelayer-paper/src/vg/civcraft/mc/namelayer/listeners/MercuryMessageListener.java
@@ -6,12 +6,14 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+
 import vg.civcraft.mc.mercury.MercuryAPI;
 import vg.civcraft.mc.mercury.events.AsyncPluginBroadcastMessageEvent;
 import vg.civcraft.mc.namelayer.GroupManager;
 import vg.civcraft.mc.namelayer.NameLayerPlugin;
 import vg.civcraft.mc.namelayer.NameAPI;
 import vg.civcraft.mc.namelayer.events.GroupDeleteEvent;
+import vg.civcraft.mc.namelayer.events.GroupInvalidationEvent;
 import vg.civcraft.mc.namelayer.events.GroupMergeEvent;
 import vg.civcraft.mc.namelayer.events.GroupTransferEvent;
 import vg.civcraft.mc.namelayer.group.Group;
@@ -33,25 +35,26 @@ public class MercuryMessageListener implements Listener{
 		String[] message = event.getMessage().split(" ");
 		String reason = message[0];	
 		String group = message[1];
-		Group g = GroupManager.getGroup(group);
 		if (reason.equals("recache")){
-			gm.invalidateCache(g.getName());
+			GroupInvalidationEvent e = new GroupInvalidationEvent(reason, group);
+			Bukkit.getPluginManager().callEvent(e);
+			gm.invalidateCache(group);
 		}
 		else if (reason.equals("delete")){
-			GroupDeleteEvent e = new GroupDeleteEvent(g, true);
+			GroupInvalidationEvent e = new GroupInvalidationEvent(reason, group);
 			Bukkit.getPluginManager().callEvent(e);
-			gm.invalidateCache(g.getName());
+			gm.invalidateCache(group);
 		}
 		else if (reason.equals("merge")){
-			GroupMergeEvent e = new GroupMergeEvent(g, GroupManager.getGroup(message[2]), true);
+			GroupInvalidationEvent e = new GroupInvalidationEvent(reason, group, message[2]);
 			Bukkit.getPluginManager().callEvent(e);
-			gm.invalidateCache(g.getName());
+			gm.invalidateCache(group);
 			gm.invalidateCache(message[2]);
 		}
 		else if (reason.equals("transfer")){
-			GroupTransferEvent e = new GroupTransferEvent(g, UUID.fromString(message[2]));
+			GroupInvalidationEvent e = new GroupInvalidationEvent(reason, message[2]);
 			Bukkit.getPluginManager().callEvent(e);
-			gm.invalidateCache(g.getName());
+			gm.invalidateCache(group);
 		}
 	}
 
