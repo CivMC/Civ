c1259e46df0bc97c3c02e30af8316618a9ca2297
maxopoly <maxseinbot@gmx.de>
2015-12-21 18:48:32 +0100

    Preventing abuse of tab autocompletes to list group members

--- plugins/namelayer-paper/NameLayerMain/src/vg/civcraft/mc/namelayer/command/TabCompleters/GroupMemberTabCompleter.java
+++ plugins/namelayer-paper/NameLayerMain/src/vg/civcraft/mc/namelayer/command/TabCompleters/GroupMemberTabCompleter.java
@@ -4,20 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.bukkit.entity.Player;
+
 import vg.civcraft.mc.namelayer.GroupManager;
 import vg.civcraft.mc.namelayer.NameAPI;
 import vg.civcraft.mc.namelayer.group.Group;
+import vg.civcraft.mc.namelayer.permission.GroupPermission;
+import vg.civcraft.mc.namelayer.permission.PermissionType;
 
 public class GroupMemberTabCompleter {
-	public static List<String> complete(String groupName, String playerName) {
+	public static List<String> complete(String groupName, String playerName,
+			Player sender) {
 		Group g = GroupManager.getGroup(groupName);
 		if (g != null) {
-			ArrayList<String> result = new ArrayList<String>();
-			List<UUID> uuids = g.getMembersByName(playerName);
-			for (UUID uuid : uuids) {
-				result.add(NameAPI.getCurrentName(uuid));
+			GroupPermission perm = NameAPI.getGroupManager()
+					.getPermissionforGroup(g);
+			if (perm.isAccessible(g.getPlayerType(sender.getUniqueId()), PermissionType.MEMBERS)) {
+				ArrayList<String> result = new ArrayList<String>();
+				List<UUID> uuids = g.getMembersByName(playerName);
+				for (UUID uuid : uuids) {
+					result.add(NameAPI.getCurrentName(uuid));
+				}
+				return result;
 			}
-			return result;
 		}
 		return null;
 	}
