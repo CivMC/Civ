94d1bee3f94f87f1fdb9945d452703982ddce6a5
Rourke750 <Rourke7501@gmail.com>
2015-12-22 19:12:48 -0500

    fix for db initializing

--- plugins/citadel-paper/src/vg/civcraft/mc/citadel/database/CitadelReinforcementData.java
+++ plugins/citadel-paper/src/vg/civcraft/mc/citadel/database/CitadelReinforcementData.java
@@ -33,8 +33,8 @@ public class CitadelReinforcementData {
 	public CitadelReinforcementData(Database db){
 		this.db = db;
 		if (db.connect()){
-			createTables();
 			initalizePreparedStatements();
+			createTables();
 			intitializeProcedures();
 		}
 	}
