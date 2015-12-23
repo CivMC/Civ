6c7399647fa2e3ee1371ec9274071f0b273e5a16
BuildTools <idoash4@gmail.com>
2015-12-23 23:43:05 +0200

    Moving ignoring players and groups to database
    Bug fixes
    
    More bug fixes

--- plugins/civchat2-paper/src/vg/civcraft/mc/civchat2/command/commands/IgnoreList.java
+++ plugins/civchat2-paper/src/vg/civcraft/mc/civchat2/command/commands/IgnoreList.java
@@ -1,6 +1,7 @@
 package vg.civcraft.mc.civchat2.command.commands;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
@@ -9,14 +10,17 @@ import org.bukkit.entity.Player;
 import vg.civcraft.mc.civchat2.CivChat2;
 import vg.civcraft.mc.civchat2.CivChat2Manager;
 import vg.civcraft.mc.civchat2.command.CivChat2CommandHandler;
+import vg.civcraft.mc.civchat2.database.DatabaseManager;
 import vg.civcraft.mc.civchat2.utility.CivChat2Log;
 import vg.civcraft.mc.civmodcore.command.PlayerCommand;
+import vg.civcraft.mc.namelayer.NameAPI;
 
 public class IgnoreList extends PlayerCommand{
 	private CivChat2 plugin = CivChat2.getInstance();
 	private CivChat2Manager chatMan;
 	private CivChat2Log logger = CivChat2.getCivChat2Log();
 	private CivChat2CommandHandler handler = (CivChat2CommandHandler) plugin.getCivChat2CommandHandler();
+	private DatabaseManager DBM = plugin.getDatabaseManager();
 	
 	public IgnoreList(String name) {
 		super(name);
@@ -42,8 +46,8 @@ public class IgnoreList extends PlayerCommand{
 			return true;
 		}
 		
-		List<String> players = chatMan.getIgnoredPlayers(player.getName());
-		List<String> groups = chatMan.getIgnoredGroups(player.getName());
+		List<UUID> players = DBM.getIgnoredPlayers(player.getUniqueId());
+		List<String> groups = DBM.getIgnoredGroups(player.getUniqueId());
 		
 		if(players == null){
 			//no players ignored
@@ -52,9 +56,12 @@ public class IgnoreList extends PlayerCommand{
 			StringBuilder sb = new StringBuilder();
 			sb.append("Ignored Players: ");
 			sb.append("\n");
-			for(String s : players){
-				sb.append(s);
-				sb.append(", ");
+			for(UUID playerUUID : players){
+				String playerName = NameAPI.getCurrentName(playerUUID);
+				if(playerName != null){
+					sb.append(playerName);
+					sb.append(", ");
+				}
 			}
 			sender.sendMessage(ChatColor.YELLOW + sb.toString());
 		}
