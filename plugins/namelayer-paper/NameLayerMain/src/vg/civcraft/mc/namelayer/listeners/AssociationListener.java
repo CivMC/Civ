4f7549df2923d358ee031e0b951088123fdea767
tyrothalos <s.goeppele.parrish@gmail.com>
2016-07-01 23:14:54 -0700

    Converted NMS to reflection.

--- plugins/namelayer-paper/NameLayerMain/src/vg/civcraft/mc/namelayer/listeners/AssociationListener.java
+++ plugins/namelayer-paper/NameLayerMain/src/vg/civcraft/mc/namelayer/listeners/AssociationListener.java
@@ -17,41 +17,40 @@ import vg.civcraft.mc.namelayer.misc.ClassHandler;
 import vg.civcraft.mc.namelayer.misc.ProfileInterface;
 
 public class AssociationListener implements Listener{
-	
+
 	private AssociationList associations;
-	private ClassHandler ch;
-	private ProfileInterface game;
-	
-	public AssociationListener(){
-		Bukkit.getScheduler().runTaskLater(NameLayerPlugin.getInstance(), new Runnable(){
+	private PlayerNameModifier modifier;
+
+	public AssociationListener() {
+		try {
+			modifier = new PlayerNameModifier();
+		} catch (ClassNotFoundException | NoSuchFieldException | NoSuchMethodException e) {
+			// TODO: A proper logging library needs to be added.
+			e.printStackTrace();
+		}
+
+		Bukkit.getScheduler().runTaskLater(NameLayerPlugin.getInstance(), new Runnable() {
 
 			@Override
 			public void run() {
-				ch = ClassHandler.ch;
-				if (ClassHandler.properlyEnabled)
-					game = ch.getProfileClass();
-				
 				associations = NameAPI.getAssociationList();
 			}
-			
+
 		}, 1);
 	}
-	
+
 	@EventHandler(priority=EventPriority.LOWEST)
-	public void OnPlayerJoin(PlayerJoinEvent event)
-	{
+	public void onPlayerJoin(PlayerJoinEvent event) {
 		String playername = event.getPlayer().getName();
 		UUID uuid = event.getPlayer().getUniqueId();
 		associations.addPlayer(playername, uuid);
 		event.setJoinMessage(ChatColor.YELLOW + NameAPI.getCurrentName(uuid) + " joined the game");
 	}
-	
+
 	@EventHandler(priority=EventPriority.LOWEST)
-	public void OnPlayerLogin(PlayerLoginEvent event)
-	{
+	public void onPlayerLogin(PlayerLoginEvent event) {
 		if (associations.getCurrentName(event.getPlayer().getUniqueId()) == null)
 			return;
-		if (game != null)
-			game.setPlayerProfle(event.getPlayer());
+		modifier.modify(event.getPlayer());
 	}
 }
