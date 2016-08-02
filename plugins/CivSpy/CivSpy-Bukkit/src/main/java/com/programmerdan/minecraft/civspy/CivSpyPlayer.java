e66e75850e02ffefd77ec9a117260d58adce971f
ProgrammerDan <programmerdan@gmail.com>
2016-08-02 18:15:09 -0400

    Partial

--- plugins/CivSpy/CivSpy-Bukkit/src/main/java/com/programmerdan/minecraft/civspy/CivSpyPlayer.java
+++ plugins/CivSpy/CivSpy-Bukkit/src/main/java/com/programmerdan/minecraft/civspy/CivSpyPlayer.java
@@ -25,4 +25,32 @@ public class CivSpyPlayer implements Cloneable {
 			return key;
 		}
 	}
+
+	class Key implements Comparable<Key> {
+		private Integer chunkX;
+		private Integer chunkY;
+		private UUID player;
+
+		@Override
+		public int compareTo(Key o) {
+			if ((this.chunkX == null && o.chunkX != null) || this.chunkX < o.chunkX) {
+				return -1;
+			} else if ((this.chunkX == this.chunkX > o.chunkX) {
+				return 1;
+			} else {
+				if (this.chunkZ < o.chunkZ) {
+					return -1;
+				} else if (this.chunkZ > o.chunkZ) {
+					return 1;
+				} else {
+					return this.player.compareTo(o);
+				}
+
+			}
+		}
+
+		@Override
+		public int hashCode() {
+
+		}
 }
