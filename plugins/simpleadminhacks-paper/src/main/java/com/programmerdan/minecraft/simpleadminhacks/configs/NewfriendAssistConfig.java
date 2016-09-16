0cef35c37649912d9521bb0310d4f2d9f6468032
ProgrammerDan <programmerdan@gmail.com>
2016-09-16 15:05:12 -0400

    Fixing announce message loading, wasn't working for some reason.

--- plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/configs/NewfriendAssistConfig.java
+++ plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/configs/NewfriendAssistConfig.java
@@ -22,7 +22,7 @@ public class NewfriendAssistConfig extends SimpleHackConfig {
 	/**
 	 * What should I send to people when I announce a newfriend?
 	 */
-	private String announceMessage = "&%Player% is brand new!";
+	private String announceMessage = "&f%Player% is brand new!";
 	/**
 	 * Who should I broadcast to?
 	 */
@@ -48,7 +48,7 @@ public class NewfriendAssistConfig extends SimpleHackConfig {
 
 	@Override
 	protected void wireup(ConfigurationSection config) {		
-		this.announceMessage = config.getString("announce.message", announceMessage);
+		this.announceMessage = config.getString("announce", announceMessage);
 		
 		List<String> broadcastTo = config.getStringList("broadcast");
 		if (this.announceBroadcast == null) {
