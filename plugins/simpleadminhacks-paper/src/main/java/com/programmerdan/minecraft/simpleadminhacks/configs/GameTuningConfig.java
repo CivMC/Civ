0dc8c6cee3bc93220a5b7976d329d0b3211ba631
ProgrammerDan <programmerdan@gmail.com>
2016-10-14 19:58:50 -0400

    Compilable, first tuning element is limiting tile entities in a chunk. Needs example configuration section.

--- plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/configs/GameTuningConfig.java
+++ plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/configs/GameTuningConfig.java
@@ -1,6 +1,18 @@
 package com.programmerdan.minecraft.simpleadminhacks.configs;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.UUID;
+import java.util.List;
+import java.util.logging.Level;
+
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.ChatColor;
+import org.bukkit.Bukkit;
 
 import com.programmerdan.minecraft.simpleadminhacks.SimpleAdminHacks;
 import com.programmerdan.minecraft.simpleadminhacks.SimpleHackConfig;
@@ -12,9 +24,10 @@ import com.programmerdan.minecraft.simpleadminhacks.SimpleHackConfig;
  */
 public class GameTuningConfig extends SimpleHackConfig {
 
+	private boolean chunkLimitsEnabled;
 	private Map<Material, Integer> blockEntityLimits;
 	private Set<UUID> exemptFromLimits;
-	private boolean limitsPreventExploits;
+	private String chunkLimitsExceededMessage;
 
 	public GameTuningConfig(SimpleAdminHacks plugin, ConfigurationSection base) {
 		super(plugin, base);
@@ -22,12 +35,97 @@ public class GameTuningConfig extends SimpleHackConfig {
 
 	@Override
 	protected void wireup(ConfigurationSection config) {
-		this.blockEntityLimits = new HashMap<Material, Integer>();
-		// for each "chunkLimits.tileEntities: " entry, record limit.
-		this.exemptFromLimits = new HashSet<UUID>();
-		// for each "chunkLimits.exempt: " entry, record limit.
-		this.limitsPreventExploits = config.getBoolean("chunkLimits.preventExploits", true);
-		// Prevent piston movement, etc.
+		ConfigurationSection chunkLimits = config.getConfigurationSection("chunkLimits");
+		wireupChunkLimits(chunkLimits);
 
+		/* Add additional tuning config grabs here. */
+	}
+
+	/**
+	 * Wireup for Chunk Limits configuration
+	 * 
+	 * @author ProgrammerDan
+	 *
+	 */ 
+	private void wireupChunkLimits(ConfigurationSection config) {
+		this.blockEntityLimits = new HashMap<Material, Integer>();
+		this.exemptFromLimits = new HashSet<UUID>();
+		this.chunkLimitsEnabled = false;
+		if (config == null) return;
+
+		this.chunkLimitsEnabled = config.getBoolean("enabled", false);
+		this.chunkLimitsExceededMessage = ChatColor.translateAlternateColorCodes('&',
+				config.getString("exceededMessage", 
+						ChatColor.RED + "Limit for this chunk reached, you cannot place that! Use a different block."));
+
+		// for each "chunkLimits.tileEntities: " entry, record limit.
+		ConfigurationSection tileEntities = config.getConfigurationSection("tileEntities");
+		for (String key : tileEntities.getKeys(false)) {
+			int limit = tileEntities.getInt(key);
+			Material toBlock = Material.getMaterial(key);
+			if (toBlock == null) continue;
+
+			this.blockEntityLimits.put(toBlock, limit);
+			plugin().log(Level.INFO, " Limiting {0} to {1} per chunk", toBlock.toString(), limit);
+		}
+		
+		// for each "chunkLimits.exempt: " entry, record limit.
+		List<String> exempts = config.getStringList("exempt");
+		for (String exempt : exempts) {
+			UUID pExempt = null;
+			try {
+				pExempt = UUID.fromString(exempt);
+			} catch (IllegalArgumentException iae) {
+				pExempt = null;
+			}
+			if (pExempt == null) {
+				OfflinePlayer ofp = Bukkit.getOfflinePlayer(exempt);
+				if (ofp != null) {
+					pExempt = ofp.getUniqueId();
+				}
+			}
+			if (pExempt == null) {
+				plugin().log(Level.INFO, " Unable to exempt {0}, player not found", exempt);
+			} else {
+				plugin().log(Level.INFO, " Adding limits exemption for {0} as {1}", exempt, pExempt);
+				this.exemptFromLimits.add(pExempt);
+			}
+		}
+	}
+
+
+	/**
+	 * @return true / false if chunk limits are on
+	 */
+	public boolean areChunkLimitsEnabled() {
+		return this.chunkLimitsEnabled;
+	}
+
+	/**
+	 * Gets the chunk limit for a specified material.
+	 * Returns null if no limit is set.
+	 *
+	 * @param mat the Material to return a limit on, if set.
+	 * @return the max # per chunk, or null if no limit set.
+	 */
+	public Integer getChunkLimit(Material mat) {
+		return this.chunkLimitsEnabled ? this.blockEntityLimits.get(mat) : null;
+	}
+
+	/**
+	 * Returns false if the supplied UUID is exempt from limits.
+	 *
+	 * @param player the UUID to check
+	 * @returns false it the player is exempt, true otherwise.
+	 */
+	public boolean applyChunkLimits(UUID player) {
+		return this.chunkLimitsEnabled ? !this.exemptFromLimits.contains(player) : false;
+	}
+
+	/**
+	 * @return the generic limit message. TODO: Replace with customized message tailored per player.
+	 */
+	public String getChunkLimitsExceededMessage() {
+		return this.chunkLimitsExceededMessage;
 	}
 }
--- plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/hacks/GameTuning.java
+++ plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/hacks/GameTuning.java
@@ -3,13 +3,22 @@ package com.programmerdan.minecraft.simpleadminhacks.hacks;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.BlockPistonExtendEvent;
+import org.bukkit.event.block.BlockPistonRetractEvent;
+import org.bukkit.entity.Player;
+import org.bukkit.Material;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Block;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 
+import org.bukkit.configuration.ConfigurationSection;
+
 import com.programmerdan.minecraft.simpleadminhacks.SimpleAdminHacks;
 import com.programmerdan.minecraft.simpleadminhacks.SimpleHack;
-import com.programmerdan.minecraft.simpleadminhacks.configs.CTAnnounceConfig;
-import com.programmerdan.minecraft.simpleadminhacks.configs.ExperimentalConfig;
+import com.programmerdan.minecraft.simpleadminhacks.configs.GameTuningConfig;
 
 /**
  * This is a grab-bag class to hold any _tuning_ related configurations that impact the 
@@ -23,6 +32,111 @@ import com.programmerdan.minecraft.simpleadminhacks.configs.ExperimentalConfig;
  *
  * Currently you can control the following:
  *  - BlockEntity limits per chunk
-public class GameTuning extends SimpleHack<GameTuningConfig> implements Listener, CommandExecutor {
+ */
+public class GameTuning extends SimpleHack<GameTuningConfig> implements Listener {
+	public static final String NAME = "GameTuning";
 
+	public GameTuning(SimpleAdminHacks plugin, GameTuningConfig config) {
+		super(plugin, config);
+	}
+
+	@Override
+	public void registerListeners() {
+		if (config != null && config.isEnabled()) {
+			plugin().log("Registering GameTuning listeners");
+			plugin().registerListener(this);
+		}
+	}
+
+	@Override
+	public void unregisterListeners() {
+		// Bukkit does this for us.
+	}
+
+	@Override
+	public void registerCommands() {
+	}
+
+	@Override
+	public void unregisterCommands() {
+	}
+
+	@Override
+	public void dataBootstrap() {
+	}
+
+	@Override
+	public void dataCleanup() {
+		// NO-OP
+	}
+
+	@Override
+	public String status() {
+		StringBuilder genStatus = new StringBuilder();
+		genStatus.append("GameTuning is ");
+		if (config != null && config.isEnabled()) {
+			genStatus.append("active\n");
+			if (config.areChunkLimitsEnabled()) {
+				genStatus.append("  Chunk Limits are enabled\n");
+			} else {
+				genStatus.append("  Chunk Limits are disabled\n");
+			}
+			// more?
+		} else {
+			genStatus.append("inactive");
+		}
+		return genStatus.toString();
+	}
+
+	public static GameTuningConfig generate(SimpleAdminHacks plugin, ConfigurationSection config) {
+		return new GameTuningConfig(plugin, config);
+	}
+	
+	/* From here on, the actual meat of the hack. Above is basically boilerplate for micro-plugins.*/
+
+	/**
+	 * Many thanks to BlackXNT for his work on this event in Humbug, which I have largely copied and expanded.
+	 *
+	 * This tracks block placements, and if a limit is configured and the block is a TileEntity w/ state,
+	 * will reject the placement if otherwise it would exceed limits for the Chunk.
+	 * 
+	 * @param event the Placement event.
+	 */
+	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled=true)
+	public void chunkLimits(BlockPlaceEvent event) {
+		if (!config.isEnabled()) return;
+
+		Player player = event.getPlayer();
+		if (player == null) return;
+
+		Block block = event.getBlock();
+		if (block == null) return;
+
+		if (!config.applyChunkLimits(player.getUniqueId())) return;
+
+		Material mat = block.getType();
+		if (mat == null) return;
+
+		Integer limit = config.getChunkLimit(mat);
+		if (limit == null) return;
+
+		int current = 0;
+		for (BlockState state : block.getChunk().getTileEntities() ) {
+			if (mat.equals(state.getType())) {
+				if ( ++current > limit) {
+					event.setCancelled(true);
+					player.sendMessage( config.getChunkLimitsExceededMessage() );
+					return;
+				}
+			}
+		}
+	}
+
+	// TODO
+	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled=true) 
+	public void chunkLimitsExploitExtend(BlockPistonExtendEvent event) {}
+
+	// TODO
+	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled=true)
+	public void chunkLimitsExploitRetract(BlockPistonRetractEvent event) {}
 }
