06aa931ee1c7396ce5dac173e7ee0305bcdcd04a
ProgrammerDan <programmerdan@gmail.com>
2017-04-14 15:22:36 -0400

    Layering in an event system for RealisticBiomes to allow other plugins to connect to the outcomes of RBs manipulation of bukkit events.

---
+++ plugins/realisticbiomes-paper/src/com/untamedears/realisticbiomes/events/RealisticBiomesStructureGrowEvent.java
@@ -0,0 +1,36 @@
+package com.untamedears.realisticbiomes.events;
+
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.TreeType;
+import org.bukkit.block.BlockState;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.world.StructureGrowEvent;
+
+public class RealisticBiomesStructureGrowEvent extends Event {
+	private static final HandlerList handlers = new HandlerList();
+	
+	private StructureGrowEvent wrapped;
+	
+	public RealisticBiomesStructureGrowEvent(Location location, TreeType species, boolean bonemeal, Player player,
+			List<BlockState> blocks) {
+		super(false);
+		wrapped = new StructureGrowEvent(location, species, bonemeal, player, blocks);
+	}
+	
+	@Override
+	public HandlerList getHandlers() {
+		return RealisticBiomesStructureGrowEvent.handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+	
+	public StructureGrowEvent getEvent() {
+		return wrapped;
+	}
+}
