7512264e607e05ff6594f424ca3c7549fff3c47c
iykHvfIvfR <iykHvfIvfR@users.noreply.github.com>
2017-07-09 04:23:35 +0000

    Use List insted of Set

--- plugins/jukealert-paper/src/main/java/com/untamedears/JukeAlert/storage/JukeAlertLogger.java
+++ plugins/jukealert-paper/src/main/java/com/untamedears/JukeAlert/storage/JukeAlertLogger.java
@@ -1633,7 +1633,7 @@ public class JukeAlertLogger {
 		} else {
 			// Add the first value
 			String[] mutedGroups = set.getString(2).split("\\s+");
-			Set<String> mutedGroupsSet = new HashSet<String>(Arrays.asList(mutedGroups));
+			List<String> mutedGroupsSet = new ArrayList<String>(Arrays.asList(mutedGroups));
 			if (mutedGroupsSet.contains(ignoredGroup)) {
 				ignoringUsers.add(set.getString(1));
 			}
@@ -1641,7 +1641,7 @@ public class JukeAlertLogger {
 			while (set.next()) {
 				// Create set of uuids
 				mutedGroups = set.getString(2).split("\\s+");
-				mutedGroupsSet = new HashSet<String>(Arrays.asList(mutedGroups));
+				mutedGroupsSet = new ArrayList<String>(Arrays.asList(mutedGroups));
 				if (mutedGroupsSet.contains(ignoredGroup)) {
 					ignoringUsers.add(set.getString(1));
 				}
