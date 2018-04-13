3c7d4bfc6c39b45b190b0fd919bf226fffdc3594
iykHvfIvfR <iykHvfIvfR@users.noreply.github.com>
2018-04-13 01:53:47 +0000

    Fix minecart teleporters

--- plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/util/TeleportUtil.java
+++ plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/util/TeleportUtil.java
@@ -1,8 +1,10 @@
 package com.programmerdan.minecraft.simpleadminhacks.util;
 
+import java.util.LinkedList;
 import java.util.logging.Level;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
@@ -12,28 +14,19 @@ import org.bukkit.util.Vector;
 import com.programmerdan.minecraft.simpleadminhacks.SimpleAdminHacks;
 
 public class TeleportUtil {
-
 	private static boolean checkForTeleportSpace(Location loc) {
 		final Block block = loc.getBlock();
-		return !block.getType().isSolid() && 
-				!block.getRelative(BlockFace.UP).getType().isSolid() &&
-				!block.getRelative(BlockFace.UP).getRelative(BlockFace.UP).getType().isSolid() &&
-				!block.getRelative(BlockFace.UP).getRelative(BlockFace.UP).getRelative(BlockFace.UP).getType().isSolid();
-	}
-	
-	private static Location getTeleportLocation(Player player, Location location) {
-		if(location.getY() >= 1) {
-			final Block block = location.getBlock();
-			if(block.getRelative(BlockFace.DOWN).getType().isSolid() && checkForTeleportSpace(location)) {
-				return location;
-			} else {
-				return getTeleportLocation(player, location.add(0, -1, 0));
-			}
-		} else {
-			return null;
+		final Material mat = block.getType();
+		if (mat.isSolid()) {
+			return false;
 		}
+		final Block above = block.getRelative(BlockFace.UP);
+		if (above.getType().isSolid()) {
+			return false;
+		}
+		return true;
 	}
-	
+
 	public static boolean tryToTeleportVertically(Player player, Location location, String reason) {
 		Location loc = location.clone();
 		loc.setX(Math.floor(loc.getX()) + 0.500000D);
@@ -41,28 +34,59 @@ public class TeleportUtil {
 		loc.setZ(Math.floor(loc.getZ()) + 0.500000D);
 		final Location baseLoc = loc.clone();
 		final World world = baseLoc.getWorld();
-		if(loc.getY() > 1) {
-			boolean teleport = checkForTeleportSpace(loc);
-			if(!teleport) {
-				loc.setY(loc.getY() + 1);
-				teleport = checkForTeleportSpace(loc);
-			}
-			if(teleport) {
-				player.setVelocity(new Vector());
-				player.teleport(loc);
-				SimpleAdminHacks.instance().log(Level.INFO, String.format("Player '%s' %s: teleported to %s", player.getName(), reason, loc.toString()));
-				return true;
-			}
+		// Check if teleportation here is viable
+		boolean performTeleport = checkForTeleportSpace(loc);
+		if (!performTeleport) {
+			loc.setY(loc.getY() + 1.000000D);
+			performTeleport = checkForTeleportSpace(loc);
 		}
-		loc = getTeleportLocation(player, baseLoc.add(0, world.getMaxHeight() - baseLoc.getY() - 2, 0));
-		if(loc != null) {
+		if (performTeleport) {
 			player.setVelocity(new Vector());
-			loc.setX(Math.floor(loc.getX()) + 0.500000D);
-	          loc.setY(loc.getY() + 1.02D);
-	          loc.setZ(Math.floor(loc.getZ()) + 0.500000D);
 			player.teleport(loc);
-			SimpleAdminHacks.instance().log(Level.INFO, String.format("Player '%s' %s: teleported to %s", player.getName(), reason, loc.toString()));
+			SimpleAdminHacks.instance().log(String.format(
+				"Player '%s' %s: Teleported to %s",
+				player.getName(), reason, loc.toString()));
 			return true;
+		}
+		loc = baseLoc.clone();
+		// Create a sliding window of block types and track how many of those
+		//  are solid. Keep fetching the block below the current block to move down.
+		int air_count = 0;
+		LinkedList<Material> air_window = new LinkedList<Material>();
+		loc.setY((float)world.getMaxHeight() - 2);
+		Block block = world.getBlockAt(loc);
+		for (int i = 0; i < 4; ++i) {
+			Material block_mat = block.getType();
+			if (!block_mat.isSolid()) {
+				++air_count;
+			}
+			air_window.addLast(block_mat);
+			block = block.getRelative(BlockFace.DOWN);
+		}
+		// Now that the window is prepared, scan down the Y-axis.
+		while (block.getY() >= 1) {
+			Material block_mat = block.getType();
+			if (block_mat.isSolid()) {
+				if (air_count == 4) {
+					player.setVelocity(new Vector());
+					loc = block.getLocation();
+					loc.setX(Math.floor(loc.getX()) + 0.500000D);
+					loc.setY(loc.getY() + 1.02D);
+					loc.setZ(Math.floor(loc.getZ()) + 0.500000D);
+					player.teleport(loc);
+					SimpleAdminHacks.instance().log(String.format(
+						"Player '%s' %s: Teleported to %s",
+						player.getName(), reason, loc.toString()));
+					return true;
+				}
+			} else {
+				++air_count;
+			}
+			air_window.addLast(block_mat);
+			if (!air_window.removeFirst().isSolid()) {
+				--air_count;
+			}
+			block = block.getRelative(BlockFace.DOWN);
 		}
 		return false;
 	}
