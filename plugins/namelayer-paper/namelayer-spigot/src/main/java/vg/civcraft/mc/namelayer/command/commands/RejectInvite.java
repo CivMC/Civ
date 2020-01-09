7f383be9525796154e08154752f8ad664e6b1069
Alexander <52572989+Protonull@users.noreply.github.com>
2020-01-09 03:07:00 +0000

    Fixed RejectInvite

--- plugins/namelayer-paper/namelayer-spigot/src/main/java/vg/civcraft/mc/namelayer/command/commands/RejectInvite.java
+++ plugins/namelayer-paper/namelayer-spigot/src/main/java/vg/civcraft/mc/namelayer/command/commands/RejectInvite.java
@@ -1,5 +1,8 @@
 package vg.civcraft.mc.namelayer.command.commands;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import vg.civcraft.mc.namelayer.GroupManager;
 import vg.civcraft.mc.namelayer.NameAPI;
 import vg.civcraft.mc.namelayer.command.PlayerCommandMiddle;
@@ -19,27 +22,26 @@ public class RejectInvite extends PlayerCommandMiddle {
 		setDescription("Reject an invitation to a group.");
 		setUsage("/nlrg <group>");
 		setArguments(1,1);
+		setSenderMustBePlayer(true);
 	}
 
 	@Override
 	public boolean execute(CommandSender sender, String[] args) {
-		if (!(sender instanceof Player)){
-			sender.sendMessage(ChatColor.YELLOW + "You don't have a UUID, sorry.");
-			return true;
-		}
 		Player player = (Player) sender;
 		String groupName = args[0];
-		Group group = gm.getGroup(groupName);
+		Group group = GroupManager.getGroup(groupName);
 		if (groupIsNull(sender, groupName, group)) {
 			return true;
 		}
 		UUID uuid = NameAPI.getUUID(player.getName());
+		// The IDE is highlighting this as a potention NullReferenceException
+		// but this is checked for in the above groupIsNull() call.
 		GroupManager.PlayerType type = group.getInvite(uuid);
-		if (type == null){
+		if (type == null) {
 			player.sendMessage(ChatColor.RED + "You were not invited to that group.");
 			return true;
 		}
-		if (group.isMember(uuid)){
+		if (group.isMember(uuid)) {
 			player.sendMessage(ChatColor.RED + "You cannot reject an invite to a group that you're already a member of.");
 			group.removeInvite(uuid, true);
 			return true;
@@ -53,12 +55,8 @@ public class RejectInvite extends PlayerCommandMiddle {
 
 	@Override
 	public List<String> tabComplete(CommandSender sender, String[] args) {
-		if (!(sender instanceof Player)){
-			return null;
-		}
-		String groupName = args[0];
 		if (args.length > 0) {
-			return InviteTabCompleter.complete(groupName, (Player) sender);
+			return InviteTabCompleter.complete(args[0], (Player) sender);
 		}
 		else {
 			return InviteTabCompleter.complete(null, (Player) sender);
