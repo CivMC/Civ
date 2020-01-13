2476191a5e8f4a24e9fdfa7bfa8c84ebce0512c9
Alexander <52572989+Protonull@users.noreply.github.com>
2020-01-13 15:54:05 +0000

    Unit tests for NBTSerializable

---
+++ plugins/civmodcore-paper/src/test/java/vg/civcraft/mc/civmodcore/serialization/ExampleSerializable.java
@@ -0,0 +1,43 @@
+package vg.civcraft.mc.civmodcore.serialization;
+
+import vg.civcraft.mc.civmodcore.api.NBTCompound;
+
+public class ExampleSerializable implements NBTSerializable {
+
+	private String message;
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	/**
+	 * Serialises a class into an NBTCompound.
+	 *
+	 * @param nbt The NBTCompound to serialise into, which <i>should</i> NEVER be null, so feel free to throw an
+	 * {@link NBTSerializationException} if it is. You can generally assume that the nbt compound is new and therefore
+	 * empty, but you may wish to check or manually {@link NBTCompound#clear() empty it}, though the latter may cause
+	 * other issues.
+	 * @throws NBTSerializationException This is thrown if the implementation has a fatal error serializing.
+	 */
+	@Override
+	public void serialize(NBTCompound nbt) throws NBTSerializationException {
+		nbt.setString("message", this.message);
+	}
+
+	/**
+	 * Serialises a class into an NBTCompound.
+	 *
+	 * @param nbt The NBTCompound to deserialize from, which <i>should</i> NEVER be null, so feel free to throw an
+	 * {@link NBTSerializationException} if it is.
+	 * @throws NBTSerializationException This is thrown if the implementation has a fatal error deserializing.
+	 */
+	@Override
+	public void deserialize(NBTCompound nbt) throws NBTSerializationException {
+		this.message = nbt.getString("message");
+	}
+
+}
