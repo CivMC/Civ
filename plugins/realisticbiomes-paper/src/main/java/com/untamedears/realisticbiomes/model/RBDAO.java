de6353b126c2cfb2cf631bfcf3553610ccfb331d
Maxopoly <maxopoly3@gmail.com>
2020-03-24 19:11:24 +0100

    Implement data migration

--- plugins/realisticbiomes-paper/src/main/java/com/untamedears/realisticbiomes/model/RBDAO.java
+++ plugins/realisticbiomes-paper/src/main/java/com/untamedears/realisticbiomes/model/RBDAO.java
@@ -7,6 +7,9 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.Callable;
 import java.util.function.Consumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -14,12 +17,17 @@ import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import com.untamedears.realisticbiomes.PlantLogicManager;
 import com.untamedears.realisticbiomes.RealisticBiomes;
 
+import vg.civcraft.mc.citadel.Citadel;
+import vg.civcraft.mc.citadel.reinforcementtypes.ReinforcementType;
+import vg.civcraft.mc.civmodcore.CivModCorePlugin;
 import vg.civcraft.mc.civmodcore.dao.ManagedDatasource;
 import vg.civcraft.mc.civmodcore.locations.chunkmeta.ChunkCoord;
+import vg.civcraft.mc.civmodcore.locations.chunkmeta.GlobalChunkMetaManager;
 import vg.civcraft.mc.civmodcore.locations.chunkmeta.block.BlockBasedChunkMeta;
 import vg.civcraft.mc.civmodcore.locations.chunkmeta.block.table.TableBasedBlockChunkMeta;
 import vg.civcraft.mc.civmodcore.locations.chunkmeta.block.table.TableStorageEngine;
@@ -108,13 +116,63 @@ public class RBDAO extends TableStorageEngine<Plant> {
 
 	@Override
 	public void registerMigrations() {
-		// TODO migrate old data
-		db.registerMigration(2, false,
-				"create table rb_plants (chunk_x int not null, chunk_z int not null, world_id smallint unsigned not null, "
-						+ "x_offset tinyint unsigned not null, y tinyint unsigned not null, z_offset tinyint unsigned not null,"
-						+ "creation_time timestamp not null default now(), index plantChunkLookUp(chunk_x, chunk_z, world_id),"
-						+ "index plantCoordLookUp (x_offset, y, z_offset, world_id), "
-						+ "constraint plantUniqueLocation unique (x_offset,y,z_offset,world_id));");
+		db.registerMigration(2, false, new Callable<Boolean>() {
+
+			@Override
+			public Boolean call() throws Exception {
+				try (Connection insertConn = db.getConnection();
+						PreparedStatement selectPlant = insertConn.prepareStatement("select x,y,z,w,date from rb_plant");
+						ResultSet rs = selectPlant.executeQuery()) {
+					PreparedStatement insertPlant = insertConn.prepareStatement(
+							"insert into rb_plants (chunk_x, chunk_z, world_id, x_offset, y, z_offset, creation_time) "
+									+ "values(?,?,?, ?,?,?, ?);");
+					try (PreparedStatement deleteExisting = insertConn
+							.prepareStatement("delete from rb_plants")) {
+						// in case this migration failed before some of the data might already have
+						// migrated, which we want to undo
+						deleteExisting.execute();
+					}
+
+					GlobalChunkMetaManager worldMan = CivModCorePlugin.getInstance().getChunkMetaManager();
+					int batchCounter = 0;
+					while (rs.next()) {
+						int xCoord = rs.getInt(1);
+						int yCoord = rs.getInt(2);
+						int zCoord = rs.getInt(3);
+						int worldId = rs.getInt(4);
+						long timeStamp = rs.getInt(5);
+						
+						byte x = (byte) ((xCoord % 16) + 16);
+						byte y = (byte) yCoord;
+						byte z = (byte) ((yCoord % 16) + 16);
+						int chunkX = xCoord / 16;
+						int chunkZ = zCoord / 16;
+						World world = Bukkit.getWorlds().get(worldId);
+						short worldID = worldMan.getInternalWorldIdByName(world.getName());
+
+						insertPlant.setInt(1, chunkX);
+						insertPlant.setInt(2, chunkZ);
+						insertPlant.setShort(3, worldID);
+						insertPlant.setByte(4, x);
+						insertPlant.setByte(5, y);
+						insertPlant.setByte(6, z);
+						insertPlant.setTimestamp(7, new Timestamp(timeStamp));
+						insertPlant.addBatch();
+						if (batchCounter > 100) {
+							batchCounter = 0;
+							insertPlant.executeBatch();
+						}
+						batchCounter++;
+					}
+					insertPlant.executeBatch();
+				}
+				return true;
+			}
+		}, "create table rb_plants (chunk_x int not null, chunk_z int not null, world_id smallint unsigned not null, "
+				+ "x_offset tinyint unsigned not null, y tinyint unsigned not null, z_offset tinyint unsigned not null,"
+				+ "creation_time timestamp not null default now(), index plantChunkLookUp(chunk_x, chunk_z, world_id),"
+				+ "index plantCoordLookUp (x_offset, y, z_offset, world_id), "
+				+ "constraint plantUniqueLocation unique (x_offset,y,z_offset,world_id));");
 	}
 
 	@Override
