15976b795dbcecb05aa158035cc4bb6031c300cc
Brandon Gous <fierchecer@gmail.com>
2020-04-04 16:21:16 +0100

    Implement DestroyVehicle representations

--- plugins/jukealert-paper/src/main/java/com/untamedears/jukealert/model/actions/impl/DestroyVehicleAction.java
+++ plugins/jukealert-paper/src/main/java/com/untamedears/jukealert/model/actions/impl/DestroyVehicleAction.java
@@ -2,6 +2,8 @@ package com.untamedears.jukealert.model.actions.impl;
 
 import java.util.UUID;
 
+import com.untamedears.jukealert.util.JAUtility;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 
@@ -9,7 +11,10 @@ import com.untamedears.jukealert.model.Snitch;
 import com.untamedears.jukealert.model.actions.abstr.LoggablePlayerVictimAction;
 
 import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import vg.civcraft.mc.civmodcore.inventorygui.IClickable;
+import vg.civcraft.mc.namelayer.NameAPI;
 
 public class DestroyVehicleAction extends LoggablePlayerVictimAction {
 	
@@ -21,14 +26,19 @@ public class DestroyVehicleAction extends LoggablePlayerVictimAction {
 
 	@Override
 	public IClickable getGUIRepresentation() {
-		// TODO Auto-generated method stub
-		return null;
+		ItemStack is = new ItemStack(getVehicle());
+		ItemMeta itemMeta = is.getItemMeta();
+		itemMeta.setDisplayName(ChatColor.GOLD + "Break");
+		is.setItemMeta(itemMeta);
+		super.enrichGUIItem(is);
+		return new DecorationStack(is);
 	}
 
 	@Override
 	public TextComponent getChatRepresentation(Location reference) {
-		// TODO Auto-generated method stub
-		return null;
+		return new TextComponent(String.format("%Break  %s%s  %s%s %s%s", ChatColor.GOLD, ChatColor.GREEN,
+				NameAPI.getCurrentName(getPlayer()), ChatColor.AQUA, getVehicle().toString(), ChatColor.YELLOW,
+				JAUtility.formatLocation(location, false)));
 	}
 	
 	/**
