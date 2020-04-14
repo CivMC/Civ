b6ace3ffde750ee878d8e85ae14814de17eaefe2
Brandon <fierchecer@gmail.com>
2020-04-15 00:03:40 +0100

    Specify mount/dismount entity name in GUI representation

--- plugins/jukealert-paper/src/main/java/com/untamedears/jukealert/model/actions/impl/MountEntityAction.java
+++ plugins/jukealert-paper/src/main/java/com/untamedears/jukealert/model/actions/impl/MountEntityAction.java
@@ -13,6 +13,7 @@ import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import vg.civcraft.mc.civmodcore.api.ItemAPI;
 import vg.civcraft.mc.civmodcore.inventorygui.DecorationStack;
 import vg.civcraft.mc.civmodcore.inventorygui.IClickable;
 import vg.civcraft.mc.namelayer.NameAPI;
@@ -28,9 +29,7 @@ public class MountEntityAction extends LoggablePlayerVictimAction {
 	@Override
 	public IClickable getGUIRepresentation() {
 		ItemStack is = new ItemStack(Material.SADDLE);
-		ItemMeta itemMeta = is.getItemMeta();
-		itemMeta.setDisplayName(ChatColor.GOLD + "Mounted");
-		is.setItemMeta(itemMeta);
+		ItemAPI.setDisplayName(is,String.format("%Mounted %s%s", ChatColor.GOLD, ChatColor.AQUA, getVictim()));
 		super.enrichGUIItem(is);
 		return new DecorationStack(is);
 	}
