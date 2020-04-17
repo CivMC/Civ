ba523dc0ad0a39c8c8c296ba62093fd994767eed
Alexander <protonull@protonmail.com>
2020-04-18 00:56:15 +0100

    BlockAPI - Removed deprecated but kept convenience APIs

--- plugins/civmodcore-paper/src/main/java/vg/civcraft/mc/civmodcore/api/BlockAPI.java
+++ plugins/civmodcore-paper/src/main/java/vg/civcraft/mc/civmodcore/api/BlockAPI.java
@@ -1,14 +1,12 @@
 package vg.civcraft.mc.civmodcore.api;
 
 import com.google.common.collect.ImmutableList;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Predicate;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -36,8 +34,6 @@ public final class BlockAPI {
 			BlockFace.SOUTH,
 			BlockFace.WEST,
 			BlockFace.EAST);
-
-	private static final Predicate<BlockFace> TRUE = f -> true;
 
 	/**
 	 * Checks whether this block is valid and so can be handled reasonably without error.
@@ -94,6 +90,26 @@ public final class BlockAPI {
 	}
 
 	/**
+	 * Returns a map of all the block's relatives.
+	 *
+	 * @param block The block to get all the relatives of.
+	 * @return Returns an immutable map of all the block's relatives.
+	 */
+	public static Map<BlockFace, Block> getAllSides(Block block) {
+		return getBlockSides(block, ALL_SIDES);
+	}
+
+	/**
+	 * Returns a map of all the block's planar relatives.
+	 *
+	 * @param block The block to get the planar relatives of.
+	 * @return Returns an immutable map of all the block's planar relatives.
+	 */
+	public static Map<BlockFace, Block> getPlanarSides(Block block) {
+		return getBlockSides(block, PLANAR_SIDES);
+	}
+
+	/**
 	 * Attempts to get the other block of a double chest.
 	 *
 	 * @param chest The block that represents the double chest block you already have.
@@ -115,95 +131,6 @@ public final class BlockAPI {
 			return null;
 		}
 		return other.getBlock();
-	}
-
-	// ------------------------------------------------------------
-	// Deprecated
-	// ------------------------------------------------------------
-
-	/**
-	 * Get all blocks on specified faces of the block, where the face also matches the filter.
-	 *
-	 * @param block the block to get surrounding blocks of
-	 * @param faces the block faces to get relative to the block
-	 * @param filter the filter the block faces must matches
-	 * @return the resulting blocks
-	 */
-	@Deprecated
-	public static List<Block> getBlockSides(Block block, List<BlockFace> faces, Predicate<BlockFace> filter) {
-		List<Block> blocks = new ArrayList<>(faces.size());
-		for (BlockFace face : faces) {
-			if (filter.test(face)) {
-				blocks.add(block.getRelative(face));
-			}
-		}
-		return blocks;
-	}
-
-	/**
-	 * Gets all blocks on every cardinal side to the block, where the block face matches the filter
-	 *
-	 * @param block the block
-	 * @param filter the filter to check block faces against
-	 * @return all blocks on every cardinal side of the block where the relative block face matches the filter
-	 */
-	@Deprecated
-	public static List<Block> getPlanarSides(Block block, Predicate<BlockFace> filter) {
-		return getBlockSides(block, PLANAR_SIDES, filter);
-	}
-
-	/**
-	 * Get all blocks on every cardinal side to the specified block
-	 *
-	 * @param block the block
-	 * @return blocks on every cardinal side
-	 */
-	@Deprecated
-	public static List<Block> getPlanarSides(Block block) {
-		return getPlanarSides(block, TRUE);
-	}
-
-	/**
-	 * Gets all blocks next to the block, where the block face matches the filter
-	 *
-	 * @param block the block
-	 * @param filter the filter to check block faces against
-	 * @return all blocks next to the specified block where the relative block face matches the filter
-	 */
-	@Deprecated
-	public static List<Block> getAllSides(Block block, Predicate<BlockFace> filter) {
-		return getBlockSides(block, ALL_SIDES, filter);
-	}
-
-	/**
-	 * Gets all blocks next to the specified block
-	 *
-	 * @param block the block
-	 * @return all blocks next to the block
-	 */
-	@Deprecated
-	public static List<Block> getAllSides(Block block) {
-		return getAllSides(block, TRUE);
-	}
-
-	/**
-	 * Returns a mutable list of all sides of a block
-	 *
-	 * @return all sides of a block: north, east, south, west, up down
-	 */
-	@Deprecated
-	public static List<BlockFace> getAllSides() {
-		return new ArrayList<>(ALL_SIDES);
-	}
-
-	/**
-	 * Returns a mutable list of all planar sides of a block
-	 *
-	 * @return all planar sides of a block: north, east, west, south
-	 */
-	@Deprecated
-	public static List<BlockFace> getPlanarSides() {
-		return new ArrayList<>(PLANAR_SIDES);
 	}
 
 }
