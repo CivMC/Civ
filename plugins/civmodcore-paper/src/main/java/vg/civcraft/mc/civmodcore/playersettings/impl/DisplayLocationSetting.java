a8e136702ace880f6edea339d880dc06ba9a3994
Maxopoly <maxopoly3@gmail.com>
2020-04-17 06:46:21 +0200

    Implement cool setting stuff

---
+++ plugins/civmodcore-paper/src/main/java/vg/civcraft/mc/civmodcore/playersettings/impl/DisplayLocationSetting.java
@@ -0,0 +1,89 @@
+package vg.civcraft.mc.civmodcore.playersettings.impl;
+
+import java.util.Arrays;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import vg.civcraft.mc.civmodcore.api.ItemAPI;
+import vg.civcraft.mc.civmodcore.inventorygui.IClickable;
+import vg.civcraft.mc.civmodcore.inventorygui.LClickable;
+import vg.civcraft.mc.civmodcore.inventorygui.MultiPageView;
+import vg.civcraft.mc.civmodcore.playersettings.gui.MenuSection;
+
+public class DisplayLocationSetting extends LimitedStringSetting {
+
+	private String displayName;
+
+	public DisplayLocationSetting(JavaPlugin plugin, DisplayLocation defaultValue, String name, String identifier,
+			ItemStack gui, String displayName) {
+		super(plugin, defaultValue.toString(), name, identifier, gui, "Set where to display " + displayName,
+				Arrays.stream(DisplayLocation.values()).map(DisplayLocation::toString).collect(Collectors.toList()),
+				false);
+		this.displayName = displayName;
+	}
+	
+	public boolean showOnSidebar(UUID uuid) {
+		DisplayLocation location = DisplayLocation.valueOf(getValue(uuid));
+		return location == DisplayLocation.SIDEBAR || location == DisplayLocation.BOTH;
+	}
+	
+	public boolean showOnActionbar(UUID uuid) {
+		DisplayLocation location = DisplayLocation.valueOf(getValue(uuid));
+		return location == DisplayLocation.ACTIONBAR || location == DisplayLocation.BOTH;
+	}
+	
+	public DisplayLocation getDisplayLocation(UUID uuid) {
+		return DisplayLocation.valueOf(getValue(uuid));
+	}
+
+	@Override
+	public void handleMenuClick(Player player, MenuSection menu) {
+		IClickable sideClick = genLocationClick(Material.YELLOW_BANNER, "%sShow %s only on side bar",
+				DisplayLocation.SIDEBAR, menu);
+		IClickable actionClick = genLocationClick(Material.YELLOW_BANNER, "%sShow %s only on action bar",
+				DisplayLocation.SIDEBAR, menu);
+		IClickable bothClick = genLocationClick(Material.YELLOW_BANNER, "%sShow %s both on action and side bar",
+				DisplayLocation.SIDEBAR, menu);
+		IClickable noneClick = genLocationClick(Material.YELLOW_BANNER, "%sShow %s neither on side bar, nor action bar",
+				DisplayLocation.SIDEBAR, menu);
+		MultiPageView selector = new MultiPageView(player, Arrays.asList(sideClick, actionClick, bothClick, noneClick),
+				"Select where to show " + displayName, true);
+		selector.showScreen();
+	}
+
+	private IClickable genLocationClick(Material mat, String infoText, DisplayLocation location, MenuSection menu) {
+		ItemStack sideStack = new ItemStack(mat);
+		ItemAPI.setDisplayName(sideStack, String.format(infoText, ChatColor.GOLD, displayName));
+		return new LClickable(sideStack, p -> {
+			setValue(p, location.toString());
+			menu.showScreen(p);
+		});
+	}
+
+	public enum DisplayLocation {
+		SIDEBAR, ACTIONBAR, BOTH, NONE;
+
+		public static DisplayLocation fromString(String s) {
+			switch (s.toUpperCase()) {
+			case "SIDEBAR":
+				return SIDEBAR;
+			case "ACTIONBAR":
+				return ACTIONBAR;
+			case "BOTH":
+				return BOTH;
+			case "NONE":
+			case "NEITHER":
+				return NONE;
+			default:
+				return null;
+			}
+		}
+	}
+
+}
