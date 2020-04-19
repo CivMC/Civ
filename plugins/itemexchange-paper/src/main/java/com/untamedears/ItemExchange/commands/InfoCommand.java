c64512a621c6634ee54ede0a29adc05252c611c0
Alexander <protonull@protonmail.com>
2020-04-19 23:44:25 +0100

    Minor command changes

--- plugins/itemexchange-paper/src/main/java/com/untamedears/ItemExchange/commands/InfoCommand.java
+++ plugins/itemexchange-paper/src/main/java/com/untamedears/ItemExchange/commands/InfoCommand.java
@@ -1,7 +1,7 @@
 package com.untamedears.itemexchange.commands;
 
 import co.aikar.commands.annotation.CommandAlias;
-import co.aikar.commands.annotation.Default;
+import co.aikar.commands.annotation.Description;
 import co.aikar.commands.annotation.Subcommand;
 import com.untamedears.itemexchange.ItemExchangePlugin;
 import org.bukkit.ChatColor;
@@ -12,12 +12,8 @@ import vg.civcraft.mc.civmodcore.command.AikarCommand;
 @CommandAlias("iei|ieinfo")
 public class InfoCommand extends AikarCommand {
 
-	@Default
-	public void onDefault() {
-
-	}
-
 	@Subcommand("shop|shopblocks")
+	@Description("Shows what blocks can be made into shops.")
 	public void onShopBlocksInfo(CommandSender sender) {
 		if (ItemExchangePlugin.SHOP_BLOCKS.isEmpty()) {
 			sender.sendMessage(ChatColor.GOLD + "ItemExchange has no configured shop blocks.");
@@ -31,6 +27,7 @@ public class InfoCommand extends AikarCommand {
 	}
 
 	@Subcommand("button|successbutton")
+	@Description("Shows what shop blocks can trigger a successful transaction button.")
 	public void onSuccessButtonInfo(CommandSender sender) {
 		if (ItemExchangePlugin.SUCCESS_BUTTON_BLOCKS.isEmpty()) {
 			sender.sendMessage(ChatColor.GOLD + "ItemExchange has no configured success button shop blocks.");
@@ -44,6 +41,7 @@ public class InfoCommand extends AikarCommand {
 	}
 
 	@Subcommand("rule|ruleitem")
+	@Description("Shows what material exchange rules will be made out of.")
 	public void onRuleItemInfo(CommandSender sender) {
 		sender.sendMessage(ChatColor.GOLD + "ItemExchange exchange rules will be made from: " + ChatColor.WHITE +
 				ItemExchangePlugin.RULE_ITEM.getType());
