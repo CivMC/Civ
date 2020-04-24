696e396ac48cd154c9aeabd52bfdb6aafe196f3e
Maxopoly <maxopoly3@gmail.com>
2020-04-24 23:35:17 +0200

    Adapt to LuckPerms 5.0

--- plugins/kirabukkitgateway-paper/src/main/java/com/github/maxopoly/KiraBukkitGateway/impersonation/KiraLuckPermsWrapper.java
+++ plugins/kirabukkitgateway-paper/src/main/java/com/github/maxopoly/KiraBukkitGateway/impersonation/KiraLuckPermsWrapper.java
@@ -2,24 +2,32 @@ package com.github.maxopoly.KiraBukkitGateway.impersonation;
 
 import java.util.UUID;
 
-import me.lucko.luckperms.LuckPerms;
-import me.lucko.luckperms.api.LuckPermsApi;
-import me.lucko.luckperms.api.User;
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.cacheddata.CachedPermissionData;
+import net.luckperms.api.context.ContextManager;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.query.QueryOptions;
+import net.luckperms.api.util.Tristate;
 
 public class KiraLuckPermsWrapper {
 	
-	private LuckPermsApi api;
+	private LuckPerms api;
 	
 	public KiraLuckPermsWrapper() {
-		api = LuckPerms.getApi();
+		api = LuckPermsProvider.get();
 	}
 	
 	public boolean hasPermission(UUID uuid, String permission) {
-		User user = api.getUser(uuid);
+		User user = api.getUserManager().getUser(uuid);
 		if (user == null) {
 			return false;
 		}
-		return user.hasPermission(api.buildNode(permission).build()).asBoolean();
+		ContextManager cm = api.getContextManager();
+		QueryOptions queryOptions = cm.getQueryOptions(user).orElse(cm.getStaticQueryOptions());
+		CachedPermissionData permissionData = user.getCachedData().getPermissionData(queryOptions);
+		Tristate checkResult = permissionData.checkPermission(permission);
+		return checkResult.asBoolean();
 	}
 
 }
