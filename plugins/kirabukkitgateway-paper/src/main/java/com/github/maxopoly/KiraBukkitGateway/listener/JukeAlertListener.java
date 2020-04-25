fbc35527e3fb137f99e6b927ca6a873663b232e8
Maxopoly <maxopoly3@gmail.com>
2020-04-25 19:24:41 +0200

    Dont show allied on snitches in discord

--- plugins/kirabukkitgateway-paper/src/main/java/com/github/maxopoly/KiraBukkitGateway/listener/JukeAlertListener.java
+++ plugins/kirabukkitgateway-paper/src/main/java/com/github/maxopoly/KiraBukkitGateway/listener/JukeAlertListener.java
@@ -1,5 +1,6 @@
 package com.github.maxopoly.KiraBukkitGateway.listener;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -7,11 +8,16 @@ import com.github.maxopoly.KiraBukkitGateway.KiraBukkitGatewayPlugin;
 import com.untamedears.jukealert.events.PlayerHitSnitchEvent;
 import com.untamedears.jukealert.events.PlayerLoginSnitchEvent;
 import com.untamedears.jukealert.events.PlayerLogoutSnitchEvent;
+import com.untamedears.jukealert.model.Snitch;
+import com.untamedears.jukealert.util.JukeAlertPermissionHandler;
 
 public class JukeAlertListener implements Listener {
 
 	@EventHandler
 	public void enter(PlayerHitSnitchEvent e) {
+		if (immune(e.getSnitch(), e.getPlayer())) {
+			return;
+		}
 		KiraBukkitGatewayPlugin.getInstance().getRabbit().sendSnitchHit(e.getPlayer(), e.getSnitch().getLocation(),
 				e.getSnitch().getName(), e.getSnitch().getGroup().getName(), SnitchHitType.ENTER,
 				e.getSnitch().getType().getName());
@@ -19,6 +25,9 @@ public class JukeAlertListener implements Listener {
 
 	@EventHandler
 	public void login(PlayerLoginSnitchEvent e) {
+		if (immune(e.getSnitch(), e.getPlayer())) {
+			return;
+		}
 		KiraBukkitGatewayPlugin.getInstance().getRabbit().sendSnitchHit(e.getPlayer(), e.getSnitch().getLocation(),
 				e.getSnitch().getName(), e.getSnitch().getGroup().getName(), SnitchHitType.LOGIN,
 				e.getSnitch().getType().getName());
@@ -27,9 +36,16 @@ public class JukeAlertListener implements Listener {
 
 	@EventHandler
 	public void login(PlayerLogoutSnitchEvent e) {
+		if (immune(e.getSnitch(), e.getPlayer())) {
+			return;
+		}
 		KiraBukkitGatewayPlugin.getInstance().getRabbit().sendSnitchHit(e.getPlayer(), e.getSnitch().getLocation(),
 				e.getSnitch().getName(), e.getSnitch().getGroup().getName(), SnitchHitType.LOGOUT,
 				e.getSnitch().getType().getName());
+	}
+	
+	private static boolean immune(Snitch snitch, Player p) {
+		return snitch.hasPermission(p, JukeAlertPermissionHandler.getSnitchImmune());
 	}
 
 }
