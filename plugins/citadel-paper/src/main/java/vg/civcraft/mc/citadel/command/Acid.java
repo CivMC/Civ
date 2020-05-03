87c78c63dc9a6869a0e64623b9f2b1dee285a757
Cola <vaultslave@gmail.com>
2020-05-03 23:28:19 +0100

    Fix Acid blocks
    Add isSolid check to iterator
    Return acid block reinforcement material

--- plugins/citadel-paper/src/main/java/vg/civcraft/mc/citadel/command/Acid.java
+++ plugins/citadel-paper/src/main/java/vg/civcraft/mc/citadel/command/Acid.java
@@ -1,20 +1,12 @@
 package vg.civcraft.mc.citadel.command;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.util.BlockIterator;
-
 import vg.civcraft.mc.citadel.Citadel;
 import vg.civcraft.mc.citadel.CitadelPermissionHandler;
 import vg.civcraft.mc.citadel.CitadelUtility;
@@ -27,6 +19,12 @@ import vg.civcraft.mc.civmodcore.command.CivCommand;
 import vg.civcraft.mc.civmodcore.command.StandaloneCommand;
 import vg.civcraft.mc.civmodcore.util.TextUtil;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+
 @CivCommand(id = "ctacid")
 public class Acid extends StandaloneCommand {
 
@@ -37,7 +35,7 @@ public class Acid extends StandaloneCommand {
 		AcidManager acidMan = Citadel.getInstance().getAcidManager();
 		while (itr.hasNext()) {
 			Block block = itr.next();
-			if (MaterialAPI.isAir(block.getType())) {
+			if (MaterialAPI.isAir(block.getType()) || !(block.getType().isSolid())) {
 				continue;
 			}
 			if (!acidMan.isPossibleAcidBlock(block)) {
@@ -93,6 +91,7 @@ public class Acid extends StandaloneCommand {
 			// play particles for top block
 			ReinforcementLogic.damageReinforcement(topRein, topRein.getHealth() + 1, p);
 			block.breakNaturally();
+			p.getWorld().dropItemNaturally(reinforcement.getLocation(), reinforcement.getType().getItem());
 			topFace.breakNaturally();
 		}
 		return true;
