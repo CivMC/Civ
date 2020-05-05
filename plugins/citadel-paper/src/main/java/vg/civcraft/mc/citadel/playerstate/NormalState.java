ddca63ac51319f4b2312ba9afdf1d14007eaa8c8
Maxopoly <maxopoly3@gmail.com>
2020-05-06 00:45:56 +0200

    Implement cte by hitting stuff

--- plugins/citadel-paper/src/main/java/vg/civcraft/mc/citadel/playerstate/NormalState.java
+++ plugins/citadel-paper/src/main/java/vg/civcraft/mc/citadel/playerstate/NormalState.java
@@ -1,11 +1,14 @@
 package vg.civcraft.mc.citadel.playerstate;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
 import vg.civcraft.mc.citadel.Citadel;
+import vg.civcraft.mc.citadel.CitadelPermissionHandler;
 import vg.civcraft.mc.citadel.CitadelUtility;
 import vg.civcraft.mc.citadel.reinforcementtypes.ReinforcementType;
 import vg.civcraft.mc.namelayer.GroupManager;
@@ -16,12 +19,11 @@ public class NormalState extends AbstractPlayerState {
 
 	public NormalState(Player p) {
 		super(p);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public String getName() {
-		return "Normal mode";
+		return ChatColor.GREEN + "Normal mode";
 	}
 
 	@Override
@@ -45,7 +47,24 @@ public class NormalState extends AbstractPlayerState {
 
 	@Override
 	public void handleInteractBlock(PlayerInteractEvent e) {
-
+		if (e.getAction() != Action.LEFT_CLICK_BLOCK && e.getAction() != Action.RIGHT_CLICK_BLOCK) {
+			return;
+		}
+		Player player = e.getPlayer();
+		if (!Citadel.getInstance().getSettingManager().isInEasyMode(player.getUniqueId())) {
+			return;
+		}
+		ItemStack hand = player.getInventory().getItemInMainHand();
+		ReinforcementType type = Citadel.getInstance().getReinforcementTypeManager().getByItemStack(hand);
+		if (type == null) {
+			return;
+		}
+		String defaultGroupName = NameAPI.getGroupManager().getDefaultGroup(player.getUniqueId());
+		Group defaultGroup = GroupManager.getGroup(defaultGroupName);
+		if (defaultGroup == null) {
+			return;
+		}
+		CitadelUtility.attemptReinforcementCreation(e.getClickedBlock(), type, defaultGroup, player);
 	}
 
 	@Override
