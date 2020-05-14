ed2040033e26286e4c038eacc0fee3dc23f95ff7
Alexander <protonull@protonmail.com>
2020-05-14 14:38:32 +0100

    Update NBT Serialization tests

--- plugins/civmodcore-paper/src/test/java/vg/civcraft/mc/civmodcore/serialization/Tester.java
+++ plugins/civmodcore-paper/src/test/java/vg/civcraft/mc/civmodcore/serialization/Tester.java
@@ -2,59 +2,97 @@ package vg.civcraft.mc.civmodcore.serialization;
 
 import org.junit.Assert;
 import org.junit.Test;
+import vg.civcraft.mc.civmodcore.util.Validation;
 
 public class Tester {
 
 	@Test
+	public void testStringSerialization() {
+		// Setup
+		String STRING_KEY = "test_string";
+		String expectedString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor " +
+				"incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation " +
+				"ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in " +
+				"voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non " +
+				"proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+		// Process
+		NBTCompound nbt = new NBTCompound();
+		nbt.setString(STRING_KEY, expectedString);
+		// Check
+		Assert.assertEquals(expectedString, nbt.getString(STRING_KEY));
+	}
+
+	@Test
+	public void testStringArraySerialization() {
+		// Setup
+		String STRING_ARRAY_KEY = "test_string_array";
+		String[] expectedStringArray = { "one", "two", null, null, "five" };
+		// Process
+		NBTCompound nbt = new NBTCompound();
+		nbt.setStringArray(STRING_ARRAY_KEY, expectedStringArray);
+		// Check
+		Assert.assertArrayEquals(expectedStringArray, nbt.getStringArray(STRING_ARRAY_KEY));
+	}
+
+	@Test
 	public void testClassSerialization() {
+		// Setup
 		NBTSerialization.registerNBTSerializable(ExampleSerializable.class);
-		String testString = "Hello, World!";
-		// Create an instance of the example serializable and store the example text.
-		ExampleSerializable example = new ExampleSerializable();
-		example.setMessage(testString);
-		// Serialize the class into an NBT compound, which *should* hold the relevant data to convert it back into an
-		// ExampleSerializable, given that it was also registered.
-		NBTCompound nbt = NBTSerialization.serialize(example);
-		// Attempts to create an ExampleSerializable instance based on the NBT data.
-		ExampleSerializable returned = (ExampleSerializable) NBTSerialization.deserialize(nbt);
-		// Tests if the message from the new instance matches that of the serialized class.
-		Assert.assertEquals(returned.getMessage(), example.getMessage());
+		String expectedString = "Turpis tincidunt id aliquet risus feugiat. Donec et odio pellentesque diam " +
+				"volutpat commodo sed egestas. Mattis nunc sed blandit libero volutpat sed. Pellentesque diam " +
+				"volutpat commodo sed egestas egestas fringilla phasellus. Nec feugiat in fermentum posuere urna " +
+				"nec. Urna neque viverra justo nec ultrices dui sapien. Interdum consectetur libero id faucibus " +
+				"nisl tincidunt eget nullam non. Dignissim sodales ut eu sem integer vitae justo eget magna. " +
+				"Amet est placerat in egestas. Pharetra vel turpis nunc eget lorem. Mauris sit amet massa vitae " +
+				"tortor condimentum lacinia quis vel. Faucibus turpis in eu mi bibendum neque egestas congue " +
+				"quisque. Sit amet venenatis urna cursus eget nunc.";
+		// Process
+		ExampleSerializable expected = new ExampleSerializable();
+		expected.setMessage(expectedString);
+		NBTCompound nbt = NBTSerialization.serialize(expected);
+		ExampleSerializable actual = (ExampleSerializable) NBTSerialization.deserialize(nbt);
 		NBTSerialization.clearAllRegistrations();
+		// Check
+		Assert.assertNotNull(actual);
+		Assert.assertEquals(expectedString, actual.getMessage());
 	}
 
 	@Test
 	public void testByteSerialization() {
-		// Define the data that will be set and tested.
-		String testString = "The pen is mightier than the sword.";
-		String testStringKey = "proverb";
-		// Create the NBT object that will be serialized.
-		NBTCompound firstNBT = new NBTCompound();
-		firstNBT.setString(testStringKey, testString);
-		byte[] firstNBTData = NBTCompound.toBytes(firstNBT);
-		// Attempt to recreate an NBT compound from the byte data.
-		NBTCompound secondNBT = NBTCompound.fromBytes(firstNBTData);
-		// Test if the stored data on the newly created compound matches what is expected to be there.
-		Assert.assertNotNull(secondNBT);
-		Assert.assertEquals(secondNBT.getString(testStringKey), testString);
+		// Setup
+		String STRING_KEY = "test_byte";
+		String expectedString = "Ultricies leo integer malesuada nunc vel risus commodo viverra. Fames ac turpis " +
+				"egestas sed tempus urna. Sollicitudin nibh sit amet commodo. Cras sed felis eget velit aliquet " +
+				"sagittis. Convallis tellus id interdum velit laoreet id donec ultrices. Mauris nunc congue nisi " +
+				"vitae suscipit tellus mauris a diam. Leo vel fringilla est ullamcorper. Justo nec ultrices dui " +
+				"sapien eget mi. Nisl vel pretium lectus quam id leo in. Nisi vitae suscipit tellus mauris a diam. " +
+				"Proin fermentum leo vel orci porta non pulvinar. Facilisis magna etiam tempor orci eu lobortis " +
+				"elementum nibh tellus. Aliquet eget sit amet tellus cras adipiscing enim.";
+		// Process
+		NBTCompound nbt = new NBTCompound();
+		nbt.setString(STRING_KEY, expectedString);
+		byte[] nbtData = NBTCompound.toBytes(nbt);
+		NBTCompound actual = NBTCompound.fromBytes(nbtData);
+		// Check
+		Assert.assertTrue(Validation.checkValidity(actual));
+		Assert.assertEquals(expectedString, actual.getString(STRING_KEY));
 	}
 
 	@Test
 	public void testNullSerialization() {
-		// Define the data that will be set and tested.
-		String testStringKey = "first";
-		String testStringArrayKey = "second";
-		String[] testStringArray = { "one", "two", null, null, "five" };
-		// Create the NBT object that will be serialized.
-		NBTCompound firstNBT = new NBTCompound();
-		firstNBT.setString(testStringKey, null);
-		firstNBT.setStringArray(testStringArrayKey, testStringArray);
-		byte[] firstNBTData = NBTCompound.toBytes(firstNBT);
-		// Attempt to recreate an NBT compound from the byte data.
-		NBTCompound secondNBT = NBTCompound.fromBytes(firstNBTData);
-		// Test if the stored data on the newly created compound matches what is expected to be there.
-		Assert.assertNotNull(secondNBT);
-		Assert.assertNull(secondNBT.getString(testStringKey));
-		Assert.assertArrayEquals(secondNBT.getStringArray(testStringArrayKey), testStringArray);
+		// Setup
+		String STRING_KEY = "test_null_string";
+		String STRING_ARRAY_KEY = "test_null_string_array";
+		// Process
+		NBTCompound nbt = new NBTCompound();
+		nbt.setString(STRING_KEY, null);
+		nbt.setStringArray(STRING_ARRAY_KEY, null);
+		byte[] nbtData = NBTCompound.toBytes(nbt);
+		NBTCompound actual = NBTCompound.fromBytes(nbtData);
+		// Check
+		Assert.assertTrue(Validation.checkValidity(actual));
+		Assert.assertNull(actual.getString(STRING_KEY));
+		Assert.assertNull(actual.getStringArray(STRING_ARRAY_KEY));
 	}
 
 }
