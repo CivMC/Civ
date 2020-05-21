1fdd3f924b4915f639246d5d64389450e7dca215
Alexander <protonull@protonmail.com>
2020-05-21 01:12:47 +0100

    Javadoc

--- plugins/civmodcore-paper/src/main/java/vg/civcraft/mc/civmodcore/serialization/NBTSerializationException.java
+++ plugins/civmodcore-paper/src/main/java/vg/civcraft/mc/civmodcore/serialization/NBTSerializationException.java
@@ -1,21 +1,29 @@
 package vg.civcraft.mc.civmodcore.serialization;
 
+/**
+ * Exception to be used within {@link NBTSerializable#serialize(NBTCompound)} and
+ * {@link NBTSerializable#deserialize(NBTCompound)}.
+ */
 public class NBTSerializationException extends RuntimeException {
 
 	private static final long serialVersionUID = 606023177729327630L;
 
+	/** {@inheritDoc} */
 	public NBTSerializationException() {
 		super();
 	}
 
+	/** {@inheritDoc} */
 	public NBTSerializationException(String message) {
 		super(message);
 	}
 
+	/** {@inheritDoc} */
 	public NBTSerializationException(String message, Throwable cause) {
 		super(message, cause);
 	}
 
+	/** {@inheritDoc} */
 	public NBTSerializationException(Throwable cause) {
 		super(cause);
 	}
