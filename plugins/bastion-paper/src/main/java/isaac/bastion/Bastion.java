f84f351280f3477f2b3a31de7cdb4fab1924015b
Kayla <saviorseven11@gmail.com>
2020-05-20 22:13:47 -0700

    Added bastions namelayer permission descriptions.

--- plugins/bastion-paper/src/main/java/isaac/bastion/Bastion.java
+++ plugins/bastion-paper/src/main/java/isaac/bastion/Bastion.java
@@ -160,10 +160,10 @@ public final class Bastion extends ACivMod {
 		adminAndAbove.add(PlayerType.ADMINS);
 		adminAndAbove.add(PlayerType.OWNER);
 
-		PermissionType.registerPermission(Permissions.BASTION_PEARL, memberAndAbove, "Allows a player to throw a pearl within a bastion.");
-		PermissionType.registerPermission(Permissions.BASTION_PLACE, modAndAbove, "Allows a player to place a bastion under this group.", false);
+		PermissionType.registerPermission(Permissions.BASTION_PEARL, memberAndAbove, "Allows a player to throw a pearl into a bastion field.");
+		PermissionType.registerPermission(Permissions.BASTION_PLACE, modAndAbove, "Allows a player to place blocks within a bastion field.", false);
 		PermissionType.registerPermission(Permissions.BASTION_LIST, modAndAbove, "Allows a player to see all bastions under this group.");
-		PermissionType.registerPermission(Permissions.BASTION_MANAGE_GROUPS, adminAndAbove, "Permits a player to change a bastions group.");
+		PermissionType.registerPermission(Permissions.BASTION_MANAGE_GROUPS, adminAndAbove, "Allows linking bastion groups.");
 	}
 
 }
