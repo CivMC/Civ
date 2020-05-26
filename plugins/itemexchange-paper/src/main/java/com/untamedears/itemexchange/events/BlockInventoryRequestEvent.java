7c6528c59685299cf441020ae5ac3ae021e910dc
Alexander <protonull@protonmail.com>
2020-05-27 00:02:08 +0100

    Make block inventory request event cancellable

--- plugins/itemexchange-paper/src/main/java/com/untamedears/itemexchange/events/BlockInventoryRequestEvent.java
+++ plugins/itemexchange-paper/src/main/java/com/untamedears/itemexchange/events/BlockInventoryRequestEvent.java
@@ -7,6 +7,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.BlockInventoryHolder;
@@ -23,13 +24,14 @@ import vg.civcraft.mc.civmodcore.api.BlockAPI;
  * Additionally there may be third party security plugins that do not wish to give the creating player access to that
  * block's inventory, thus this event is used as a bridge.</p>
  */
-public class BlockInventoryRequestEvent extends Event {
+public class BlockInventoryRequestEvent extends Event implements Cancellable {
 
 	private static final HandlerList handlers = new HandlerList();
 
 	private final Block block;
 	private final Player requester;
 	private Inventory inventory;
+	private boolean cancelled;
 
 	private BlockInventoryRequestEvent(Block block, Player requester) {
 		Preconditions.checkArgument(BlockAPI.isValidBlock(block));
@@ -89,6 +91,16 @@ public class BlockInventoryRequestEvent extends Event {
 	 */
 	public void setInventory(Inventory inventory) {
 		this.inventory = inventory;
+	}
+
+	@Override
+	public boolean isCancelled() {
+		return this.cancelled;
+	}
+
+	@Override
+	public void setCancelled(boolean cancelled) {
+		this.cancelled = cancelled;
 	}
 
 	@NotNull
