d049e8925ac5ae2ea43d8091bac28265094cfff4
Alexander <protonull@protonmail.com>
2020-06-20 18:53:34 +0100

    Remove unnecessary javadoc

--- plugins/itemexchange-paper/src/main/java/com/untamedears/itemexchange/rules/modifiers/CustomItemModifier.java
+++ plugins/itemexchange-paper/src/main/java/com/untamedears/itemexchange/rules/modifiers/CustomItemModifier.java
@@ -2,20 +2,16 @@ package com.untamedears.itemexchange.rules.modifiers;
 
 import co.aikar.commands.annotation.CommandAlias;
 import com.untamedears.itemexchange.commands.SetCommand;
-import com.untamedears.itemexchange.rules.interfaces.ExchangeData;
 import com.untamedears.itemexchange.rules.interfaces.Modifier;
 import com.untamedears.itemexchange.rules.interfaces.ModifierData;
 import org.bukkit.ChatColor;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
-import vg.civcraft.mc.civmodcore.api.ItemAPI;
 import vg.civcraft.mc.civmodcore.api.NamespaceAPI;
 import vg.civcraft.mc.civmodcore.custom.items.CustomItems;
 import vg.civcraft.mc.civmodcore.custom.items.ItemCriteria;
 import vg.civcraft.mc.civmodcore.serialization.NBTCompound;
-import vg.civcraft.mc.civmodcore.serialization.NBTSerializationException;
 import vg.civcraft.mc.civmodcore.util.NullCoalescing;
-import vg.civcraft.mc.civmodcore.util.Validation;
 
 @CommandAlias(SetCommand.ALIAS)
 @Modifier(slug = "CUSTOM", order = 0)
@@ -27,13 +23,6 @@ public final class CustomItemModifier extends ModifierData {
 
 	private NamespacedKey key;
 
-	/**
-	 * Constructs a new instance of a modifier.
-	 *
-	 * @param item The item to base this exchange data on. You can assume that the item has passed a
-	 *         {@link ItemAPI#isValidItem(ItemStack)} check.
-	 * @return Returns a new instance of the extended class.
-	 */
 	@Override
 	public CustomItemModifier construct(ItemStack item) {
 		ItemCriteria criteria = CustomItems.findMatch(item);
@@ -45,13 +34,6 @@ public final class CustomItemModifier extends ModifierData {
 		return modifier;
 	}
 
-	/**
-	 * Essentially a renamed {@link ExchangeData#isValid()} to bring more clarity. isValid is still being used
-	 * to maintain usage of {@link Validation#checkValidity(Validation)}. This method is intended to convey
-	 * whether this rule is broken, not whether it should be filtered.
-	 *
-	 * @return Returns true if this rule is broken.
-	 */
 	@Override
 	public boolean isBroken() {
 		if (this.key == null) {
@@ -60,13 +42,6 @@ public final class CustomItemModifier extends ModifierData {
 		return false;
 	}
 
-	/**
-	 * Checks if an arbitrary item conforms to this exchange data's requirements.
-	 *
-	 * @param item The arbitrary item to check. You can assume that the item has passed a
-	 *         {@link ItemAPI#isValidItem(ItemStack)} check.
-	 * @return Returns true if the given item conforms.
-	 */
 	@Override
 	public boolean conforms(ItemStack item) {
 		ItemCriteria criteria = CustomItems.findMatch(item);
@@ -79,35 +54,16 @@ public final class CustomItemModifier extends ModifierData {
 		return true;
 	}
 
-	/**
-	 * Serializes a class into an NBTCompound.
-	 *
-	 * @param nbt The NBTCompound to serialize into, which <i>should</i> NEVER be null, so feel free to throw an
-	 *         {@link NBTSerializationException} if it is. You can generally assume that the nbt compound is new and
-	 *         therefore empty, but you may wish to check or manually {@link NBTCompound#clear() empty it}, though the
-	 *         latter may cause other issues.
-	 * @throws NBTSerializationException This is thrown if the implementation has a fatal error serializing.
-	 */
 	@Override
 	public void serialize(NBTCompound nbt) {
 		nbt.setString(ID_KEY, NamespaceAPI.getString(this.key));
 	}
 
-	/**
-	 * Deserializes a class into an NBTCompound.
-	 *
-	 * @param nbt The NBTCompound to deserialize from, which <i>should</i> NEVER be null, so feel free to throw an
-	 *         {@link NBTSerializationException} if it is.
-	 * @throws NBTSerializationException This is thrown if the implementation has a fatal error deserializing.
-	 */
 	@Override
 	public void deserialize(NBTCompound nbt) {
 		this.key = NamespaceAPI.fromString(nbt.getString(ID_KEY));
 	}
 
-	/**
-	 * @return Returns a new listing, or null.
-	 */
 	@Override
 	public String getDisplayListing() {
 		if (this.key == null) {
