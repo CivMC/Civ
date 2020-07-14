cd778929bd4195f2d19fa8b50f5a0854b268e8f7
specificlanguage <specificlanguage@gmail.com>
2020-07-14 18:55:01 -0400

    Revert "fixed my mistakes when i was rushing"
    This reverts commit f4423337ec4aacaf779356e5409eea54dbfd96b6.

--- plugins/finale-paper/src/main/java/com/github/maxopoly/finale/command/CardinalCommand.java
+++ plugins/finale-paper/src/main/java/com/github/maxopoly/finale/command/CardinalCommand.java
@@ -7,7 +7,7 @@ import org.bukkit.entity.Player;
 import vg.civcraft.mc.civmodcore.command.CivCommand;
 import vg.civcraft.mc.civmodcore.command.StandaloneCommand;
 
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 @CivCommand(id = "cardinal")
@@ -15,48 +15,51 @@ public class CardinalCommand extends StandaloneCommand {
 
 	@Override
 	public boolean execute(CommandSender commandSender, String[] args) {
-		Player player = (Player) commandSender;
-		Location currLocation = player.getLocation();
+		Player p = (Player) commandSender;
+		Location currLocation = p.getLocation();
 
 		if(args.length == 0){
+
 			double newYaw = Math.rint(currLocation.getYaw() / 45) * 45;
-			player.teleport(new Location(player.getWorld(), currLocation.getX(), currLocation.getY(),
+
+			p.teleport(new Location(p.getWorld(), currLocation.getX(), currLocation.getY(),
 					currLocation.getZ(), (float) newYaw, currLocation.getPitch()));
 			return true;
 		}
 
 		String direction = args[0].toUpperCase();
-		Location location = new Location(player.getWorld(), currLocation.getX(), currLocation.getY(), currLocation.getZ());
+		Location l = new Location(p.getWorld(), currLocation.getX(), currLocation.getY(), currLocation.getZ());
 
 		switch(direction){
 			case "UP":
-				location.setPitch(-90f);
+				l.setPitch(-90f);
 				break;
 			case "DOWN":
-				location.setPitch(90f);
+				l.setPitch(90f);
 				break;
 			case "SOUTH": case "180":
-				location.setYaw(0f);
+				l.setYaw(0f);
 				break;
 			case "EAST": case "90":
-				location.setYaw(90f);
+				l.setYaw(-90f);
 				break;
 			case "NORTH": case "0":
-				location.setYaw(180f);
+				l.setYaw(180f);
 				break;
 			case "WEST": case "-90": case "270":
-				location.setYaw(-90f);
+				l.setYaw(90f);
 				break;
 			default:
-				player.sendMessage(ChatColor.RED + "Cardinal direction required. You can also use degrees, or 'up' or 'down'.");
+				p.sendMessage(ChatColor.WHITE + "Cardinal direction required. You can also use degrees, or 'up' or 'down'. B");
 				return true;
 		}
-		player.teleport(location);
+
+		p.teleport(l);
 		return true;
 	}
 
 	@Override
-	public List<String> tabComplete(CommandSender commandSender, String[] args) {
-		return doTabComplete(args[0], Arrays.asList("NORTH","WEST","EAST","SOUTH", "UP","DOWN"), false);
+	public List<String> tabComplete(CommandSender commandSender, String[] strings) {
+		return new LinkedList<>();
 	}
 }
