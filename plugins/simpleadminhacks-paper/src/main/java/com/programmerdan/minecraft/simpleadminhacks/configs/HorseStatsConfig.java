da5802a69307101b15f4d6224dfa5f6e828267f9
Falvyu <falvyu.jaeda@gmail.com>
2020-08-03 01:56:45 +0200

    Added horsestats module

---
+++ plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/configs/HorseStatsConfig.java
@@ -0,0 +1,18 @@
+package com.programmerdan.minecraft.simpleadminhacks.configs;
+
+import com.programmerdan.minecraft.simpleadminhacks.SimpleAdminHacks;
+import com.programmerdan.minecraft.simpleadminhacks.SimpleHackConfig;
+import org.bukkit.configuration.ConfigurationSection;
+
+public class HorseStatsConfig extends SimpleHackConfig {
+
+
+	public HorseStatsConfig(SimpleAdminHacks plugin, ConfigurationSection base) {
+		super(plugin, base);
+	}
+
+	@Override
+	protected void wireup(ConfigurationSection config) {
+
+	}
+}
