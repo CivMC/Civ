abbd21c5587de2a1f8fcb4823cbca3dcee55eda0
BuildTools <squareblob@protonmail.com>
2020-08-09 21:27:13 -0400

    Use bastion material for config icon

--- plugins/bastion-paper/src/main/java/isaac/bastion/utils/BastionSettingManager.java
+++ plugins/bastion-paper/src/main/java/isaac/bastion/utils/BastionSettingManager.java
@@ -3,6 +3,7 @@ package isaac.bastion.utils;
 import java.util.UUID;
 
 import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.inventory.ItemStack;
 
 import isaac.bastion.Bastion;
@@ -23,18 +24,22 @@ public class BastionSettingManager {
 	}
 
 	private void initSettings() {
-		MenuSection menu = PlayerSettingAPI.getMainMenu().createMenuSection("Bastion", "All settings related to Bastion");
+		ConfigurationSection config = Bastion.getPlugin().getConfig().getConfigurationSection("bastions");
+		String bastionType = config.getKeys(false).toArray(new String[0])[0];
+		Material bastionMaterial = Material.getMaterial(config.getConfigurationSection(bastionType).getString("block.material"));
+		MenuSection menu = new MenuSection("Bastion", "All settings related to Bastion", PlayerSettingAPI.getMainMenu(),
+				new ItemStack(bastionMaterial));
 
 		bsiOverlay = new BooleanSetting(Bastion.getPlugin(), true, "Display Bastion Information", "bsiOverlay", "Shows if the block you're standing on is bastioned territory.");
 		showNoBastion = new BooleanSetting(Bastion.getPlugin(), false, "Display if you are not in a bastion field", "showNoBastion", "If enabled, will display Bastion status, even if you are not currently in a bastion field");
-
 		bsiLocation = new DisplayLocationSetting(Bastion.getPlugin(), DisplayLocationSetting.DisplayLocation.SIDEBAR, "BSI Location", "bsiLocation", new ItemStack(Material.ARROW), "BSI");
 		ignorePlacementWarnings = new BooleanSetting(Bastion.getPlugin(), false, "Ignore placement warnings", "ignorePlacementWarnings", "Show placements warning in chat when placing in a bastion field");
 
-		PlayerSettingAPI.registerSetting(bsiOverlay, menu);
-		PlayerSettingAPI.registerSetting(bsiLocation, menu);
-		PlayerSettingAPI.registerSetting(showNoBastion, menu);
-		PlayerSettingAPI.registerSetting(ignorePlacementWarnings, menu);
+		menu.registerToParentMenu();
+		menu.registerSetting(bsiOverlay);
+		menu.registerSetting(showNoBastion);
+		menu.registerSetting(bsiLocation);
+		menu.registerSetting(ignorePlacementWarnings);
 	}
 
 	public BooleanSetting getBsiOverlay() {
