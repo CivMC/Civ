4770b0d8afad924e95f202d165d9732878d98ca2
Alexander <protonull@protonmail.com>
2020-11-23 16:49:12 +0000

    ColourAPI # Remove fromBukkit method, it's redundant

--- plugins/civmodcore-paper/src/main/java/vg/civcraft/mc/civmodcore/api/ColourAPI.java
+++ plugins/civmodcore-paper/src/main/java/vg/civcraft/mc/civmodcore/api/ColourAPI.java
@@ -42,67 +42,6 @@ public class ColourAPI {
 	}
 
 	/**
-	 * Converts a Bukkit ChatColor to a Bungee ChatColor.
-	 *
-	 * @param colour The Bukkit ChatColour to convert.
-	 * @return The matching Bungee ChatColour, or null.
-	 */
-	public static ChatColor fromBukkit(final org.bukkit.ChatColor colour) {
-		if (colour == null) {
-			return null;
-		}
-		switch (colour) {
-			case BLACK:
-				return ChatColor.BLACK;
-			case DARK_BLUE:
-				return ChatColor.DARK_BLUE;
-			case DARK_GREEN:
-				return ChatColor.DARK_GREEN;
-			case DARK_AQUA:
-				return ChatColor.DARK_AQUA;
-			case DARK_RED:
-				return ChatColor.DARK_RED;
-			case DARK_PURPLE:
-				return ChatColor.DARK_PURPLE;
-			case GOLD:
-				return ChatColor.GOLD;
-			case GRAY:
-				return ChatColor.GRAY;
-			case DARK_GRAY:
-				return ChatColor.DARK_GRAY;
-			case BLUE:
-				return ChatColor.BLUE;
-			case GREEN:
-				return ChatColor.GREEN;
-			case AQUA:
-				return ChatColor.AQUA;
-			case RED:
-				return ChatColor.RED;
-			case LIGHT_PURPLE:
-				return ChatColor.LIGHT_PURPLE;
-			case YELLOW:
-				return ChatColor.YELLOW;
-			case WHITE:
-				return ChatColor.WHITE;
-			case MAGIC:
-				return ChatColor.MAGIC;
-			case BOLD:
-				return ChatColor.BOLD;
-			case STRIKETHROUGH:
-				return ChatColor.STRIKETHROUGH;
-			case UNDERLINE:
-				return ChatColor.UNDERLINE;
-			case ITALIC:
-				return ChatColor.ITALIC;
-			case RESET:
-				return ChatColor.RESET;
-			default:
-				// TODO: Add log out to say the colour hasn't been added
-				return ChatColor.RESET;
-		}
-	}
-
-	/**
 	 * Attempts to collapse an RGB colour to established Minecraft colours.
 	 *
 	 * @param colour The given RGB colour.
