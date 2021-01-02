64b34dfa242322ef08a7e689e6612a0b50a4c5c9
ProgrammerDan <programmerdan@gmail.com>
2021-01-02 17:06:28 -0500

    Whitespace changes from .editorconfig

--- plugins/banstick-paper/src/main/java/com/programmerdan/minecraft/banstick/data/BSExclusions.java
+++ plugins/banstick-paper/src/main/java/com/programmerdan/minecraft/banstick/data/BSExclusions.java
@@ -14,120 +14,122 @@ import java.util.Set;
  */
 public final class BSExclusions {
 
-    private BSPlayer forPlayer;
-    private List<BSExclusion> exclusionList;
+	private BSPlayer forPlayer;
+	private List<BSExclusion> exclusionList;
 
-    private BSExclusions() {
-    }
+	private BSExclusions() {
+	}
 
-    /**
-     * Get the exclusions list for a particular player.
-     * 
-     * @param player The player to retrieve, as a BSPlayer
-     * @return a new BSExclusions for this player.
-     */
-    public static BSExclusions onlyFor(BSPlayer player) {
-        BSExclusions exclusions = new BSExclusions();
-        exclusions.forPlayer = player;
-        return exclusions;
-    }
+	/**
+	 * Get the exclusions list for a particular player.
+	 * 
+	 * @param player The player to retrieve, as a BSPlayer
+	 * @return a new BSExclusions for this player.
+	 */
+	public static BSExclusions onlyFor(BSPlayer player) {
+		BSExclusions exclusions = new BSExclusions();
+		exclusions.forPlayer = player;
+		return exclusions;
+	}
 
-    /**
-     * @return a new list of exclusions (clone)
-     */
-    public List<BSExclusion> getAll() {
-        if (exclusionList == null) {
-            fill();
-        }
-        return new LinkedList<>(exclusionList);
-    }
+	/**
+	 * @return a new list of exclusions (clone)
+	 */
+	public List<BSExclusion> getAll() {
+		if (exclusionList == null) {
+			fill();
+		}
+		return new LinkedList<>(exclusionList);
+	}
 
-    /**
-     * @return Set containing player ids of all players this one has an exclusion with
-     */
-    public Set<Long> getExcludedPlayerIDs() {
-        Set<Long> pids = new HashSet<>();
-        for (BSExclusion excl : getAll()) {
-            pids.add(excl.getFirstPlayerID());
-            pids.add(excl.getSecondPlayerID());
-        }
-        pids.remove(forPlayer.getId());
-        return pids;
-    }
+	/**
+	 * @return Set containing player ids of all players this one has an exclusion
+	 *         with
+	 */
+	public Set<Long> getExcludedPlayerIDs() {
+		Set<Long> pids = new HashSet<>();
+		for (BSExclusion excl : getAll()) {
+			pids.add(excl.getFirstPlayerID());
+			pids.add(excl.getSecondPlayerID());
+		}
+		pids.remove(forPlayer.getId());
+		return pids;
+	}
 
-    /**
-     * @return Set containing all players this one has an exclusion with
-     */
-    public Set<BSPlayer> getExcludedPlayers() {
-        Set<BSPlayer> pids = new HashSet<>();
-        for (BSExclusion excl : getAll()) {
-            pids.add(excl.getFirstPlayer());
-            pids.add(excl.getSecondPlayer());
-        }
-        pids.remove(forPlayer);
-        return pids;
-    }
+	/**
+	 * @return Set containing all players this one has an exclusion with
+	 */
+	public Set<BSPlayer> getExcludedPlayers() {
+		Set<BSPlayer> pids = new HashSet<>();
+		for (BSExclusion excl : getAll()) {
+			pids.add(excl.getFirstPlayer());
+			pids.add(excl.getSecondPlayer());
+		}
+		pids.remove(forPlayer);
+		return pids;
+	}
 
-    public boolean hasExclusionWith(BSPlayer player) {
-        return getExclusionWith(player) != null;
-    }
+	public boolean hasExclusionWith(BSPlayer player) {
+		return getExclusionWith(player) != null;
+	}
 
-    /**
-     * @param player to retrieve mutual exclusions
-     * @return an exclusion that covers given player and this BSExclusions' player, 
-     *     or null if none found.
-     */
-    public BSExclusion getExclusionWith(BSPlayer player) {
-    	if (forPlayer.getId() == player.getId()) {
-    		return null;
-    	}
-        for (BSExclusion excl : getAll()) {
-            if (excl.getFirstPlayerID() == player.getId() || excl.getSecondPlayerID() == player.getId()) {
-                return excl;
-            }
-        }
-        return null;
-    }
+	/**
+	 * @param player to retrieve mutual exclusions
+	 * @return an exclusion that covers given player and this BSExclusions' player,
+	 *         or null if none found.
+	 */
+	public BSExclusion getExclusionWith(BSPlayer player) {
+		if (forPlayer.getId() == player.getId()) {
+			return null;
+		}
+		for (BSExclusion excl : getAll()) {
+			if (excl.getFirstPlayerID() == player.getId() || excl.getSecondPlayerID() == player.getId()) {
+				return excl;
+			}
+		}
+		return null;
+	}
 
-    /**
-     * Removes a particular exclusion from the exclusion list.
-     * @param excl the exclusion to remove. Removes by object reference
-     */
-    public void remove(BSExclusion excl) {
-    	if (exclusionList == null) {
-            fill();
-        }
-        exclusionList.remove(excl);
-    }
+	/**
+	 * Removes a particular exclusion from the exclusion list.
+	 * 
+	 * @param excl the exclusion to remove. Removes by object reference
+	 */
+	public void remove(BSExclusion excl) {
+		if (exclusionList == null) {
+			fill();
+		}
+		exclusionList.remove(excl);
+	}
 
-    /**
-     * Resets and refills the exclusion list.
-     */
-    private void fill() {
-        exclusionList = new ArrayList<>();
-        exclusionList.addAll(BSExclusion.byPlayer(forPlayer).values());
-    }
+	/**
+	 * Resets and refills the exclusion list.
+	 */
+	private void fill() {
+		exclusionList = new ArrayList<>();
+		exclusionList.addAll(BSExclusion.byPlayer(forPlayer).values());
+	}
 
-    /**
-     * Adds a new exclusion to the exclusion list. Does not check if already there.
-     * 
-     * @param excl the exclusion to add. Adds by object reference
-     */
-    public void addNew(BSExclusion excl) {
-        if (exclusionList == null) {
-            fill();
-        }
-        this.exclusionList.add(excl);
+	/**
+	 * Adds a new exclusion to the exclusion list. Does not check if already there.
+	 * 
+	 * @param excl the exclusion to add. Adds by object reference
+	 */
+	public void addNew(BSExclusion excl) {
+		if (exclusionList == null) {
+			fill();
+		}
+		this.exclusionList.add(excl);
 
-    }
+	}
 
-    /**
-     * @return the size of the Exclusions list.
-     */
-    public int getOrdinality() {
-        if (exclusionList == null) {
-        	fill();
-        }
-        return this.exclusionList.size();
-    }
+	/**
+	 * @return the size of the Exclusions list.
+	 */
+	public int getOrdinality() {
+		if (exclusionList == null) {
+			fill();
+		}
+		return this.exclusionList.size();
+	}
 }
