e8aac882ca8983d9e17f00ea7b8b2e5886891ecf
ProgrammerDan <programmerdan@gmail.com>
2021-01-03 00:21:48 -0500

    checkstyles fixes for BSShares.

--- plugins/banstick-paper/src/main/java/com/programmerdan/minecraft/banstick/data/BSShares.java
+++ plugins/banstick-paper/src/main/java/com/programmerdan/minecraft/banstick/data/BSShares.java
@@ -11,18 +11,27 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class BSShares {
-	
-	private BSShares() {}
+/**
+ * BSShares DAO management object.
+ * 
+ * @author <a href="mailto:programmerdan@gmail.com">ProgrammerDan</a>
+ *
+ */
+public final class BSShares {
 	
 	private BSPlayer forPlayer;
-	
 	private List<Long> shareList;
-	
 	private Set<Long> overlaps;
-	
 	private Set<Long> unpardonedList;
 
+	private BSShares() { }
+	
+	/**
+	 * Get BSShares DAO for a particular BSPlayer
+	 * 
+	 * @param player the Player to look at
+	 * @return the BSShares DAO for this player.
+	 */
 	public static BSShares onlyFor(BSPlayer player) {
 		BSShares shares = new BSShares();
 		shares.forPlayer = player;
@@ -32,6 +41,11 @@ public class BSShares {
 		return shares;
 	}
 	
+	/**
+	 * Release a BSShares DAO.
+	 * 
+	 * @param shares the BSShares DAO to release.
+	 */
 	public static void release(BSShares shares) {
 		shares.forPlayer = null;
 		if (shares.shareList != null) {
@@ -46,6 +60,11 @@ public class BSShares {
 		shares.shareList = null;
 	}
 	
+	/**
+	 * Get all Shares related to the configured player.
+	 * 
+	 * @return a List of Shares, which could be empty if none are known.
+	 */
 	public List<BSShare> getAll() {
 		if (shareList == null) {
 			fill();
@@ -62,8 +81,15 @@ public class BSShares {
 		return all;
 	}
 	
+	/**
+	 * Get all shares that are not pardoned.
+	 * 
+	 * @return a List of Shares, which could be empty if all are pardoned.
+	 */
 	public List<BSShare> getUnpardoned() {
-		if (shareList == null) { fill(); }
+		if (shareList == null) {
+			fill();
+		}
 		
 		List<BSShare> unpardoned = new ArrayList<>();
 		
@@ -76,8 +102,15 @@ public class BSShares {
 		return unpardoned;
 	}
 	
+	/**
+	 * Get all players that have shares with the configured player.
+	 * 
+	 * @return a List of Players, which could be empty if none are shared.
+	 */
 	public List<BSPlayer> getSharesWith() {
-		if (shareList == null) { fill(); }
+		if (shareList == null) {
+			fill();
+		}
 		
 		List<BSPlayer> players = new ArrayList<>();
 		if (overlaps != null && !overlaps.isEmpty()) {
@@ -89,22 +122,41 @@ public class BSShares {
 		return players;
 	}
 	
-	public boolean doesShareWith(Long playerId) {
-		if (shareList == null) { fill(); }
-		return overlaps.contains(playerId);
-	}
-	
+	/**
+	 * Get all Shares between this player and the passed player.
+	 * 
+	 * @param player the player to find shares with
+	 * @return a list of shares, might be empty if no shares exist.
+	 */
 	public List<BSShare> getSharesWith(BSPlayer player) {
-		if (shareList == null) { fill(); }
+		if (shareList == null) {
+			fill();
+		}
 		List<BSShare> returns = new ArrayList<>();
-		if (shareList == null || shareList.isEmpty()) return returns;
+		if (shareList == null || shareList.isEmpty()) {
+			return returns;
+		}
 		for (Long id : shareList) {
 			BSShare share = BSShare.byId(id);
-			if (share != null && (player.getId() == share.getFirstPlayer().getId() || player.getId() == share.getSecondPlayer().getId())) {
+			if (share != null && (player.getId() == share.getFirstPlayer().getId()
+					|| player.getId() == share.getSecondPlayer().getId())) {
 				returns.add(share);
 			}
 		}
 		return returns;
+	}
+
+	/**
+	 * Check if we have any overlap with a specific player.
+	 * 
+	 * @param playerId The player to check
+	 * @return true if any share overlaps, false otherwise.
+	 */
+	public boolean doesShareWith(Long playerId) {
+		if (shareList == null) { 
+			fill();
+		}
+		return overlaps.contains(playerId);
 	}
 	
 	private synchronized void fill() {
@@ -112,12 +164,12 @@ public class BSShares {
 			return;
 		}
 		try (Connection connection = BanStickDatabaseHandler.getinstanceData().getConnection();
-				PreparedStatement getIDs = connection.prepareStatement( // Get all ids only, order by create time.
-					"SELECT sid, first_pid, second_pid, pardon FROM bs_share WHERE first_pid = ? OR second_pid = ? ORDER BY create_time;");) {
+				PreparedStatement getIDs = connection.prepareStatement(// Get all ids only, order by create time.
+						"SELECT sid, first_pid, second_pid, pardon FROM bs_share WHERE first_pid = ? OR second_pid = ? ORDER BY create_time;");) {
 			getIDs.setLong(1, forPlayer.getId());
 			getIDs.setLong(2, forPlayer.getId());
 			try (ResultSet rs = getIDs.executeQuery()) {
-				List <Long> localShareList = new ArrayList<>();
+				List<Long> localShareList = new ArrayList<>();
 				overlaps = new HashSet<>();
 				unpardonedList = new HashSet<>();
 				while (rs.next()) {
@@ -144,8 +196,15 @@ public class BSShares {
 		}
 	}
 	
+	/**
+	 * Check if there's any overlap between this Shares and a given session.
+	 * 
+	 * @param overlap The session to check against.
+	 */
 	public void check(BSSession overlap) {
-		if (shareList == null) { fill(); }
+		if (shareList == null) {
+			fill(); 
+		}
 		
 		// looks for all potential overlaps based on IP information
 		// Checks for existing overlaps
@@ -153,9 +212,12 @@ public class BSShares {
 		// If new, create a new share.
 		List<BSSession> allSessions = BSSession.byIP(overlap.getIP());
 		for (BSSession session : allSessions) {
-			if (forPlayer.getId() == session.getPlayer().getId() || session.getId() == overlap.getId()) continue;
+			if (forPlayer.getId() == session.getPlayer().getId()
+					|| session.getId() == overlap.getId()) {
+				continue;
+			}
 			
-			if (!overlaps.contains(session.getPlayer().getId())) {// we know. We only record first overlap.
+			if (!overlaps.contains(session.getPlayer().getId())) { // we know. We only record first overlap.
 				BSShare newOverlap = BSShare.create(overlap, session);
 				if (newOverlap != null) {
 					addNew(newOverlap, session.getPlayer());
@@ -167,8 +229,15 @@ public class BSShares {
 		}
 	}
 	
+	/**
+	 * Adds a new share / player to this BSShare.
+	 * @param share the Share
+	 * @param player the Player
+	 */
 	public void addNew(BSShare share, BSPlayer player) {
-		if (shareList == null) { fill(); }
+		if (shareList == null) { 
+			fill(); 
+		}
 		
 		this.shareList.add(share.getId());
 		this.overlaps.add(player.getId());
@@ -179,18 +248,33 @@ public class BSShares {
 
 	}
 	
+	/**
+	 * @return Determine share ordinality.
+	 */
 	public int shareOrdinality() {
-		if (shareList == null) { fill(); }
+		if (shareList == null) {
+			fill(); 
+		}
 		return shareList.size();
 	}
-	
+
+	/**
+	 * @return Determine the share unpardoned ordinality
+	 */
 	public int unpardonedOrdinality() {
-		if (shareList == null) { fill(); }
+		if (shareList == null) { 
+			fill();
+		}
 		return unpardonedList.size();
 	}
 	
+	/**
+	 * @param share the Share to pardon.
+	 */
 	public void markPardoned(BSShare share) {
-		if (shareList == null) { fill(); }
+		if (shareList == null) {
+			fill();
+		}
 		// Check if a share of this Player
 		// Then check if the Share is actually marked pardoned
 		// Then shift out of unpardoned list if pardoned.
@@ -204,8 +288,13 @@ public class BSShares {
 		}
 	}
 	
+	/**
+	 * @param share the Share to unpardon
+	 */
 	public void markUnpardoned(BSShare share) {
-		if (shareList == null) { fill(); }
+		if (shareList == null) {
+			fill();
+		}
 		// Check if a share of this Player
 		// Then check if Share is actually unpardoned
 		// Then shift into the unpardoned list is unpardoned.
@@ -219,8 +308,13 @@ public class BSShares {
 		}
 	}
 
+	/**
+	 * @return the latest Share
+	 */
 	public BSShare getLatest() {
-		if (shareList == null) { fill(); }
+		if (shareList == null) {
+			fill();
+		}
 		if (!shareList.isEmpty()) {
 			return BSShare.byId(shareList.get(shareList.size() - 1));
 		}
