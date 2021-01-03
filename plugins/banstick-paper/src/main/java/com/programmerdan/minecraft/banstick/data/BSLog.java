e0f0e2e950e19f0808fdc822e75761407f2410f4
ProgrammerDan <programmerdan@gmail.com>
2021-01-02 22:25:28 -0500

    Updates to BSLog based on checkstyle. Some moving around, comments, etc.

--- plugins/banstick-paper/src/main/java/com/programmerdan/minecraft/banstick/data/BSLog.java
+++ plugins/banstick-paper/src/main/java/com/programmerdan/minecraft/banstick/data/BSLog.java
@@ -13,15 +13,23 @@ import org.bukkit.scheduler.BukkitRunnable;
 /**
  * TODO: Make accessors
  *
- * Currently a depositor log for changes to player ban status. Not all bans have internal tracking so this external log ensures that all bans made or removed are tracked.
+ * <p>Currently a depositor log for changes to player ban status. 
+ * Not all bans have internal tracking so this external log ensures 
+ * that all bans made or removed are tracked.
+ * This ONLY works with player bans.
  * 
- * @author ProgrammerDan
+ * @author <a href="mailto:programmerdan@gmail.com">ProgrammerDan</a>
  *
  */
-public class BSLog extends BukkitRunnable{
+public class BSLog extends BukkitRunnable {
 
+	private static ConcurrentLinkedQueue<LogEntry> toSave = new ConcurrentLinkedQueue<>();
+
+	private int maxBatch = 100;
+	private long delay = 100L;
+	private long period = 1200L;
+	
 	public BSLog(FileConfiguration config) {
-		super();
 		config(config.getConfigurationSection("log"));
 	}
 	
@@ -34,10 +42,6 @@ public class BSLog extends BukkitRunnable{
 		period = config.getLong("period", period);
 	}
 	
-	private int maxBatch = 100;
-	private long delay = 100l;
-	private long period = 1200l;
-	
 	public long getDelay() {
 		return delay;
 	}
@@ -46,47 +50,31 @@ public class BSLog extends BukkitRunnable{
 		return period;
 	}
 	
-	enum Action {
-		BAN,
-		UNBAN,
-		CHANGE
-	}
-	
-	static class LogEntry {
-		Action action;
-		long ban;
-		long player;
-		Date time;
-		
-		public LogEntry(Action action, BSPlayer player, BSBan ban) {
-			this.action = action;
-			this.ban = ban.getId();
-			this.player = player.getId();
-			this.time = new Date();
-		}
-	}
-	
-	private static ConcurrentLinkedQueue<LogEntry> toSave = new ConcurrentLinkedQueue<>();
-
 	@Override
 	public void run() {
 		// drain the queue into the DB in batches
-		if (toSave.isEmpty()) return;
+		if (toSave.isEmpty()) {
+			return;
+		}
 		try (Connection connection = BanStickDatabaseHandler.getinstanceData().getConnection();
-				PreparedStatement saveEm = connection.prepareStatement("INSERT INTO bs_ban_log (pid, bid, action) VALUES (?, ?, ?);");) {
-			int cBatch = 0;
-			while (cBatch < this.maxBatch && !toSave.isEmpty()) {
+				PreparedStatement saveEm = connection.prepareStatement(
+						"INSERT INTO bs_ban_log (pid, bid, action) VALUES (?, ?, ?);");) {
+			int curBatch = 0;
+			while (curBatch < this.maxBatch && !toSave.isEmpty()) {
 				LogEntry nextCheck = toSave.poll();
-				if (nextCheck == null) break; // we're somehow empty already
+				if (nextCheck == null) {
+					break; // we're somehow empty already
+				}
 				saveEm.setLong(1, nextCheck.player);
 				saveEm.setLong(2, nextCheck.ban);
 				saveEm.setString(3, nextCheck.action.toString());
 				saveEm.addBatch();
-				cBatch ++;
+				curBatch ++;
 			}
 			int[] batchRun = saveEm.executeBatch();
-			if (batchRun.length != cBatch) {
-				BanStick.getPlugin().severe("Some elements of the log batch didn't save? " + cBatch + " vs " + batchRun.length);
+			if (batchRun.length != curBatch) {
+				BanStick.getPlugin().severe("Some elements of the log batch didn't save? "
+						+ curBatch + " vs " + batchRun.length);
 			} else {
 				BanStick.getPlugin().debug("Log batch: {0} saves", batchRun.length);
 			}
@@ -96,22 +84,67 @@ public class BSLog extends BukkitRunnable{
 		}
 	}
 
+	/**
+	 * Register a new log entry to eventually be committed.
+	 * 
+	 * @param action the nature of the log -- BAN/UNBAN/CHANGE
+	 * @param bsPlayer the BSPlayer whose ban is being updated
+	 * @param bid the Ban (BSBan)
+	 */
 	public static void register(Action action, BSPlayer bsPlayer, BSBan...bid) {
-		switch(action) {
-		case BAN:
-		case UNBAN:
-			if (bid.length > 0) toSave.offer(new LogEntry(action, bsPlayer, bid[0]));
-			break;
-		case CHANGE:
-			if (bid.length > 0) toSave.offer(new LogEntry(Action.UNBAN, bsPlayer, bid[0]));
-			if (bid.length > 1) toSave.offer(new LogEntry(Action.BAN, bsPlayer, bid[1]));
-			break;
+		switch (action) {
+			case BAN:
+			case UNBAN:
+				if (bid.length > 0) {
+					toSave.offer(new LogEntry(action, bsPlayer, bid[0]));
+				}
+				break;
+			case CHANGE:
+				if (bid.length > 0) {
+					toSave.offer(new LogEntry(Action.UNBAN, bsPlayer, bid[0]));
+				}
+				if (bid.length > 1) {
+					toSave.offer(new LogEntry(Action.BAN, bsPlayer, bid[1]));
+				}
+				break;
+			default:
+				break;
 		}
 	}
 	
+	/**
+	 * Attempts to shut down this log, but first attempts to force a clear of all 
+	 * logged entries.
+	 */
 	public void disable() {
 		while (!toSave.isEmpty()) {
 			run();
+		}
+	}
+
+	/**
+	 * Internal enum for type of ban.
+	 */
+	enum Action {
+		BAN,
+		UNBAN,
+		CHANGE
+	}
+	
+	/**
+	 * Internal LogEntry container. On creation just strips down to IDs.
+	 */
+	static class LogEntry {
+		Action action;
+		long ban;
+		long player;
+		Date time;
+		
+		LogEntry(Action action, BSPlayer player, BSBan ban) {
+			this.action = action;
+			this.ban = ban.getId();
+			this.player = player.getId();
+			this.time = new Date();
 		}
 	}
 }
