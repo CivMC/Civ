196ff575942a475fed06f87976a683c67f643b4d
Cola <vaultslave@gmail.com>
2021-01-08 18:39:31 +0000

    Add methods
    Still needs work to use parent stats

--- plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/hacks/basic/StriderBreeding.java
+++ plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/hacks/basic/StriderBreeding.java
@@ -4,6 +4,8 @@ import com.programmerdan.minecraft.simpleadminhacks.SimpleAdminHacks;
 import com.programmerdan.minecraft.simpleadminhacks.framework.BasicHack;
 import com.programmerdan.minecraft.simpleadminhacks.framework.BasicHackConfig;
 import java.util.Random;
+import java.util.logging.Level;
+
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.Entity;
@@ -14,20 +16,8 @@ import org.bukkit.event.entity.CreatureSpawnEvent;
 
 public class StriderBreeding extends BasicHack {
 
-	Random random = new Random();
-
 	public StriderBreeding(SimpleAdminHacks plugin, BasicHackConfig config) {
 		super(plugin, config);
-	}
-
-	@Override
-	public void onEnable() {
-
-	}
-
-	@Override
-	public void onDisable() {
-
 	}
 
 	@EventHandler
@@ -35,20 +25,30 @@ public class StriderBreeding extends BasicHack {
 		if (event.getEntity().getType() != EntityType.STRIDER) {
 			return;
 		}
-		rollSpeedStat(event.getEntity());
-		rollHealthStat(event.getEntity());
+		LivingEntity strider = event.getEntity();
+		rollSpeedStat(strider);
+		rollHealthStat(strider);
 	}
 
-	public void rollSpeedStat(Entity strider) {
-		LivingEntity str = (LivingEntity) strider;
-		AttributeInstance moveSpeed = str.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED);
+	public void rollSpeedStat(LivingEntity strider) {
+		AttributeInstance moveSpeed = strider.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED);
 		if (moveSpeed == null) {
 			return;
 		}
-		moveSpeed.setBaseValue(random.nextDouble());
+		Double newValue = Math.random() * (0.3375 - 0.175) + 0.175;
+		moveSpeed.setBaseValue(newValue);
+		plugin.getLogger().log(Level.INFO, "Setting Strider Speed to: " + newValue);
 	}
 
-	public void rollHealthStat(Entity Strider) {
-
+	public void rollHealthStat(LivingEntity strider) {
+		AttributeInstance moveSpeed = strider.getAttribute(Attribute.GENERIC_MAX_HEALTH);
+		if (moveSpeed == null) {
+			return;
+		}
+		int newValue = Math.round((float) Math.random() * (50 - 20) + 20);
+		moveSpeed.setBaseValue(newValue);
+		//We set the health to the new value since when we update the health, they dont auto heal
+		strider.setHealth(newValue);
+		plugin.getLogger().log(Level.INFO, "Setting Strider Health to: " + newValue);
 	}
 }
