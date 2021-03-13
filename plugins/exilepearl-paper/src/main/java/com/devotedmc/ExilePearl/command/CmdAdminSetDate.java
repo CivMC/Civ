32a696e58670a7445f9250bca8ef71e899033f55
Archie <Chrishal01@web.de>
2021-03-13 18:13:15 +0100

    """"FIXES"""" for WING
    I don't AGREE with these changes BUT you REQUESTED them so HERE you GO

--- plugins/exilepearl-paper/src/main/java/com/devotedmc/ExilePearl/command/CmdAdminSetDate.java
+++ plugins/exilepearl-paper/src/main/java/com/devotedmc/ExilePearl/command/CmdAdminSetDate.java
@@ -19,7 +19,7 @@ public class CmdAdminSetDate extends PearlCommand {
 		this.setHelpShort("Sets the creation date of a pearl.");
 
 		this.commandArgs.add(requiredPearlPlayer());
-		this.commandArgs.add(required("date yy/mm/dd", autoTab("")));
+		this.commandArgs.add(required("date yy/mm/dd", autoTab("", "date yy/mm/dd")));
 
 		this.permission = Permission.SET_DATE.node;
 		this.visibility = CommandVisibility.SECRET;
