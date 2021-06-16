2d0ceb777608f4b5ed5d3e7f95c08647fcf1c3ad
Cola <vaultslave@gmail.com>
2021-06-16 11:00:47 +0100

    Update documentation

--- plugins/exilepearl-paper/src/main/java/com/devotedmc/ExilePearl/config/PearlConfig.java
+++ plugins/exilepearl-paper/src/main/java/com/devotedmc/ExilePearl/config/PearlConfig.java
@@ -192,7 +192,8 @@ public interface PearlConfig extends MySqlConfig, DocumentConfig {
 
 	/**
 	 * Gets whether people should be allowed to deploy an elytra with a pearl in their inventory
-	 * @return true if they should be able to fly
+	 * Also determines if they should be able to pick up pearl items while gliding
+	 * @return true if they should be able to fly & pick up exile pearls while gliding
 	 */
 	boolean canElytraWithPearls();
 }
