733c479c79ace9e4f6d44e597a0409f374b2da9f
Brandon <fierchecer@gmail.com>
2021-06-19 19:31:50 +0100

    Update MobCondenser to support per-entity values

--- plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/hacks/basic/MobCondenser.java
+++ plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/hacks/basic/MobCondenser.java
@@ -3,34 +3,62 @@ package com.programmerdan.minecraft.simpleadminhacks.hacks.basic;
 import com.programmerdan.minecraft.simpleadminhacks.SimpleAdminHacks;
 import com.programmerdan.minecraft.simpleadminhacks.framework.BasicHack;
 import com.programmerdan.minecraft.simpleadminhacks.framework.BasicHackConfig;
-import com.programmerdan.minecraft.simpleadminhacks.framework.autoload.AutoLoad;
-import com.programmerdan.minecraft.simpleadminhacks.framework.autoload.DataParser;
-import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
+import vg.civcraft.mc.civmodcore.entities.EntityUtils;
 
+import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class MobCondenser extends BasicHack
 {
 	private Random rng;
-
-	@AutoLoad
-	private double mobSpawnMultiplier;
-
-	@AutoLoad(processor = DataParser.ENTITY_TYPE)
-	private List<EntityType> types;
+	private Map<EntityType, Double> mobSpawnModifiers;
 
 	public MobCondenser(SimpleAdminHacks plugin, BasicHackConfig config)
 	{
 		super(plugin, config);
-		rng = new Random();
+		this.rng = new Random();
+		this.mobSpawnModifiers = new HashMap<>();
+	}
+
+	@Override
+	public void onEnable()
+	{
+		super.onEnable();
+
+		final ConfigurationSection base = this.config.getBase();
+		final ConfigurationSection spawnModifiers = base.getConfigurationSection("mobSpawnModifiers");
+		if (spawnModifiers != null) {
+			for (final String key : spawnModifiers.getKeys(false)) {
+				final EntityType type = EntityUtils.getEntityType(key);
+				if (type == null) {
+					this.plugin.warning("[" + getClass().getSimpleName() + "] EntityType: \"" + key + "\" does not exist, skipping.");
+					continue;
+				}
+				double modifier = spawnModifiers.getDouble(key, 1.0d);
+				if (modifier < 0.0d) {
+					this.plugin.warning("[" + getClass().getSimpleName() + "] Mob Spawn Modifier: \"" + modifier + "\" for \"" + key + "\" is unsupported, defaulting to 1.0");
+					modifier = 1.0d;
+				}
+				this.mobSpawnModifiers.put(type, modifier);
+			}
+		}
+	}
+
+	@Override
+	public void onDisable()
+	{
+		this.mobSpawnModifiers.clear();
+
+		super.onDisable();
 	}
 
 	private boolean roll(double chance) {
@@ -39,8 +67,8 @@ public class MobCondenser extends BasicHack
 
 	@EventHandler(priority = EventPriority.LOW) //Run before the PortalSpawnModifier
 	public void onCreatureSpawn(CreatureSpawnEvent e) {
-		if (types.contains(e.getEntityType())) {
-			if (!roll(mobSpawnMultiplier)) {
+		if (mobSpawnModifiers.containsKey(e.getEntityType())) {
+			if (!roll(mobSpawnModifiers.get(e.getEntityType()))) {
 				e.setCancelled(true);
 				return;
 			}
@@ -49,12 +77,12 @@ public class MobCondenser extends BasicHack
 
 	@EventHandler(priority = EventPriority.HIGH) //Run after PortalSpawnModifier
 	public void onEntityDeath(EntityDeathEvent e) {
-		if (types.contains(e.getEntityType())) {
+		if (mobSpawnModifiers.containsKey(e.getEntityType())) {
 			Iterator<ItemStack> iterator = e.getDrops().iterator();
 
 			while (iterator.hasNext()) {
 				ItemStack itemStack = iterator.next();
-				itemStack.setAmount((int) Math.round(itemStack.getAmount() / mobSpawnMultiplier));
+				itemStack.setAmount((int) Math.round(itemStack.getAmount() / mobSpawnModifiers.get(e.getEntityType())));
 			}
 		}
 	}
