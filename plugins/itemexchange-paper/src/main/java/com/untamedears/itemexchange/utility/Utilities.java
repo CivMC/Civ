6c7637636ac5051dc5d74b2433a5b14af8eeec09
Alexander <protonull@protonmail.com>
2021-07-24 14:31:55 +0100

    Update Utilities

--- plugins/itemexchange-paper/src/main/java/com/untamedears/itemexchange/utility/Utilities.java
+++ plugins/itemexchange-paper/src/main/java/com/untamedears/itemexchange/utility/Utilities.java
@@ -10,6 +10,7 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import javax.annotation.Nonnull;
 import lombok.experimental.ExtensionMethod;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.MapUtils;
@@ -30,7 +31,6 @@ import org.bukkit.potion.PotionEffect;
 import vg.civcraft.mc.civmodcore.inventory.InventoryUtils;
 import vg.civcraft.mc.civmodcore.utilities.JavaExtensions;
 import vg.civcraft.mc.civmodcore.utilities.KeyedUtils;
-import vg.civcraft.mc.civmodcore.utilities.MoreClassUtils;
 import vg.civcraft.mc.civmodcore.utilities.NullUtils;
 import vg.civcraft.mc.civmodcore.world.WorldUtils;
 
@@ -46,17 +46,12 @@ public final class Utilities {
 	 * @param item The item to test.
 	 * @return Returns true if the item is an exchange rule or bulk exchange rule.
 	 */
-	public static boolean isExchangeRule(ItemStack item) {
+	public static boolean isExchangeRule(final ItemStack item) {
 		if (item == null) {
 			return false;
 		}
-		if (ExchangeRule.fromItem(item) != null) {
-			return true;
-		}
-		if (BulkExchangeRule.fromItem(item) != null) {
-			return true;
-		}
-		return false;
+		return ExchangeRule.fromItem(item) != null
+				|| BulkExchangeRule.fromItem(item) != null;
 	}
 
 	/**
@@ -65,7 +60,7 @@ public final class Utilities {
 	 * @param player The player to give the exchange rule to.
 	 * @param rule The exchange rule to give the player.
 	 */
-	public static void givePlayerExchangeRule(Player player, ExchangeRule rule) {
+	public static void givePlayerExchangeRule(final Player player, final ExchangeRule rule) {
 		RuntimeException error = new InvalidCommandArgument("Could not create that rule.");
 		if (player == null || rule == null) {
 			throw error;
@@ -81,13 +76,13 @@ public final class Utilities {
 	 * @param inventory The inventory to give the items to. It must have a location, like a chest inventory.
 	 * @param items The items to give to the inventory.
 	 */
-	public static void giveItemsOrDrop(Inventory inventory, ItemStack... items) {
+	public static void giveItemsOrDrop(@Nonnull final Inventory inventory, @Nonnull final ItemStack... items) {
 		Preconditions.checkArgument(InventoryUtils.isValidInventory(inventory));
 		Preconditions.checkArgument(WorldUtils.isValidLocation(inventory.getLocation()));
 		Preconditions.checkArgument(ArrayUtils.isNotEmpty(items));
+		World world = inventory.getLocation().getWorld();
+		assert world != null;
 		for (Map.Entry<Integer, ItemStack> entry : inventory.addItem(items).entrySet()) {
-			World world = inventory.getLocation().getWorld();
-			assert world != null;
 			world.dropItem(inventory.getLocation(), entry.getValue());
 		}
 	}
@@ -100,30 +95,24 @@ public final class Utilities {
 	 * @param allowUnlistedEnchants Is metaEnchants allowed to include enchantments not included in ruleEnchants?
 	 * @return Returns true if the meta enchantments satisfy the rule enchantments.
 	 */
-	public static boolean conformsRequiresEnchants(Map<Enchantment, Integer> ruleEnchants,
-												   Map<Enchantment, Integer> metaEnchants,
-												   boolean allowUnlistedEnchants) {
+	public static boolean conformsRequiresEnchants(final Map<Enchantment, Integer> ruleEnchants,
+												   final Map<Enchantment, Integer> metaEnchants,
+												   final boolean allowUnlistedEnchants) {
 		if (MapUtils.isEmpty(ruleEnchants)) {
-			if (allowUnlistedEnchants || MapUtils.isEmpty(metaEnchants)) {
-				return true;
-			}
+			return allowUnlistedEnchants || MapUtils.isEmpty(metaEnchants);
+		}
+		if (MapUtils.isEmpty(metaEnchants) || metaEnchants.size() < ruleEnchants.size()) {
 			return false;
 		}
-		if (MapUtils.isEmpty(metaEnchants)) {
+		if (!allowUnlistedEnchants && metaEnchants.size() != ruleEnchants.size()) {
 			return false;
 		}
-		if (allowUnlistedEnchants && metaEnchants.size() < ruleEnchants.size()) {
-			return false;
-		}
-		else if (metaEnchants.size() != ruleEnchants.size()) {
-			return false;
-		}
-		for (Map.Entry<Enchantment, Integer> entry : ruleEnchants.entrySet()) {
-			if (!metaEnchants.containsKey(entry.getKey())) {
+		for (final var ruleEnchant : ruleEnchants.entrySet()) {
+			if (!metaEnchants.containsKey(ruleEnchant.getKey())) {
 				return false;
 			}
-			if (entry.getValue() != ExchangeRule.ANY) {
-				if (!NullUtils.equalsNotNull(metaEnchants.get(entry.getKey()), entry.getValue())) {
+			if (ruleEnchant.getValue() != ExchangeRule.ANY) {
+				if (!NullUtils.equalsNotNull(metaEnchants.get(ruleEnchant.getKey()), ruleEnchant.getValue())) {
 					return false;
 				}
 			}
@@ -139,15 +128,14 @@ public final class Utilities {
 	public static void successfulTransactionButton(Block shop) {
 		Stream.of(shop, WorldUtils.getOtherDoubleChestBlock(shop, true))
 				.filter(WorldUtils::isValidBlock)
-				.filter(block -> ItemExchangeConfig.hasSuccessButtonBlock(block.getType()))
+				.filter((block) -> ItemExchangeConfig.hasSuccessButtonBlock(block.getType()))
 				.distinct()
-				.forEach(block -> {
-					Directional directional = MoreClassUtils.castOrNull(Directional.class, block.getBlockData());
-					if (directional == null) {
+				.forEach((block) -> {
+					if (!(block.getBlockData() instanceof Directional directional)) {
 						return;
 					}
-					BlockFace backFace = directional.getFacing().getOppositeFace();
-					Block behindBlock = block.getRelative(backFace);
+					final BlockFace backFace = directional.getFacing().getOppositeFace();
+					final Block behindBlock = block.getRelative(backFace);
 					if (!WorldUtils.isValidBlock(behindBlock) || !behindBlock.getType().isOccluding()) {
 						return;
 					}
@@ -155,12 +143,11 @@ public final class Utilities {
 						if (face.getOppositeFace() == backFace) {
 							continue;
 						}
-						Block buttonBlock = behindBlock.getRelative(face);
+						final Block buttonBlock = behindBlock.getRelative(face);
 						if (!WorldUtils.isValidBlock(buttonBlock) || !Tag.BUTTONS.isTagged(buttonBlock.getType())) {
 							continue;
 						}
-						Switch button = MoreClassUtils.castOrNull(Switch.class, buttonBlock.getBlockData());
-						if (button == null) {
+						if (!(buttonBlock.getBlockData() instanceof Switch button)) {
 							continue;
 						}
 						if (WorldUtils.getAttachedFace(button) != face.getOppositeFace()) {
@@ -168,11 +155,11 @@ public final class Utilities {
 						}
 						button.setPowered(true);
 						buttonBlock.setBlockData(button);
-						// Wait to depower the block
+						// Wait to de-power the block
 						Bukkit.getScheduler().scheduleSyncDelayedTask(ItemExchangePlugin.getInstance(), () -> {
-							Block newBlock = buttonBlock.getLocation().getBlock(); // Refresh block
-							Switch newButton = MoreClassUtils.castOrNull(Switch.class, newBlock.getBlockData());
-							if (!button.matches(newButton)) {
+							final Block newBlock = buttonBlock.getLocation().getBlock(); // Refresh block
+							if (!(newBlock.getBlockData() instanceof Switch newButton)
+									|| !button.matches(newButton)) {
 								return;
 							}
 							newButton.setPowered(false);
@@ -186,7 +173,7 @@ public final class Utilities {
 	// Stringifiers
 	// ------------------------------------------------------------
 
-	public static String leveledEnchantsToString(Map<Enchantment, Integer> leveledEnchants) {
+	public static String leveledEnchantsToString(final Map<Enchantment, Integer> leveledEnchants) {
 		if (MapUtils.isEmpty(leveledEnchants)) {
 			return "[]";
 		}
@@ -197,7 +184,7 @@ public final class Utilities {
 				"]";
 	}
 
-	public static String enchantsToString(Collection<Enchantment> enchants) {
+	public static String enchantsToString(final Collection<Enchantment> enchants) {
 		if (CollectionUtils.isEmpty(enchants)) {
 			return "[]";
 		}
@@ -208,7 +195,7 @@ public final class Utilities {
 				"]";
 	}
 
-	public static String potionDataToString(PotionData data) {
+	public static String potionDataToString(final PotionData data) {
 		if (data == null) {
 			return null;
 		}
@@ -219,7 +206,7 @@ public final class Utilities {
 				"}";
 	}
 
-	public static String potionEffectsToString(Collection<PotionEffect> effects) {
+	public static String potionEffectsToString(final Collection<PotionEffect> effects) {
 		if (CollectionUtils.isEmpty(effects)) {
 			return "[]";
 		}
