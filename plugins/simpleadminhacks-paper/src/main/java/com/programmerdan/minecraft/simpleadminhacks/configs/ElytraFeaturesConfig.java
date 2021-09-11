a6c700d73f01cf4d31129cf0ce352e43ca237307
Alexander <protonull@protonmail.com>
2021-09-11 10:41:07 +0000

    Expand ElytraFeatures

---
+++ plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/configs/ElytraFeaturesConfig.java
@@ -0,0 +1,98 @@
+package com.programmerdan.minecraft.simpleadminhacks.configs;
+
+import com.programmerdan.minecraft.simpleadminhacks.SimpleAdminHacks;
+import com.programmerdan.minecraft.simpleadminhacks.framework.SimpleHackConfig;
+import javax.annotation.Nonnull;
+import org.bukkit.configuration.ConfigurationSection;
+import vg.civcraft.mc.civmodcore.util.CivLogger;
+
+public class ElytraFeaturesConfig extends SimpleHackConfig {
+	private final CivLogger logger;
+
+	private boolean disableFlight;
+	private boolean disableFlightInCombat;
+
+	private boolean disableFireworkBoosting;
+	private boolean disableFireworkBoostingInCombat;
+	private boolean disableSafeFireworkBoosting;
+
+	private int heightDamage;
+	private boolean heightDamageScaling;
+	private int heightBuffer;
+	private long heightDamageInterval;
+
+	public ElytraFeaturesConfig(@Nonnull final SimpleAdminHacks plugin,
+								@Nonnull final ConfigurationSection base) {
+		super(plugin, base, false);
+		this.logger = CivLogger.getLogger(getClass());
+		wireup(base);
+	}
+
+	@Override
+	protected void wireup(@Nonnull final ConfigurationSection config) {
+		this.disableFlight = config.getBoolean("disableFlight", false);
+
+		this.disableFlightInCombat = config.getBoolean("disableFlightInCombat", false);
+
+		this.disableFireworkBoosting = config.getBoolean("disableFireworkBoosting", false);
+
+		this.disableFireworkBoostingInCombat = config.getBoolean("disableFireworkBoostingInCombat", false);
+
+		this.disableSafeFireworkBoosting = config.getBoolean("disableSafeFireworkBoosting", false);
+
+		this.heightDamage = config.getInt("heightDamage.damage", 1);
+		if (this.heightDamage < 0) {
+			this.logger.warning("[heightDamage.damage] was set to [" + this.heightDamage + "], " +
+					"which is invalid, defaulting to: 1");
+			this.heightDamage = 1;
+		}
+
+		this.heightDamageScaling = config.getBoolean("heightDamage.scales", true);
+
+		this.heightBuffer = config.getInt("heightDamage.buffer", 5);
+
+		this.heightDamageInterval = config.getLong("heightDamage.interval", 1000L);
+		if (this.heightDamageInterval < 1L) {
+			this.logger.warning("[heightDamage.interval] was set to [" + this.heightDamageInterval + "], " +
+					"which is invalid, defaulting to: 1000L");
+			this.heightDamageInterval = 1000L;
+		}
+	}
+
+	public boolean isFlightDisabled() {
+		return this.disableFlight;
+	}
+
+	public boolean isFlightDisabledInCombat() {
+		return this.disableFlightInCombat;
+	}
+
+	public boolean isBoostingDisabled() {
+		return this.disableFireworkBoosting;
+	}
+
+	public boolean isBoostingDisabledInCombat() {
+		return this.disableFireworkBoostingInCombat;
+	}
+
+	public boolean isSafeBoostingDisabled() {
+		return this.disableSafeFireworkBoosting;
+	}
+
+	public int getHeightDamage() {
+		return this.heightDamage;
+	}
+
+	public boolean isHeightDamageScaling() {
+		return this.heightDamageScaling;
+	}
+
+	public int getHeightBuffer() {
+		return this.heightBuffer;
+	}
+
+	public long getHeightDamageInterval() {
+		return this.heightDamageInterval;
+	}
+
+}
