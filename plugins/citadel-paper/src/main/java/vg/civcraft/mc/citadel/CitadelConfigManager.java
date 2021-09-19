89321274c188532d1b8c785c44dff18debf42498
Cola <vaultslave@gmail.com>
2021-09-19 15:34:13 +0100

    Change parseMaterialList usage to account for CMC updates to method
    CMC updates means this method now returns an empty ArrayList vs null

--- plugins/citadel-paper/src/main/java/vg/civcraft/mc/citadel/CitadelConfigManager.java
+++ plugins/citadel-paper/src/main/java/vg/civcraft/mc/citadel/CitadelConfigManager.java
@@ -170,7 +170,7 @@ public class CitadelConfigManager extends ConfigParser {
 			logger.warning("Reinforcement type at " + config.getCurrentPath() + " had no id, it was ignored");
 			return null;
 		}
-		if (reinforceables != null && nonReinforceables != null) {
+		if (!reinforceables.isEmpty() && !nonReinforceables.isEmpty()) {
 			logger.warning("Both blacklist and whitelist specified for reinforcement type at " + config.getCurrentPath()
 					+ ". This does not make sense and the type will be ignored");
 			return null;
