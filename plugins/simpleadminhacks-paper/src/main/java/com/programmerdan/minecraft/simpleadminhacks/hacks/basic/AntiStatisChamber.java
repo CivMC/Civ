d62e375d89edce7a6b1281e1fb353ca7e6834be0
Cola <vaultslave@gmail.com>
2021-09-22 13:07:15 +0100

    Change to PDC & removal based on time in flight

--- plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/hacks/basic/AntiStatisChamber.java
+++ plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/hacks/basic/AntiStatisChamber.java
@@ -1,36 +1,63 @@
 package com.programmerdan.minecraft.simpleadminhacks.hacks.basic;
 
+import com.destroystokyo.paper.event.player.PlayerLaunchProjectileEvent;
 import com.programmerdan.minecraft.simpleadminhacks.SimpleAdminHacks;
 import com.programmerdan.minecraft.simpleadminhacks.framework.BasicHack;
 import com.programmerdan.minecraft.simpleadminhacks.framework.BasicHackConfig;
 import com.programmerdan.minecraft.simpleadminhacks.framework.autoload.AutoLoad;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
-import org.bukkit.Location;
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.EnderPearl;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+import vg.civcraft.mc.civmodcore.util.ConfigParsing;
 
 public class AntiStatisChamber extends BasicHack {
 
 	@AutoLoad
-	private double maxPearlDistance;
+	private String pearlLifetime;
+	private NamespacedKey key;
 
 	public AntiStatisChamber(SimpleAdminHacks plugin, BasicHackConfig config) {
 		super(plugin, config);
+		this.key = new NamespacedKey(plugin, "pearl_thrown_time");
 	}
 
 	@EventHandler
-	public void onPlayerPearl(PlayerTeleportEvent event) {
-		if (event.getCause() != PlayerTeleportEvent.TeleportCause.ENDER_PEARL) {
+	public void onPlayerThrowPearl(PlayerLaunchProjectileEvent event) {
+		Projectile projectile = event.getProjectile();
+		if (!(projectile instanceof EnderPearl)) {
 			return;
 		}
-		Location from = event.getFrom();
-		Location to = event.getTo();
-		double distance = from.distance(to);
-		if (distance > maxPearlDistance) {
-			event.setCancelled(true);
-			event.getPlayer().sendMessage(Component.text("Your pearl was cancelled since it exceeded the maximum teleporting distance of " + maxPearlDistance + "!").color(
-					NamedTextColor.RED));
+		EnderPearl pearl = (EnderPearl) projectile;
+		PersistentDataContainer pdc = pearl.getPersistentDataContainer();
+		pdc.set(key, PersistentDataType.LONG, System.currentTimeMillis());
+	}
+
+	@EventHandler
+	public void onPearlLand(ProjectileHitEvent event) {
+		Projectile projectile = event.getEntity();
+		if (!(projectile instanceof EnderPearl)) {
+			return;
+		}
+		EnderPearl pearl = (EnderPearl) projectile;
+		PersistentDataContainer pdc = pearl.getPersistentDataContainer();
+		Long pearlThrownTime = pdc.get(key, PersistentDataType.LONG);
+		if (pearlThrownTime == null) {
+			pearl.remove();
+			return;
+		}
+		if ((System.currentTimeMillis() - pearlThrownTime) > ConfigParsing.parseTime(pearlLifetime)) {
+			//We remove the pearl here because cancelling the event doesn't prevent the pearl from actually landing
+			pearl.remove();
+			if (pearl.getShooter() instanceof Player) {
+				((Player) pearl.getShooter()).sendMessage(Component.text("Your pearl failed to land for being in flight for more than " + pearlLifetime).color(NamedTextColor.RED));
+			}
 		}
 	}
 }
