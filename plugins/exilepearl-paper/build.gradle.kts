b9f4821c1bbfc6c987a99e662c3af67df260dd08
AngrySoundTech <angrysoundtech@gmail.com>
2021-12-20 17:41:59 -0500

    Use brewery api

--- plugins/exilepearl-paper/build.gradle.kts
+++ plugins/exilepearl-paper/build.gradle.kts
@@ -32,7 +32,6 @@ repositories {
     civRepo("RandomSpawn")
     civRepo("WorldBorder")
     civRepo("CombatTagPlus")
-    civRepo("Brewery")
     civRepo("BanStick")
 
     maven("https://papermc.io/repo/repository/maven-public/")
@@ -50,7 +49,8 @@ dependencies {
     compileOnly("net.civmc:randomspawn:3.1.0-SNAPSHOT:dev")
     compileOnly("net.civmc:worldborder:2.0.0-SNAPSHOT:dev")
     compileOnly("net.civmc:combattagplus:1.5.0-SNAPSHOT:dev")
-    compileOnly("net.civmc:brewery:3.2-SNAPSHOT:dev")
+    compileOnly("com.github.DieReicheErethons:Brewery:3.1")
+
     compileOnly("net.civmc:banstick:2.0.0-SNAPSHOT:dev")
 }
 
