fbc3a3bffffd7b32ee6c3c01935898ebebb56395
AngrySoundTech <angrysoundtech@gmail.com>
2021-12-20 19:08:09 -0500

    Update CTP to 1.18... probably.

--- plugins/combattagplus-paper/CombatTagPlus/src/main/java/net/minelink/ctplus/util/BarUtils.java
+++ plugins/combattagplus-paper/CombatTagPlus/src/main/java/net/minelink/ctplus/util/BarUtils.java
@@ -5,7 +5,6 @@ import me.confuser.barapi.BarAPI;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.connorlinfoot.actionbarapi.ActionBarAPI;
 import com.google.common.base.Preconditions;
 
 import net.minelink.ctplus.CombatTagPlus;
@@ -16,7 +15,6 @@ import org.bukkit.boss.BarStyle;
 import org.bukkit.boss.BossBar;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.inventivetalent.bossbar.BossBarAPI;
 
 public final class BarUtils {
 
@@ -26,12 +24,8 @@ public final class BarUtils {
     public static void init() {
         if (ReflectionUtils.getClass("org.bukkit.boss.BossBar") != null) {
             handler = Handler.NEW_BUKKIT_API;
-        } else if (Bukkit.getPluginManager().isPluginEnabled("ActionBarAPI")) {
-            handler = Handler.ACTIONBAR_API;
         } else if (Bukkit.getPluginManager().isPluginEnabled("BarAPI")) {
             handler = Handler.CONFUSER_BAR_API;
-        } else if (Bukkit.getPluginManager().isPluginEnabled("BossBarAPI")) {
-            handler = Handler.BOSSBAR_API;
         }
     }
 
@@ -102,27 +96,6 @@ public final class BarUtils {
                 }
             }
         },
-        ACTIONBAR_API {
-            @Override
-            public boolean hasBar(Player player) {
-                return false;
-            }
-
-            @Override
-            public void setMessage(Player player, String message, int timeout) {
-                ActionBarAPI.sendActionBar(player, message);
-            }
-
-            @Override
-            public void setMessage(Player player, String message, float percent) {
-                ActionBarAPI.sendActionBar(player, message);
-            }
-
-            @Override
-            public void removeBar(Player player) {
-
-            }
-        },
 
         CONFUSER_BAR_API {
             @Override
@@ -143,29 +116,6 @@ public final class BarUtils {
             @Override
             public void removeBar(Player player) {
                 BarAPI.removeBar(player);
-            }
-        },
-
-        BOSSBAR_API {
-            @Override
-            public boolean hasBar(Player player) {
-                return BossBarAPI.hasBar(player);
-            }
-
-            @Override
-            public void setMessage(Player player, String message, int timeout) {
-                BossBarAPI.setMessage(player, message, 100, timeout);
-            }
-
-            @Override
-            public void setMessage(Player player, String message, float percent) {
-                BossBarAPI.setMessage(player, message);
-                BossBarAPI.setHealth(player, percent);
-            }
-
-            @Override
-            public void removeBar(Player player) {
-                BossBarAPI.removeBar(player);
             }
         };
 
---
+++ plugins/combattagplus-paper/CombatTagPlusCompat-v1_12_R1/build.gradle.kts
@@ -0,0 +1,7 @@
+repositories {
+    maven("https://repo.techcable.net/content/groups/public/")
+}
+
+dependencies {
+    implementation("org.bukkit:craftbukkit:1.12-R0.1-SNAPSHOT")
+}
