9ea1c4953b34cac7caa647508d412329f5bfbecd
AngrySoundTech <angrysoundtech@gmail.com>
2021-12-20 19:16:45 -0500

    Update CTP to 2.0.0-SNAPSHOT

--- plugins/exilepearl-paper/build.gradle.kts
+++ plugins/exilepearl-paper/build.gradle.kts
@@ -48,7 +48,7 @@ dependencies {
     compileOnly("net.civmc:bastion:3.0.0-SNAPSHOT:dev")
     compileOnly("net.civmc:randomspawn:3.0.0-SNAPSHOT:dev")
     compileOnly("net.civmc:worldborder:2.0.0-SNAPSHOT:dev")
-    compileOnly("net.civmc:combattagplus:1.5.0-SNAPSHOT:dev")
+    compileOnly("net.civmc:combattagplus:2.0.0-SNAPSHOT:dev")
     compileOnly("com.github.DieReicheErethons:Brewery:3.1")
 
     compileOnly("net.civmc:banstick:2.0.0-SNAPSHOT:dev")
