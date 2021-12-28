3473fa42942301d3e4c946c8613f651056b16de3
Cola <vaultslave@gmail.com>
2021-12-28 00:54:38 +0000

    Shadow dependencies

--- plugins/banstick-paper/build.gradle.kts
+++ plugins/banstick-paper/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     `java-library`
     `maven-publish`
     id("io.papermc.paperweight.userdev") version "1.3.1"
+    id("com.github.johnrengelman.shadow") version "7.1.0"
 }
 
 group = "net.civmc"
@@ -33,8 +34,8 @@ dependencies {
 
     implementation("com.github.seancfoley:ipaddress:2.0.2")
     implementation("org.jsoup:jsoup:1.13.1")
-    implementation("net.civmc:civmodcore:2.0.0-SNAPSHOT:dev-all")
-    implementation("net.civmc:namelayer-spigot:3.0.0-SNAPSHOT:dev")
+    compileOnly("net.civmc:civmodcore:2.0.0-SNAPSHOT:dev-all")
+    compileOnly("net.civmc:namelayer-spigot:3.0.0-SNAPSHOT:dev")
 }
 
 java {
@@ -63,6 +64,15 @@ tasks {
 	test {
 		useJUnitPlatform()
 	}
+
+	shadowJar {
+    		fun reloc(pkg: String) = relocate(pkg, "net.civmc.banstick.shadow")
+
+    		dependencies {
+    			exclude(dependency("it.unimi.dsi:fastutil"))
+    			exclude(dependency("co.aikar:cleaner"))
+    		}
+    	}
 }
 
 publishing {
