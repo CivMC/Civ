6b62f05868fb10b07c49157817cf0dd11d53ed1c
AngrySoundTech <angrysoundtech@gmail.com>
2021-12-29 13:10:05 -0500

    Use civgradle

---
+++ plugins/citadel-paper/paper/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+	`java-library`
+	id("net.civmc.civgradle.plugin")
+	id("io.papermc.paperweight.userdev") version "1.3.1"
+}
+
+civGradle {
+	paper {
+		pluginName = "Citadel"
+	}
+}
+
+dependencies {
+	paperDevBundle("1.18.1-R0.1-SNAPSHOT")
+
+	compileOnly("net.civmc.civmodcore:paper:2.0.0-SNAPSHOT:dev-all")
+	compileOnly("net.civmc.namelayer:paper:3.0.0-SNAPSHOT:dev")
+	compileOnly("com.gmail.filoghost.holographicdisplays:holographicdisplays-api:2.4.9-SNAPSHOT")
+}
