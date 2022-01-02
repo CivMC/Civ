db51aa33cbdb44062b6cf575769e57e5c4e8af7b
AngrySoundTech <angrysoundtech@gmail.com>
2022-01-02 10:32:17 -0500

    Use new maven repo location

--- plugins/railswitch-paper/paper/build.gradle.kts
+++ plugins/railswitch-paper/paper/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	`java-library`
 	id("net.civmc.civgradle.plugin")
-	id("io.papermc.paperweight.userdev") version "1.3.1"
+	id("io.papermc.paperweight.userdev") version "1.3.3"
 }
 
 civGradle {
@@ -13,7 +13,7 @@ civGradle {
 dependencies {
     paperDevBundle("1.18.1-R0.1-SNAPSHOT")
 
-    implementation("net.civmc:civmodcore:2.0.0-SNAPSHOT:dev-all")
-    implementation("net.civmc:namelayer-spigot:3.0.0-SNAPSHOT:dev")
-    implementation("net.civmc:citadel:5.0.0-SNAPSHOT:dev")
+    implementation("net.civmc.civmodcore:paper:2.0.0-SNAPSHOT:dev-all")
+    implementation("net.civmc.namelayer:paper:3.0.0-SNAPSHOT:dev")
+    implementation("net.civmc.citadel:paper:5.0.0-SNAPSHOT:dev")
 }
