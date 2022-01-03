9889a259880fd683f3d181b537a686a8526ef1b5
Cola <vaultslave@gmail.com>
2022-01-03 22:46:25 +0000

    Nearly there
    Just NuVotifier being a pain

---
+++ plugins/essenceglue-paper/paper/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+	`java-library`
+	id("net.civmc.civgradle.plugin")
+	id("io.papermc.paperweight.userdev") version "1.3.1"
+}
+
+civGradle {
+	paper {
+		pluginName = "EssenceGlue"
+	}
+}
+
+dependencies {
+	paperDevBundle("1.18.1-R0.1-SNAPSHOT")
+
+    compileOnly("net.civmc:civmodcore:2.0.0-SNAPSHOT:dev-all")
+    compileOnly("com.vexsoftware:nuvotifier-bukkit:2.7.3")
+    compileOnly("net.civmc:banstick:2.0.0-SNAPSHOT:dev")
+    compileOnly("net.civmc:exilepearl:2.0.0-SNAPSHOT:dev")
+}
