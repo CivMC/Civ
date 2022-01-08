29c1a8252d4d8210c7b38d064d6248c9cf92365d
AngrySoundTech <angrysoundtech@gmail.com>
2022-01-08 12:05:24 -0500

    Update Dependencies

--- plugins/exilepearl-paper/paper/build.gradle.kts
+++ plugins/exilepearl-paper/paper/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	`java-library`
 	id("net.civmc.civgradle.plugin")
-	id("io.papermc.paperweight.userdev") version "1.3.1"
+	id("io.papermc.paperweight.userdev") version "1.3.3"
 }
 
 civGradle {
@@ -12,14 +12,16 @@ civGradle {
 
 dependencies {
     paperDevBundle("1.18.1-R0.1-SNAPSHOT")
-    compileOnly("net.civmc:civmodcore:2.0.0-SNAPSHOT:dev-all")
-    compileOnly("net.civmc:namelayer-spigot:3.0.0-SNAPSHOT:dev")
-    compileOnly("net.civmc:citadel:5.0.0-SNAPSHOT:dev")
-    compileOnly("net.civmc:civchat2:2.0.0-SNAPSHOT:dev")
-    compileOnly("net.civmc:bastion:3.0.0-SNAPSHOT:dev")
-    compileOnly("net.civmc:randomspawn:3.0.0-SNAPSHOT:dev")
-    compileOnly("net.civmc:worldborder:2.0.0-SNAPSHOT:dev")
-    compileOnly("net.civmc.combattagplus:combattagplus-spigot:2.0.0-SNAPSHOT:dev")
-    compileOnly("com.github.DieReicheErethons:Brewery:3.1")
-    compileOnly("net.civmc:banstick:2.0.0-SNAPSHOT:dev")
+
+    compileOnly("net.civmc.civmodcore:paper:2.0.0-SNAPSHOT:dev-all")
+    compileOnly("net.civmc.namelayer:paper:3.0.0-SNAPSHOT:dev")
+    compileOnly("net.civmc.citadel:paper:5.0.0-SNAPSHOT:dev")
+    compileOnly("net.civmc.civchat2:paper:2.0.0-SNAPSHOT:dev")
+    compileOnly("net.civmc.bastion:paper:3.0.0-SNAPSHOT:dev")
+    compileOnly("net.civmc.combattagplus:paper:2.0.0-SNAPSHOT:dev")
+	compileOnly("net.civmc.banstick:paper:2.0.0-SNAPSHOT:dev")
+	compileOnly("net.civmc:randomspawn:3.0.0-SNAPSHOT:dev")
+	compileOnly("net.civmc:worldborder:2.0.0-SNAPSHOT:dev")
+
+	compileOnly("com.github.DieReicheErethons:Brewery:3.1")
 }
