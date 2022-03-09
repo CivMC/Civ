789a8fd9c9bb8ce37b4e951c1a827299b16ba22f
Cola <vaultslave@gmail.com>
2022-03-09 21:25:48 +0000

    Retrieve the new item ourselves, working

--- plugins/simpleadminhacks-paper/paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/hacks/basic/ArmourBound.java
+++ plugins/simpleadminhacks-paper/paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/hacks/basic/ArmourBound.java
@@ -11,13 +11,15 @@ import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import vg.civcraft.mc.civmodcore.inventory.items.MetaUtils;
 
-import java.util.Set;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 public class ArmourBound extends BasicHack {
@@ -25,18 +27,12 @@ public class ArmourBound extends BasicHack {
 	private SimpleAdminHacks plugin;
 	private NamespacedKey key;
 	@AutoLoad
-	private Set<String> whitelist;
+	private List<String> whitelist;
 
 	public ArmourBound(SimpleAdminHacks plugin, BasicHackConfig config) {
 		super(plugin, config);
 		this.plugin = plugin;
 		this.key = new NamespacedKey(plugin, "SAH_ArmourBound");
-		for (String string : whitelist) {
-			Material material = Material.matchMaterial(string);
-			if (material == null) {
-				this.plugin.getLogger().warning("ArmourBound found " + string + " as a whitelist item but couldn't match it, are you sure this is correct?");
-			}
-		}
 	}
 
 	@EventHandler
@@ -46,7 +42,15 @@ public class ArmourBound extends BasicHack {
 		if (newItem == null) {
 			return;
 		}
-
+		if (newItem.getType() == Material.AIR) {
+			return;
+		}
+		EquipmentSlot realSlot = getRealSlot(event.getSlotType());
+		if (realSlot == null) {
+			return;
+		}
+		//From here, we take over from the copy, get the actual itemstack ourselves
+		newItem = player.getInventory().getItem(realSlot);
 		if (!whitelist.contains(newItem.getType().toString())) {
 			return;
 		}
@@ -65,12 +69,27 @@ public class ArmourBound extends BasicHack {
 	private void addBoundLore(ItemStack equippedItem, Player player) {
 		ItemMeta item = equippedItem.getItemMeta();
 		Component boundComponent = Component.text("This armor is bound to: ").color(NamedTextColor.WHITE).append(Component.text(player.getName()).color(NamedTextColor.GOLD));
-		MetaUtils.addComponentLore(item, boundComponent);
+		item.lore(List.of(boundComponent));
 		equippedItem.setItemMeta(item);
 	}
 
 	private UUID getOrSetOwnerUUID(ItemStack itemStack, Player player) {
 		PersistentDataContainer itemContainer = itemStack.getItemMeta().getPersistentDataContainer();
 		return UUID.fromString(itemContainer.getOrDefault(this.key, PersistentDataType.STRING, player.getUniqueId().toString()));
+	}
+
+	private EquipmentSlot getRealSlot(PlayerArmorChangeEvent.SlotType slotType) {
+		switch (slotType) {
+			case HEAD:
+				return EquipmentSlot.HEAD;
+			case CHEST:
+				return EquipmentSlot.CHEST;
+			case LEGS:
+				return EquipmentSlot.LEGS;
+			case FEET:
+				return EquipmentSlot.FEET;
+			default:
+				return null;
+		}
 	}
 }
