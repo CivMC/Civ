12efd01ac9cd3b9d3f0de1900280d1b1a7770c22
Cola <vaultslave@gmail.com>
2022-04-26 23:44:27 +0100

    Implement method from paper

--- plugins/civmodcore-paper/paper/src/main/java/org/bukkit/pseudo/PseudoServer.java
+++ plugins/civmodcore-paper/paper/src/main/java/org/bukkit/pseudo/PseudoServer.java
@@ -35,6 +35,7 @@ import org.bukkit.Tag;
 import org.bukkit.UnsafeValues;
 import org.bukkit.Warning;
 import org.bukkit.World;
+import org.bukkit.WorldBorder;
 import org.bukkit.WorldCreator;
 import org.bukkit.advancement.Advancement;
 import org.bukkit.block.data.BlockData;
@@ -408,6 +409,11 @@ public class PseudoServer implements Server {
 	@Nullable
 	@Override
 	public World getWorld(@Nonnull final NamespacedKey namespacedKey) {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	public @NotNull WorldBorder createWorldBorder() {
 		throw new NotImplementedException();
 	}
 
@@ -1007,6 +1013,12 @@ public class PseudoServer implements Server {
 	@Override
 	public PlayerProfile createProfile(@Nullable final UUID uuid, @Nullable final String s) {
 		throw new NotImplementedException();
+	}
+
+	@Override
+	public @NotNull PlayerProfile createProfileExact(@org.jetbrains.annotations.Nullable UUID uuid,
+													 @org.jetbrains.annotations.Nullable String name) {
+		return null;
 	}
 
 	@Override
