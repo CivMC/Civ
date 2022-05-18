f6bfaec0ca431ac94f265d9d822f2e2c9e945059
Cola <vaultslave@gmail.com>
2022-05-18 02:29:53 +0100

    Prevent exiled people using portals

--- plugins/exilepearl-paper/paper/src/main/java/com/devotedmc/ExilePearl/listener/PlayerListener.java
+++ plugins/exilepearl-paper/paper/src/main/java/com/devotedmc/ExilePearl/listener/PlayerListener.java
@@ -1223,6 +1223,7 @@ public class PlayerListener implements Listener, Configurable {
 		ExilePearl pearl = pearlApi.getPearl(event.getPlayer().getUniqueId());
 		if(pearl != null && pearl.getPearlType() == PearlType.PRISON && event.getCause() == TeleportCause.END_PORTAL) {
 			event.setTo(SpawnUtil.chooseSpawn(pearlApi.getPearlConfig().getPrisonWorld()));
+			event.getPlayer().sendMessage(Component.text("Pearled players cannot use portals!", NamedTextColor.RED));
 			event.setCancelled(true);
 		}
 	}
