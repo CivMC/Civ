f867cab69b34c8b008fe9ee38930657720866c06
Cola <vaultslave@gmail.com>
2022-05-18 02:22:21 +0100

    Attempt to prevent exile teleporting

--- plugins/exilepearl-paper/paper/src/main/java/com/devotedmc/ExilePearl/listener/PlayerListener.java
+++ plugins/exilepearl-paper/paper/src/main/java/com/devotedmc/ExilePearl/listener/PlayerListener.java
@@ -1218,11 +1218,12 @@ public class PlayerListener implements Listener, Configurable {
 		event.getPlayer().sendMessage(Component.text("You cannot fly with imprisoned players!").color(NamedTextColor.RED));
 	}
 
-	@EventHandler
+	@EventHandler(priority = EventPriority.LOWEST)
 	public void onPlayerPortal(PlayerPortalEvent event) {
 		ExilePearl pearl = pearlApi.getPearl(event.getPlayer().getUniqueId());
 		if(pearl != null && pearl.getPearlType() == PearlType.PRISON && event.getCause() == TeleportCause.END_PORTAL) {
 			event.setTo(SpawnUtil.chooseSpawn(pearlApi.getPearlConfig().getPrisonWorld()));
+			event.setCancelled(true);
 		}
 	}
 
