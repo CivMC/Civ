a53d4b8cc9c79b4591deb97aab00da1c56827c53
AngrySoundTech <angrysoundtech@github.com>
2022-06-05 18:05:46 -0400

    Move plugin to root directory

--- scripts/civgradle/example/spigot/src/main/java/net/civmc/exampleplugin/ExamplePlugin.java
+++
@@ -1,7 +0,0 @@
-package net.civmc.exampleplugin;
-
-import vg.civcraft.mc.civmodcore.ACivMod;
-
-public class ExamplePlugin extends ACivMod {
-
-}
--- scripts/civgradle/settings.gradle.kts
+++ scripts/civgradle/settings.gradle.kts
@@ -1,13 +1,1 @@
 rootProject.name = "civgradle"
-
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        maven("https://papermc.io/repo/repository/maven-public/")
-    }
-}
-
-include(":example")
-include(":example:spigot")
-
-includeBuild("plugin")
