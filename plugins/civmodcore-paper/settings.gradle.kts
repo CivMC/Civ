48df571d530e81c9fe75497a5e0fb0d5acc763c4
AngrySoundTech <angrysoundtech@github.com>
2022-06-05 22:37:21 -0400

    Use CivGradle 2.+

--- plugins/civmodcore-paper/settings.gradle.kts
+++ plugins/civmodcore-paper/settings.gradle.kts
@@ -1,10 +1,12 @@
-rootProject.name = "civmodcore"
-
 pluginManagement {
 	repositories {
 		gradlePluginPortal()
+		maven("https://repo.civmc.net/repository/maven-public/")
 		maven("https://papermc.io/repo/repository/maven-public/")
 	}
 }
 
+rootProject.name = "civmodcore"
+
 include(":paper")
+project(":paper").name = rootProject.name + "-paper"
