c39babb8f724fc224d141be03942a604385b57f0
AngrySoundTech <angrysoundtech@github.com>
2022-06-19 10:00:18 -0400

    Update CMC Version

--- plugins/realisticbiomes-paper/paper/build.gradle.kts
+++ plugins/realisticbiomes-paper/paper/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 dependencies {
 	paperDevBundle("1.18.2-R0.1-SNAPSHOT")
 
-    compileOnly("net.civmc.civmodcore:civmodcore-paper:2.2.0:dev-all")
+    compileOnly("net.civmc.civmodcore:civmodcore-paper:2.3.0:dev-all")
 	compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.2.8")
     implementation("org.apache.commons:commons-math3:3.6.1")
 }
