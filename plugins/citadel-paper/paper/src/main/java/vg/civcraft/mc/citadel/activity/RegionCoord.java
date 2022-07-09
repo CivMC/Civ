51f184fd593b9e60ee7f0f0ea4e273a029c9a7b4
Aleksey Terzi <aleksey.terzi@gmail.com>
2022-07-09 12:39:36 -0600

    Remove equals method from records

--- plugins/citadel-paper/paper/src/main/java/vg/civcraft/mc/citadel/activity/RegionCoord.java
+++ plugins/citadel-paper/paper/src/main/java/vg/civcraft/mc/citadel/activity/RegionCoord.java
@@ -2,14 +2,6 @@ package vg.civcraft.mc.citadel.activity;
 
 record RegionCoord (short worldId, int x, int z) {
 	@Override
-	public boolean equals(Object obj) {
-		return obj instanceof RegionCoord coord
-				&& worldId == coord.worldId
-				&& x == coord.x
-				&& z == coord.z;
-	}
-
-	@Override
 	public int hashCode() {
 		return (int)worldId & 0xffff | (x << 8) | (z << 16);
 	}
--- plugins/citadel-paper/paper/src/main/java/vg/civcraft/mc/citadel/activity/XZKey.java
+++ plugins/citadel-paper/paper/src/main/java/vg/civcraft/mc/citadel/activity/XZKey.java
@@ -2,13 +2,6 @@ package vg.civcraft.mc.citadel.activity;
 
 record XZKey (int x, int z) {
 	@Override
-	public boolean equals(Object obj) {
-		return obj instanceof XZKey key
-				&& x == key.x
-				&& z == key.z;
-	}
-
-	@Override
 	public int hashCode() {
 		return x | (z << 8);
 	}
