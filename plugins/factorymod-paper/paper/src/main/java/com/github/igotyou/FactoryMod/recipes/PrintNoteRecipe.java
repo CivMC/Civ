ae62125b0edc974cfb07b305bda5d7d9cd4ecb70
MelnCat <tnthacker2015@gmail.com>
2022-07-31 19:49:56 -0700

    Prevent replacing color codes in hex colors

--- plugins/factorymod-paper/paper/src/main/java/com/github/igotyou/FactoryMod/recipes/PrintNoteRecipe.java
+++ plugins/factorymod-paper/paper/src/main/java/com/github/igotyou/FactoryMod/recipes/PrintNoteRecipe.java
@@ -95,7 +95,7 @@ public class PrintNoteRecipe extends PrintBookRecipe {
 
 		for (String line : lines) {
 			fixedLines.add(ChatColor.GRAY + line
-					.replaceAll(ChatColor.BLACK.toString(), ChatColor.GRAY.toString())
+					.replaceAll("(?<!§x(§[\\da-f]){0,5})" + ChatColor.BLACK, ChatColor.GRAY.toString())
 					.replaceAll(ChatColor.RESET.toString(), ChatColor.GRAY.toString()));
 		}
 
