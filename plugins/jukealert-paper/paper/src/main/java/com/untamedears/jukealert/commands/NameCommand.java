bc9954f744d7d0d99b6433ec620f30f170827c36
awoo-civ <17804892+awoo-civ@users.noreply.github.com>
2022-08-11 19:57:09 +0000

    Revert support for remote naming via /janame

--- plugins/jukealert-paper/paper/src/main/java/com/untamedears/jukealert/commands/NameCommand.java
+++ plugins/jukealert-paper/paper/src/main/java/com/untamedears/jukealert/commands/NameCommand.java
@@ -2,7 +2,6 @@ package com.untamedears.jukealert.commands;
 
 import static com.untamedears.jukealert.util.JAUtility.findLookingAtOrClosestSnitch;
 
-
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.Description;
@@ -12,10 +11,7 @@ import com.untamedears.jukealert.model.Snitch;
 import com.untamedears.jukealert.util.JAUtility;
 import com.untamedears.jukealert.util.JukeAlertPermissionHandler;
 import net.md_5.bungee.api.chat.TextComponent;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.entity.Player;
 import vg.civcraft.mc.namelayer.permission.PermissionType;
 
@@ -32,40 +28,6 @@ public class NameCommand extends BaseCommand {
 		}
 
 		renameSnitch(player, snitchName, snitch);
-	}
-
-	@CommandAlias("janame")
-	@Syntax("<world> <x> <y> <z> <name>")
-	@Description("Name a snitch")
-	public void execute(Player player, String worldName, int x, int y, int z, String snitchName) {
-		World world = Bukkit.getWorld(worldName);
-		if (world == null) {
-			player.sendMessage(ChatColor.RED + "Invalid world.");
-			return;
-		}
-
-		Location location = new Location(world, x, y, z);
-		renameSnitch(player, snitchName, location);
-	}
-
-	@CommandAlias("janame")
-	@Syntax("<x> <y> <z> <name>")
-	@Description("Name a snitch")
-	public void execute(Player player, int x, int y, int z, String snitchName) {
-		World world = player.getLocation().getWorld();
-		Location location = new Location(world, x, y, z);
-		renameSnitch(player, snitchName, location);
-	}
-
-	private static void renameSnitch(Player player, String name, Location location) {
-		Snitch snitch = JukeAlert.getInstance().getSnitchManager().getSnitchAt(location);
-		if (snitch == null || !snitch.hasPermission(player, getPermission())) {
-			player.sendMessage(
-					ChatColor.RED + "You do not own a snitch at those coordinates or lack permission to rename it!");
-			return;
-		}
-
-		renameSnitch(player, name, snitch);
 	}
 
 	private static void renameSnitch(Player player, String name, Snitch snitch) {
