745416e7b1f26500c7711a34d6a7eac4899bfc47
Diet Cola <Diet-Cola@users.noreply.github.com>
2022-08-11 18:25:26 +0100

    Lowest priority when setting scoreboard

--- plugins/civmodcore-paper/paper/src/main/java/vg/civcraft/mc/civmodcore/players/scoreboard/side/ScoreBoardListener.java
+++ plugins/civmodcore-paper/paper/src/main/java/vg/civcraft/mc/civmodcore/players/scoreboard/side/ScoreBoardListener.java
@@ -2,12 +2,13 @@ package vg.civcraft.mc.civmodcore.players.scoreboard.side;
 
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class ScoreBoardListener implements Listener {
 	
-	@EventHandler
+	@EventHandler(priority = EventPriority.LOWEST)
 	public void join(PlayerJoinEvent e) {
 		e.getPlayer().setScoreboard(Bukkit.getScoreboardManager().getNewScoreboard());
 	}
