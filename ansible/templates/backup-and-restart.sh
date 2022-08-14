660ab975737320246f1b84b71098065c798de32d
AngrySoundTech <angrysoundtech@github.com>
2022-08-14 15:02:40 -0400

    Shorten waits

--- ansible/templates/backup-and-restart.sh
+++ ansible/templates/backup-and-restart.sh
@@ -5,7 +5,7 @@ docker service scale minecraft_waterfall=0
 sleep 1m
 docker service scale minecraft_paper=0
 docker service scale minecraft_queue=0
-sleep 20m
+sleep 10m
 
 echo "$(date) Starting backup..."
 export AWS_ACCESS_KEY_ID={{secret.backup.s3_access_key_id}}
@@ -20,7 +20,7 @@ restic \
 echo "$(date) Starting services after backup..."
 docker service scale minecraft_paper=1
 docker service scale minecraft_queue=1
-sleep 10m
+sleep 5m
 docker service scale minecraft_waterfall=1
 
 echo "$(date) Copying backup to longterm..."
