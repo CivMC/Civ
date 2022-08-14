d722caf1cee9c65ab9d255bc617070265bc4321b
AngrySoundTech <angrysoundtech@github.com>
2022-08-14 15:00:24 -0400

    Use shortterm backups as an intermediate

--- ansible/templates/backup-and-restart.sh
+++ ansible/templates/backup-and-restart.sh
@@ -12,7 +12,7 @@ export AWS_ACCESS_KEY_ID={{secret.backup.s3_access_key_id}}
 export AWS_SECRET_ACCESS_KEY={{secret.backup.s3_access_key}}
 export RESTIC_PASSWORD={{secret.backup.restic_password}}
 restic \
-  -r {{secret.backup.restic_repo}} backup \
+  -r {{secret.backup.restic_shortterm_repo}} backup \
   /opt/stacks/minecraft/ \
   --exclude '**orebfuscator_cache'\
   --exclude '**dynmap'
@@ -22,5 +22,18 @@ docker service scale minecraft_paper=1
 docker service scale minecraft_queue=1
 sleep 10m
 docker service scale minecraft_waterfall=1
+
+echo "$(date) Copying backup to longterm..."
+restic \
+  -r {{secret.backup.restic_shortterm_repo}} copy \
+  --repo2 {{secret.backup.restic_repo}} \
+  latest
+
+echo "$(date) Pruning shortterm backups..."
+restic \
+  -r {{secret.backup.restic_shortterm_repo}} copy \
+  forget \
+  --keep-last 3 \
+  --prune
 
 echo "$(date) Backup finished!"
