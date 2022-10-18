5c705501d1ba0bf49e110bb99fb4665d6d84e654
Cola <vaultslave@gmail.com>
2022-10-18 15:55:26 +0100

    CivGradle 2 + Compiling

--- plugins/randomspawn-paper/paper/build.gradle.kts
+++ plugins/randomspawn-paper/paper/build.gradle.kts
@@ -1,18 +1,11 @@
 plugins {
-	`java-library`
-	id("net.civmc.civgradle.plugin")
-	id("io.papermc.paperweight.userdev") version "1.3.1"
-}
-
-civGradle {
-	paper {
-		pluginName = "RandomSpawn"
-	}
+    id("net.civmc.civgradle")
+    id("io.papermc.paperweight.userdev")
 }
 
 dependencies {
-    paperDevBundle("1.18.1-R0.1-SNAPSHOT")
-    compileOnly("net.civmc:civmodcore:2.0.0-SNAPSHOT:dev-all")
+    paperDevBundle("1.18.2-R0.1-SNAPSHOT")
+    compileOnly("net.civmc.civmodcore:paper:2.0.0-SNAPSHOT:dev-all")
     compileOnly("net.civmc:worldborder:2.0.0-SNAPSHOT:dev")
     compileOnly("net.civmc:bastion:3.0.0-SNAPSHOT:dev")
     compileOnly("net.civmc:banstick:2.0.0-SNAPSHOT:dev")
