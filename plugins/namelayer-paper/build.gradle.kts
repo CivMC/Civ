fec590904421820c967f34f353cb36067b722955
AngrySoundTech <angrysoundtech@gmail.com>
2023-02-25 17:55:46 -0500

    CivGradle2

--- plugins/namelayer-paper/build.gradle.kts
+++ plugins/namelayer-paper/build.gradle.kts
@@ -1,57 +1,21 @@
-import net.civmc.civgradle.common.util.civRepo
+import net.civmc.civgradle.CivGradleExtension
 
 plugins {
-	`java-library`
-	`maven-publish`
-	id("net.civmc.civgradle.plugin") version "1.0.0-SNAPSHOT"
-}
-
-// Temporary hack:
-// Remove the root build directory
-gradle.buildFinished {
-	project.buildDir.deleteRecursively()
-}
-
-allprojects {
-	group = "net.civmc.namelayer"
-	version = "3.0.0-SNAPSHOT"
-	description = "NameLayer"
+	id("net.civmc.civgradle") version "2.+" apply false
 }
 
 subprojects {
-	apply(plugin = "net.civmc.civgradle.plugin")
+	apply(plugin = "net.civmc.civgradle")
 	apply(plugin = "java-library")
 	apply(plugin = "maven-publish")
 
-	java {
-		toolchain {
-			languageVersion.set(JavaLanguageVersion.of(17))
-		}
+	configure<CivGradleExtension> {
+		pluginName = project.property("pluginName") as String
 	}
 
 	repositories {
 		mavenCentral()
-
-		maven("https://papermc.io/repo/repository/maven-public/")
-
-		civRepo("CivMC/CivModCore")
-	}
-
-	publishing {
-		repositories {
-			maven {
-				name = "GitHubPackages"
-				url = uri("https://maven.pkg.github.com/CivMC/NameLayer")
-				credentials {
-					username = System.getenv("GITHUB_ACTOR")
-					password = System.getenv("GITHUB_TOKEN")
-				}
-			}
-		}
-		publications {
-			register<MavenPublication>("gpr") {
-				from(components["java"])
-			}
-		}
+		maven("https://repo.civmc.net/repository/maven-public")
+		maven("https://jitpack.io")
 	}
 }
--- plugins/namelayer-paper/settings.gradle.kts
+++ plugins/namelayer-paper/settings.gradle.kts
@@ -1,17 +1,12 @@
-rootProject.name = "namelayer"
-
 pluginManagement {
 	repositories {
 		gradlePluginPortal()
+		maven("https://repo.civmc.net/repository/maven-public/")
 		maven("https://papermc.io/repo/repository/maven-public/")
-		maven {
-			url = uri("https://maven.pkg.github.com/CivMC/CivGradle")
-			credentials {
-				username = System.getenv("GITHUB_ACTOR")
-				password = System.getenv("GITHUB_TOKEN")
-			}
-		}
 	}
 }
 
-include("paper")
+rootProject.name = "randomspawn"
+
+include(":paper")
+project(":paper").name = rootProject.name + "-paper"
