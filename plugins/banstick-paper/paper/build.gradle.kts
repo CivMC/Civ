6049375ee682ec4aa27a5979c559dfc7cd9fdeca
AngrySoundTech <angrysoundtech@gmail.com>
2023-02-26 10:46:35 -0500

    Update civgradle

--- plugins/banstick-paper/paper/build.gradle.kts
+++ plugins/banstick-paper/paper/build.gradle.kts
@@ -1,21 +1,13 @@
 plugins {
-	`java-library`
-	id("net.civmc.civgradle.plugin")
-	id("io.papermc.paperweight.userdev") version "1.3.1"
-	id("com.github.johnrengelman.shadow") version "7.1.0"
-}
-
-civGradle {
-	paper {
-		pluginName = "BanStick"
-	}
+	id("io.papermc.paperweight.userdev")
+	id("com.github.johnrengelman.shadow")
 }
 
 dependencies {
-	paperDevBundle("1.18.1-R0.1-SNAPSHOT")
+	paperDevBundle("1.18.2-R0.1-SNAPSHOT")
 
-	compileOnly("net.civmc.civmodcore:paper:2.0.0-SNAPSHOT:dev-all")
-	compileOnly("net.civmc.namelayer:paper:3.0.0-SNAPSHOT:dev")
+	compileOnly("net.civmc.civmodcore:civmodcore-paper:2.3.5:dev-all")
+	compileOnly("net.civmc.namelayer:namelayer-paper:3.0.3:dev")
 
     implementation("com.github.seancfoley:ipaddress:2.0.2")
     implementation("org.jsoup:jsoup:1.13.1")
