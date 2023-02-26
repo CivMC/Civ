fecef8c8e45f905ea7730473fa9a806c0b882893
AngrySoundTech <angrysoundtech@gmail.com>
2023-02-26 10:53:07 -0500

    Update civgradle

--- plugins/randomspawn-paper/paper/build.gradle.kts
+++ plugins/randomspawn-paper/paper/build.gradle.kts
@@ -4,8 +4,7 @@ plugins {
 
 dependencies {
     paperDevBundle("1.18.2-R0.1-SNAPSHOT")
-    compileOnly("net.civmc.civmodcore:paper:2.0.0-SNAPSHOT:dev-all")
-    compileOnly("net.civmc:worldborder:2.0.0-SNAPSHOT:dev")
-    compileOnly("net.civmc:bastion:3.0.0-SNAPSHOT:dev")
-    compileOnly("net.civmc:banstick:2.0.0-SNAPSHOT:dev")
+    compileOnly("net.civmc.civmodcore:civmodcore-paper:2.3.5:dev-all")
+    compileOnly("net.civmc.bastion:bastion-paper:3.0.1:dev")
+    compileOnly("net.civmc.banstick:banstick-paper:2.0.1:dev")
 }
