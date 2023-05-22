4dc93798fec9434cbf2b210c0b3277ece2f49fd1
AngrySoundTech <angrysoundtech@gmail.com>
2023-05-22 12:34:35 -0400

    remove references to queue

--- ansible/templates/backup-and-restart.sh
+++ ansible/templates/backup-and-restart.sh
@@ -4,7 +4,6 @@ echo "$(date) Stopping services for backup..."
 docker service scale minecraft_waterfall=0
 sleep 1m
 docker service scale minecraft_paper=0
-docker service scale minecraft_queue=0
 sleep 10m
 
 echo "$(date) Starting backup..."
@@ -21,7 +20,6 @@ restic \
 
 echo "$(date) Starting services after backup..."
 docker service scale minecraft_paper=1
-docker service scale minecraft_queue=1
 sleep 5m
 docker service scale minecraft_waterfall=1
 
