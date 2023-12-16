c80378b38fd82e65a82b9f477f174893325f3634
AngrySoundTech <angrysoundtech@gmail.com>
2023-12-16 14:40:32 -0500

    Bump version and update civgradle

--- plugins/factorymod-paper/paper/build.gradle.kts
+++ plugins/factorymod-paper/paper/build.gradle.kts
@@ -3,7 +3,9 @@ plugins {
 }
 
 dependencies {
-	paperDevBundle("1.18.2-R0.1-SNAPSHOT")
+	paperweight {
+		paperDevBundle("1.18.2-R0.1-SNAPSHOT")
+	}
 
     compileOnly("net.civmc.civmodcore:civmodcore-paper:2.3.5:dev-all")
 	compileOnly("net.civmc.namelayer:namelayer-paper:3.0.3:dev")
