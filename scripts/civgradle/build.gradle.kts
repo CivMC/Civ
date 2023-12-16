9767c4913a33c5598ac9eaab0ea1c86ea92dd4fa
AngrySoundTech <angrysoundtech@gmail.com>
2023-12-16 12:23:04 -0500

    Update plugins and bump version to 3.1.0

--- scripts/civgradle/build.gradle.kts
+++ scripts/civgradle/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 group = "net.civmc"
-version = "3.0.1"
+version = "3.1.0"
 
 kotlin {
     jvmToolchain {
-        (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(8))
+        this.languageVersion.set(JavaLanguageVersion.of(8))
     }
 }
 
@@ -30,10 +30,10 @@ dependencies {
     }
 
     // Keep these plugin versions on the classpath, so we can update them all at once if needed.
-    implementation("com.gradle.enterprise:com.gradle.enterprise.gradle.plugin:3.15.1")
-    implementation("io.papermc.paperweight.userdev:io.papermc.paperweight.userdev.gradle.plugin:1.5.5") // https://github.com/PaperMC/paperweight/tags
-    implementation("xyz.jpenilla.run-paper:xyz.jpenilla.run-paper.gradle.plugin:2.1.0") // https://github.com/jpenilla/run-task/releases
-    implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
+    implementation("com.gradle.enterprise:com.gradle.enterprise.gradle.plugin:3.16.1")
+    implementation("io.papermc.paperweight.userdev:io.papermc.paperweight.userdev.gradle.plugin:1.5.11") // https://github.com/PaperMC/paperweight/tags
+    implementation("xyz.jpenilla.run-paper:xyz.jpenilla.run-paper.gradle.plugin:2.2.2") // https://github.com/jpenilla/run-task/releases
+    implementation("com.github.johnrengelman:shadow:8.1.1")
 }
 
 gradlePlugin {
