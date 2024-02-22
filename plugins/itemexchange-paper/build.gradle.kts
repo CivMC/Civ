8d23e729828d066aa63a36dc6973e81b6cb6ce93
okx-code <okx@okx.sh>
2024-02-22 02:15:20 +0000

    Update ItemExchange to 1.20

--- plugins/itemexchange-paper/build.gradle.kts
+++ plugins/itemexchange-paper/build.gradle.kts
@@ -5,7 +5,9 @@ plugins {
 version = "2.0.1"
 
 dependencies {
-	paperDevBundle("1.18.2-R0.1-SNAPSHOT")
+	paperweight {
+		paperDevBundle("1.20.4-R0.1-SNAPSHOT")
+	}
 
 	compileOnly(project(":plugins:civmodcore-paper"))
 	compileOnly(project(":plugins:namelayer-paper"))
