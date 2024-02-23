68dcf17f63815a3b57c5df52f94ed62760ec2070
okx-code <okx@okx.sh>
2024-02-23 22:26:13 +0000

    fix indentation

--- plugins/civduties-paper/build.gradle.kts
+++ plugins/civduties-paper/build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
-	id("io.papermc.paperweight.userdev")
+    id("io.papermc.paperweight.userdev")
 }
 
 version = "1.5.0-SNAPSHOT"
 
 dependencies {
-	paperDevBundle("1.18.2-R0.1-SNAPSHOT")
+    paperDevBundle("1.18.2-R0.1-SNAPSHOT")
 
-	compileOnly(project(":plugins:civmodcore-paper"))
+    compileOnly(project(":plugins:civmodcore-paper"))
     compileOnly("com.github.MilkBowl:VaultAPI:1.7")
-	compileOnly(project(":plugins:combattagplus-paper"))
+    compileOnly(project(":plugins:combattagplus-paper"))
 }
