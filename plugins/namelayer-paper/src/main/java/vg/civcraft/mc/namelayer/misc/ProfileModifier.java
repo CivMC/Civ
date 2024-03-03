1405594fa00fb31e1666855d3fecffd9fc0c4fe0
MrJeremyFisher <63616270+MrJeremyFisher@users.noreply.github.com>
2024-03-02 21:42:36 -0500

    Fix skin not carrying over on profile change

--- plugins/namelayer-paper/src/main/java/vg/civcraft/mc/namelayer/misc/ProfileModifier.java
+++ plugins/namelayer-paper/src/main/java/vg/civcraft/mc/namelayer/misc/ProfileModifier.java
@@ -1,9 +1,11 @@
 package vg.civcraft.mc.namelayer.misc;
 
 import com.destroystokyo.paper.profile.CraftPlayerProfile;
-import java.util.logging.Logger;
+import com.destroystokyo.paper.profile.PlayerProfile;
 import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
+
+import java.util.logging.Logger;
 
 public class ProfileModifier implements ProfileInterface {
 
@@ -12,12 +14,15 @@ public class ProfileModifier implements ProfileInterface {
 	@Override
 	public void setPlayerProfile(final Player player, final String name) {
 		final String oldName = player.getName();
+        PlayerProfile oldProfile = player.getPlayerProfile();
+        PlayerProfile newProfile = new CraftPlayerProfile(player.getUniqueId(), name);
 		if (name.length() > 16) {
 			LOGGER.info(String.format("The player %s (%s) was kicked from the server due to their "
 							+ "name already existing but now becoming over 16 characters.",
 					name, player.getUniqueId()));
 		}
-		player.setPlayerProfile(new CraftPlayerProfile(player.getUniqueId(), name));
+        newProfile.setProperties(oldProfile.getProperties());
+        player.setPlayerProfile(newProfile);
 		player.displayName(Component.text(name));
 		player.playerListName(Component.text(name));
 		player.customName(Component.text(name));
