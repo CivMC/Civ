2548f0f42ad88bc3624decbde0d4ca7333277118
okx-code <okx@okx.sh>
2024-03-16 02:27:18 +0000

    Add link to Mojang issue

--- plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/hacks/basic/NoBoatsInMinecarts.java
+++ plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/hacks/basic/NoBoatsInMinecarts.java
@@ -10,6 +10,7 @@ import org.bukkit.event.vehicle.VehicleEntityCollisionEvent;
 
 /**
  * Prevents boats from entering minecarts
+ * Fix for <a href="https://bugs.mojang.com/browse/MC-113871">MC-113871</a>
  */
 public class NoBoatsInMinecarts extends BasicHack {
 
