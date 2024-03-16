71a197c9801e6e747f78c197d4a8dc6293ea0618
okx-code <okx@okx.sh>
2024-03-16 01:52:55 +0000

    Prevent boats from entering minecarts

---
+++ plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/hacks/basic/NoBoatsInMinecarts.java
@@ -0,0 +1,27 @@
+package com.programmerdan.minecraft.simpleadminhacks.hacks.basic;
+
+import com.programmerdan.minecraft.simpleadminhacks.SimpleAdminHacks;
+import com.programmerdan.minecraft.simpleadminhacks.framework.BasicHack;
+import com.programmerdan.minecraft.simpleadminhacks.framework.BasicHackConfig;
+import org.bukkit.entity.Boat;
+import org.bukkit.entity.Minecart;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.vehicle.VehicleEntityCollisionEvent;
+
+/**
+ * Prevents boats from entering minecarts
+ */
+public class NoBoatsInMinecarts extends BasicHack {
+
+	public NoBoatsInMinecarts(SimpleAdminHacks plugin, BasicHackConfig config) {
+		super(plugin, config);
+	}
+
+	@EventHandler
+	public void on(VehicleEntityCollisionEvent e) {
+		if (e.getVehicle() instanceof Minecart m && e.getEntity() instanceof Boat b
+				&& !b.isInsideVehicle() && m.getPassengers().isEmpty()) {
+			e.setCancelled(true);
+		}
+	}
+}
