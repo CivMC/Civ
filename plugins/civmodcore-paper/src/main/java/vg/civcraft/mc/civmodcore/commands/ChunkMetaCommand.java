bd37d9a375e156763cf0cefb3c694d5cd7d97c85
okx-code <okx@okx.sh>
2024-03-25 21:27:08 +0000

    Fix logs

--- plugins/civmodcore-paper/src/main/java/vg/civcraft/mc/civmodcore/commands/ChunkMetaCommand.java
+++ plugins/civmodcore-paper/src/main/java/vg/civcraft/mc/civmodcore/commands/ChunkMetaCommand.java
@@ -7,24 +7,26 @@ import co.aikar.commands.annotation.Description;
 import co.aikar.commands.annotation.Subcommand;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
 import org.bukkit.command.CommandSender;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @CommandAlias("cmc")
 public class ChunkMetaCommand extends BaseCommand {
 
-	private static final Logger CHUNK_META_LOGGER = Logger.getLogger("Chunk meta");
+	private static final Logger CHUNK_META_LOGGER = LogManager.getLogger("Chunk meta");
 
 	@Subcommand("togglechunkmeta")
 	@Description("Toggle showing chunk meta logs")
 	@CommandPermission("cmc.debug")
 	public void chunkmeta(CommandSender sender) {
-		if (CHUNK_META_LOGGER.getLevel().intValue() < Level.INFO.intValue()) {
-			CHUNK_META_LOGGER.setLevel(Level.INFO);
+		if (!CHUNK_META_LOGGER.getLevel().isMoreSpecificThan(Level.INFO)) {
+			Configurator.setLevel("Chunk meta", Level.INFO);
 			sender.sendMessage(Component.text("Chunk meta logs disabled.", NamedTextColor.RED));
 		} else {
-			CHUNK_META_LOGGER.setLevel(Level.FINE);
+			Configurator.setLevel("Chunk meta", Level.DEBUG);
 			sender.sendMessage(Component.text("Chunk meta logs enabled.", NamedTextColor.GREEN));
 		}
 	}
