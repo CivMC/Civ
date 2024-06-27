20361bbc740429198035894a9c9664b8aa316871
Alexander <protonull@protonmail.com>
2024-06-27 03:23:47 +0100

    Cleanup AbstractPersistentDataType

--- plugins/civmodcore-paper/src/main/java/vg/civcraft/mc/civmodcore/pdc/AbstractPersistentDataType.java
+++ plugins/civmodcore-paper/src/main/java/vg/civcraft/mc/civmodcore/pdc/AbstractPersistentDataType.java
@@ -6,48 +6,37 @@ import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 
 public abstract class AbstractPersistentDataType<P, C> implements PersistentDataType<P, C> {
-
     protected final Class<P> primitiveClass;
     protected final Class<C> complexClass;
 
-    public AbstractPersistentDataType(@NotNull final Class<P> primitiveClass,
-                                      @NotNull final Class<C> complexClass) {
+    public AbstractPersistentDataType(
+        final @NotNull Class<P> primitiveClass,
+        final @NotNull Class<C> complexClass
+    ) {
         this.primitiveClass = Objects.requireNonNull(primitiveClass);
         this.complexClass = Objects.requireNonNull(complexClass);
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @NotNull
     @Override
-    public final Class<P> getPrimitiveType() {
+    public final @NotNull Class<P> getPrimitiveType() {
         return this.primitiveClass;
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @NotNull
     @Override
-    public final Class<C> getComplexType() {
+    public final @NotNull Class<C> getComplexType() {
         return this.complexClass;
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @NotNull
     @Override
-    public abstract P toPrimitive(@NotNull C instance,
-                                  @NotNull PersistentDataAdapterContext adapter);
+    public abstract @NotNull P toPrimitive(
+        @NotNull C value,
+        @NotNull PersistentDataAdapterContext adapter
+    );
 
-    /**
-     * {@inheritDoc}
-     */
-    @NotNull
     @Override
-    public abstract C fromPrimitive(@NotNull P raw,
-                                    @NotNull PersistentDataAdapterContext adapter);
+    public abstract @NotNull C fromPrimitive(
+        @NotNull P raw,
+        @NotNull PersistentDataAdapterContext adapter
+    );
 
 }
