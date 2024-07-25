Protonull/CivPlugins

Add horse armour hack
Adds hack that allows the registration of crafting recipes for horse armour, and the custom reimplementation of durability and unbreaking.

plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/configs/HorseArmourConfig.java
@@ -1,0 +1,34 @@
+package com.programmerdan.minecraft.simpleadminhacks.configs;
+
+import com.programmerdan.minecraft.simpleadminhacks.SimpleAdminHacks;
+import com.programmerdan.minecraft.simpleadminhacks.framework.SimpleHackConfig;
+import org.bukkit.configuration.ConfigurationSection;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
+
+public final class HorseArmourConfig extends SimpleHackConfig {
+	@ApiStatus.Internal
+	public HorseArmourConfig(
+		final @NotNull SimpleAdminHacks plugin,
+		final @NotNull ConfigurationSection base
+	) {
+		super(plugin, base);
+	}
+
+	@Override
+	protected void wireup(
+		final @NotNull ConfigurationSection config
+	) { }
+
+	public boolean shouldRegisterCraftingRecipes() {
+		return getBase().getBoolean("registerRecipes", true);
+	}
+
+	public boolean implementCustomDurability() {
+		return getBase().getBoolean("customDurability", true);
+	}
+
+	public boolean implementCustomUnbreaking() {
+		return getBase().getBoolean("customUnbreaking", true);
+	}
+}

CivMC/Civ

plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/configs/HorseArmourConfig.java
@@ -1,0 +1,34 @@
+package com.programmerdan.minecraft.simpleadminhacks.configs;
+
+import com.programmerdan.minecraft.simpleadminhacks.SimpleAdminHacks;
+import com.programmerdan.minecraft.simpleadminhacks.framework.SimpleHackConfig;
+import org.bukkit.configuration.ConfigurationSection;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
+
+public final class HorseArmourConfig extends SimpleHackConfig {
+	@ApiStatus.Internal
+	public HorseArmourConfig(
+		final @NotNull SimpleAdminHacks plugin,
+		final @NotNull ConfigurationSection base
+	) {
+		super(plugin, base);
+	}
+
+	@Override
+	protected void wireup(
+		final @NotNull ConfigurationSection config
+	) { }
+
+	public boolean shouldRegisterCraftingRecipes() {
+		return getBase().getBoolean("registerRecipes", true);
+	}
+
+	public boolean reimplementDurability() {
+		return getBase().getBoolean("customDurability", true);
+	}
+
+	public boolean reimplementUnbreaking() {
+		return getBase().getBoolean("customUnbreaking", true);
+	}
+}