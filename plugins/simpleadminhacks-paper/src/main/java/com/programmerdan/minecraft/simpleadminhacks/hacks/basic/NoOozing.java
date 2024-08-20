e3c59366149ec345ff38fb21f6a8cc38c23f4123
okx-code <okx@okx.sh>
2024-08-21 00:44:48 +0100

    No broken potions

---
+++ plugins/simpleadminhacks-paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/hacks/basic/NoOozing.java
@@ -0,0 +1,25 @@
+package com.programmerdan.minecraft.simpleadminhacks.hacks.basic;
+
+import com.programmerdan.minecraft.simpleadminhacks.SimpleAdminHacks;
+import com.programmerdan.minecraft.simpleadminhacks.framework.BasicHack;
+import com.programmerdan.minecraft.simpleadminhacks.framework.BasicHackConfig;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityPotionEffectEvent;
+import org.bukkit.potion.PotionEffectType;
+
+// Thanks kicky
+
+public final class NoOozing extends BasicHack {
+    public NoOozing(final SimpleAdminHacks plugin, final BasicHackConfig config) {
+        super(plugin, config);
+    }
+
+    @EventHandler
+    public void on(EntityPotionEffectEvent event) {
+        PotionEffectType type = event.getNewEffect().getType();
+        // Mojang added some busted potions that break the balance by letting you get stuff too easily
+        if (type == PotionEffectType.OOZING || type == PotionEffectType.WEAVING || type == PotionEffectType.INFESTED || type == PotionEffectType.WIND_CHARGED) {
+            event.setCancelled(true);
+        }
+    }
+}
