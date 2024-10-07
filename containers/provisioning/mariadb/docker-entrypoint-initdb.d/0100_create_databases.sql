b4d11f72355fc58a937ad05ba4d808e77c0617d9
okx-code <okx@okx.sh>
2024-10-07 23:29:31 +0100

    fix kitpvp

--- containers/provisioning/mariadb/docker-entrypoint-initdb.d/0100_create_databases.sql
+++ containers/provisioning/mariadb/docker-entrypoint-initdb.d/0100_create_databases.sql
@@ -28,6 +28,8 @@ GRANT ALL PRIVILEGES ON `realisticbiomes`.* TO 'mariadb'@'%';
 
 CREATE DATABASE pvp_civduties;
 CREATE DATABASE pvp_civmodcore;
+CREATE DATABASE pvp_kitpvp;
 
 GRANT ALL PRIVILEGES ON `pvp_civduties`.* TO 'mariadb'@'%';
 GRANT ALL PRIVILEGES ON `pvp_civmodcore`.* TO 'mariadb'@'%';
+GRANT ALL PRIVILEGES ON `pvp_kitpvp`.* TO 'mariadb'@'%';
