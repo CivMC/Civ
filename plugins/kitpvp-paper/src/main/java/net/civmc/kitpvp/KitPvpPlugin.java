b2976ced97d2c68b3276760310b341306a77c776
okx-code <okx@okx.sh>
2024-10-08 20:25:34 +0100

    add voidworld

--- plugins/kitpvp-paper/src/main/java/net/civmc/kitpvp/KitPvpPlugin.java
+++ plugins/kitpvp-paper/src/main/java/net/civmc/kitpvp/KitPvpPlugin.java
@@ -3,6 +3,9 @@ package net.civmc.kitpvp;
 import net.civmc.kitpvp.command.ClearCommand;
 import net.civmc.kitpvp.command.KitCommand;
 import net.civmc.kitpvp.sql.SqlKitPvpDao;
+import org.bukkit.generator.ChunkGenerator;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import vg.civcraft.mc.civmodcore.ACivMod;
 import vg.civcraft.mc.civmodcore.dao.DatabaseCredentials;
 import vg.civcraft.mc.civmodcore.dao.ManagedDatasource;
@@ -15,5 +18,12 @@ public class KitPvpPlugin extends ACivMod {
         ManagedDatasource source = ManagedDatasource.construct(this, (DatabaseCredentials) getConfig().get("database"));
         getCommand("kit").setExecutor(new KitCommand(new SqlKitPvpDao(source)));
         getCommand("clear").setExecutor(new ClearCommand());
+    }
+
+    @Override
+    public @Nullable ChunkGenerator getDefaultWorldGenerator(@NotNull String worldName, @Nullable String id) {
+        return new ChunkGenerator() {
+
+        };
     }
 }
