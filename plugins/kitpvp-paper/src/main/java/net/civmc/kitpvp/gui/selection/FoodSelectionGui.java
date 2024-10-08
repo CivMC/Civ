8582d29b0e6aa4a836d4cf47e65c90b858d7e015
okx-code <okx@okx.sh>
2024-10-08 02:11:29 +0100

    add baked potato

--- plugins/kitpvp-paper/src/main/java/net/civmc/kitpvp/gui/selection/FoodSelectionGui.java
+++ plugins/kitpvp-paper/src/main/java/net/civmc/kitpvp/gui/selection/FoodSelectionGui.java
@@ -25,5 +25,6 @@ public class FoodSelectionGui extends ItemSelectionGui {
         inventory.setSlot(toClickable(new ItemStack(Material.PUMPKIN_PIE)), 6);
         inventory.setSlot(toClickable(new ItemStack(Material.BREAD)), 7);
         inventory.setSlot(toClickable(new ItemStack(Material.COOKED_CHICKEN)), 8);
+        inventory.setSlot(toClickable(new ItemStack(Material.BAKED_POTATO)), 8);
     }
 }
