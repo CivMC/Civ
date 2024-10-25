0a18f244d6a2358f30c1a9686f906db30f88c176
okx-code <okx@okx.sh>
2024-10-25 01:52:44 +0100

    fix respawn

--- plugins/kitpvp-paper/src/main/java/net/civmc/kitpvp/arena/RespawnListener.java
+++ plugins/kitpvp-paper/src/main/java/net/civmc/kitpvp/arena/RespawnListener.java
@@ -2,6 +2,7 @@ package net.civmc.kitpvp.arena;
 
 import net.civmc.kitpvp.spawn.SpawnProvider;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
@@ -9,21 +10,22 @@ import org.bukkit.event.player.PlayerRespawnEvent;
 public class RespawnListener implements Listener {
 
     private final ArenaManager manager;
-    private final SpawnProvider provider;
 
-    public RespawnListener(ArenaManager manager, SpawnProvider provider) {
+    public RespawnListener(ArenaManager manager) {
         this.manager = manager;
-        this.provider = provider;
     }
 
 
     @EventHandler
     public void on(PlayerRespawnEvent event) {
-        String worldName = event.getPlayer().getWorld().getName();
+        World world = event.getPlayer().getWorld();
+        String worldName = world.getName();
         if (manager.isArena(worldName)) {
             for (LoadedArena arena : manager.getArenas()) {
                 if (manager.getArenaName(arena.arena().name(), arena.owner()).equals(worldName)) {
-                    event.setRespawnLocation(arena.arena().spawn());
+                    Location spawn = arena.arena().spawn().clone();
+                    spawn.setWorld(world);
+                    event.setRespawnLocation(spawn);
                 }
             }
         }
