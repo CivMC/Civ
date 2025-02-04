7c59ad6304977a01b4f6a5d284c20b7dd9775e27
Alexander <protonull@protonmail.com>
2025-02-04 20:40:01 +0000

    Add ItemUpdater interface
    + refactors ItemMetaConverterHack

---
+++ plugins/civmodcore-paper/src/main/java/vg/civcraft/mc/civmodcore/inventory/items/updater/ItemUpdater.java
@@ -0,0 +1,32 @@
+package vg.civcraft.mc.civmodcore.inventory.items.updater;
+
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import vg.civcraft.mc.civmodcore.inventory.items.ItemUtils;
+
+@FunctionalInterface
+public interface ItemUpdater {
+    /**
+     * Updates the item however the implementer sees fit.
+     *
+     * @param item The item to update, which is guaranteed to be a non-empty item, as determined by a {@link ItemUtils#isEmptyItem(ItemStack)} check.
+     */
+    void updateItem(
+        @NotNull ItemStack item
+    );
+
+    /**
+     * Updates all non-empty items (as determined by {@link ItemUtils#isEmptyItem(ItemStack)}) within a given inventory.
+     */
+    static void updateInventory(
+        final @NotNull ItemUpdater updater,
+        final @NotNull Inventory inventory
+    ) {
+        for (final ItemStack item : inventory) {
+            if (!ItemUtils.isEmptyItem(item)) {
+                updater.updateItem(item);
+            }
+        }
+    }
+}
---
+++ plugins/civmodcore-paper/src/main/java/vg/civcraft/mc/civmodcore/inventory/items/updater/listeners/UpdateContainerItemsOnOpen.java
@@ -0,0 +1,25 @@
+package vg.civcraft.mc.civmodcore.inventory.items.updater.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.inventory.Inventory;
+import org.jetbrains.annotations.NotNull;
+import vg.civcraft.mc.civmodcore.inventory.items.updater.ItemUpdater;
+
+public interface UpdateContainerItemsOnOpen extends ItemUpdater, Listener {
+    @EventHandler(
+        priority = EventPriority.LOWEST,
+        ignoreCancelled = true
+    )
+    default void updateContainerItemsOnOpen(
+        final @NotNull InventoryOpenEvent event
+    ) {
+        final Inventory inventory = event.getInventory();
+        if (inventory.getHolder() == null) {
+            return; // GUI
+        }
+        ItemUpdater.updateInventory(this, inventory);
+    }
+}
