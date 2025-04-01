f89b36bd24ae8795e9eed5cf35887f3a005ee87f
Alexander <protonull@protonmail.com>
2025-04-01 02:50:45 +0100

    Remove JUNIT tests

--- plugins/civmodcore-paper/src/test/java/vg/civcraft/mc/civmodcore/inheritance/ChildClass.java
+++
@@ -1,8 +0,0 @@
-package vg.civcraft.mc.civmodcore.inheritance;
-
-/**
- * Child class to see whether {@link ParentClass#create()} is properly inherited.
- */
-public class ChildClass extends ParentClass {
-
-}
