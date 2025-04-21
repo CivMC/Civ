34735debdff7c86e79378611452df0186097c3cb
okx-code <okx@okx.sh>
2025-04-21 22:18:46 +0100

    Revert "Reapply "revert""
    This reverts commit f51f859b8d7b45c52eee36b6378c8a05d42b4305.

--- plugins/bastion-paper/src/main/java/isaac/bastion/listeners/CitadelListener.java
+++ plugins/bastion-paper/src/main/java/isaac/bastion/listeners/CitadelListener.java
@@ -8,8 +8,12 @@ import isaac.bastion.manager.BastionBlockManager;
 import java.util.Set;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import vg.civcraft.mc.citadel.events.ReinforcementChangeTypeEvent;
 import vg.civcraft.mc.citadel.events.ReinforcementCreationEvent;
+import vg.civcraft.mc.citadel.events.ReinforcementGroupChangeEvent;
+import vg.civcraft.mc.citadel.events.ReinforcementRepairEvent;
 import vg.civcraft.mc.namelayer.permission.PermissionType;
 
 public class CitadelListener implements Listener {
@@ -35,5 +39,26 @@ public class CitadelListener implements Listener {
                 return;
             }
         }
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    public void onReinforcementRepair(ReinforcementRepairEvent event) {
+        BastionBlock bastionBlock = Bastion.getBastionStorage().getBastionBlock(event.getReinforcement().getLocation());
+        if (bastionBlock == null) return;
+        bastionBlock.setPlaced(System.currentTimeMillis());
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    public void onReinforcementRepair(ReinforcementChangeTypeEvent event) {
+        BastionBlock bastionBlock = Bastion.getBastionStorage().getBastionBlock(event.getReinforcement().getLocation());
+        if (bastionBlock == null) return;
+        bastionBlock.setPlaced(System.currentTimeMillis());
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    public void onReinforcementRepair(ReinforcementGroupChangeEvent event) {
+        BastionBlock bastionBlock = Bastion.getBastionStorage().getBastionBlock(event.getReinforcement().getLocation());
+        if (bastionBlock == null) return;
+        bastionBlock.setPlaced(System.currentTimeMillis());
     }
 }
