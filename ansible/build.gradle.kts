41711d62807b600204a73416043a62ab88fe32b1
Husky <39809509+Huskydog9988@users.noreply.github.com>
2025-09-20 13:35:46 -0400

    make kira auth a proxy command

--- ansible/build.gradle.kts
+++ ansible/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
 
     proxyPlugin(project(path = ":plugins:civproxy-velocity", configuration = "shadow"))
     proxyPlugin(project(path = ":plugins:announcements-velocity", configuration = "shadow"))
+    proxyPlugin(project(path = ":plugins:kiragateway-velocity", configuration = "shadow"))
 }
 
 val copyPaperPlugins = tasks.register<Copy>("copyPaperPlugins") {
---
+++ plugins/kiragateway-velocity/src/main/templates/xyz/huskydog/kiragatewayVelocity/BuildConstants.java
@@ -0,0 +1,7 @@
+package xyz.huskydog.kiragatewayVelocity;
+
+// The constants are replaced before compilation
+public class BuildConstants {
+
+    public static final String VERSION = "${version}";
+}
