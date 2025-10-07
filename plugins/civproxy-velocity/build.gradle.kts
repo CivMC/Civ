3e126cdc64e4cd047b860afc805ca3beef5ba3e5
okx-code <okx@okx.sh>
2025-10-07 06:07:53 +0100

    use mariadb

--- plugins/civproxy-velocity/build.gradle.kts
+++ plugins/civproxy-velocity/build.gradle.kts
@@ -8,7 +8,7 @@ dependencies {
     compileOnly("us.ajg0702.queue.api:api:2.8.0")
     api(libs.hikaricp)
     api(libs.configurate.yaml)
-    api("mysql:mysql-connector-java:5.1.13")
+    api("org.mariadb.jdbc:mariadb-java-client:3.5.6")
     compileOnly(libs.luckperms.api)
     annotationProcessor("com.velocitypowered:velocity-api:3.4.0-SNAPSHOT")
 }
