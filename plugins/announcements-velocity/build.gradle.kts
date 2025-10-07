0b08f560cc300b52cffffb842dc1fa21103b1984
okx-code <okx@okx.sh>
2025-10-07 03:06:38 +0100

    update deps

--- plugins/announcements-velocity/build.gradle.kts
+++ plugins/announcements-velocity/build.gradle.kts
@@ -8,6 +8,6 @@ dependencies {
     compileOnly(libs.velocity.api)
     annotationProcessor(libs.velocity.api)
 
-    implementation(libs.cron.utils)
+    api(libs.cron.utils)
     api(libs.configurate.yaml)
 }
