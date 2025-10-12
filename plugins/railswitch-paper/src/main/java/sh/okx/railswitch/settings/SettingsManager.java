1461ded9220265138d733919e9f9bfc8672d887e
Husky <39809509+Huskydog9988@users.noreply.github.com>
2025-10-12 16:22:10 -0400

    add dest to scoreboard

--- plugins/railswitch-paper/src/main/java/sh/okx/railswitch/settings/SettingsManager.java
+++ plugins/railswitch-paper/src/main/java/sh/okx/railswitch/settings/SettingsManager.java
@@ -3,8 +3,15 @@ package sh.okx.railswitch.settings;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import sh.okx.railswitch.RailSwitchPlugin;
+import vg.civcraft.mc.civmodcore.players.scoreboard.bottom.BottomLine;
+import vg.civcraft.mc.civmodcore.players.scoreboard.bottom.BottomLineAPI;
+import vg.civcraft.mc.civmodcore.players.scoreboard.side.CivScoreBoard;
+import vg.civcraft.mc.civmodcore.players.scoreboard.side.ScoreBoardAPI;
+import vg.civcraft.mc.civmodcore.players.settings.impl.DisplayLocationSetting;
 
 /**
  * Manages the initialisation and registration of menu settings.
@@ -16,6 +23,10 @@ public final class SettingsManager {
     private static DestinationSetting destSetting;
 
     private static ResetSetting resetSetting;
+    private static DisplayLocationSetting destDisplayLocation;
+
+    private static CivScoreBoard destScoreBoard;
+    private static BottomLine destBottomLine;
 
     /**
      * Initialise the settings manager. This should only be called within RailSwitch onEnable().
@@ -27,10 +38,15 @@ public final class SettingsManager {
         menu = new RailSwitchMenu();
         destSetting = new DestinationSetting(plugin);
         resetSetting = new ResetSetting(plugin, destSetting);
+        destDisplayLocation = new DisplayLocationSetting(plugin, DisplayLocationSetting.DisplayLocation.SIDEBAR,
+            "Dest Display Location", "destDisplayLocation", new ItemStack(Material.ARROW), "the  status");
         // Register those elements
         menu.registerToParentMenu();
         menu.registerSetting(destSetting);
         menu.registerSetting(resetSetting);
+
+        destScoreBoard = ScoreBoardAPI.createBoard("CivChatAFKDisplay");
+        destBottomLine = BottomLineAPI.createBottomLine("CivChatAFKDisplay", 4);
     }
 
     /**
@@ -66,6 +82,7 @@ public final class SettingsManager {
                 player.sendMessage(ChatColor.RED + "Could not set your destination.");
             }
         }
+        // updateDestScoreboardHud(player);
     }
 
     /**
@@ -82,4 +99,24 @@ public final class SettingsManager {
         return value;
     }
 
+    public static void updateDestScoreboardHud(Player p) {
+        String dest = getDestination(p);
+        if (Strings.isNullOrEmpty(dest)) {
+            // remove if dest is empty
+            if (destDisplayLocation.showOnSidebar(p.getUniqueId())) {
+                destScoreBoard.hide(p);
+            }
+            if (destDisplayLocation.showOnActionbar(p.getUniqueId())) {
+                destBottomLine.removePlayer(p);
+            }
+        } else {
+            // set dest on hud
+            if (destDisplayLocation.showOnSidebar(p.getUniqueId())) {
+                destScoreBoard.set(p, ChatColor.GOLD + "Dest: " + ChatColor.AQUA + dest);
+            }
+            if (destDisplayLocation.showOnActionbar(p.getUniqueId())) {
+                destBottomLine.updatePlayer(p, ChatColor.YELLOW + "Dest: " + ChatColor.AQUA + dest);
+            }
+        }
+    }
 }
