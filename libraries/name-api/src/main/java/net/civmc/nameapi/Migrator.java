65e4bd27679dcf22501b7ea671921ad05af609d9
okx-code <okx@okx.sh>
2026-02-07 02:33:09 +0000

    omfg

--- libraries/name-api/src/main/java/net/civmc/nameapi/Migrator.java
+++ libraries/name-api/src/main/java/net/civmc/nameapi/Migrator.java
@@ -56,7 +56,7 @@ public class Migrator {
             }
 
             if (maxId != minId) {
-                PreparedStatement setMigrationId = connection.prepareStatement("REPLACE INTO migations (namespace, id) VALUES (?, ?)");
+                PreparedStatement setMigrationId = connection.prepareStatement("REPLACE INTO migrations (namespace, id) VALUES (?, ?)");
                 setMigrationId.setString(1, entry.getKey());
                 setMigrationId.setInt(2, maxId);
                 setMigrationId.executeUpdate();
