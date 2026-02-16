e7e767f1a8a8724fa2618cf836365036f378a12e
okx-code <okx@okx.sh>
2026-02-16 14:22:33 +0000

    add place vehicle event for snitches (fixes #786)

---
+++ plugins/jukealert-paper/src/main/java/com/untamedears/jukealert/model/actions/impl/PlaceVehicleAction.java
@@ -0,0 +1,46 @@
+package com.untamedears.jukealert.model.actions.impl;
+
+import com.untamedears.jukealert.model.Snitch;
+import com.untamedears.jukealert.model.actions.abstr.LoggablePlayerVictimAction;
+import com.untamedears.jukealert.util.JAUtility;
+import java.util.UUID;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import vg.civcraft.mc.civmodcore.inventory.gui.DecorationStack;
+import vg.civcraft.mc.civmodcore.inventory.gui.IClickable;
+import vg.civcraft.mc.civmodcore.inventory.items.ItemUtils;
+
+public class PlaceVehicleAction extends LoggablePlayerVictimAction {
+
+    public static final String ID = "PLACE_VEHICLE";
+
+    public PlaceVehicleAction(long time, Snitch snitch, UUID player, Location location, String victim) {
+        super(time, snitch, player, location, victim);
+    }
+
+    @Override
+    public IClickable getGUIRepresentation() {
+        ItemStack is = new ItemStack(getVehicle());
+        super.enrichGUIItem(is);
+        return new DecorationStack(is);
+    }
+
+    /**
+     * @return Material of the vehicle placed
+     */
+    public Material getVehicle() {
+        return JAUtility.getVehicle(victim);
+    }
+
+    @Override
+    public String getIdentifier() {
+        return ID;
+    }
+
+    @Override
+    public String getChatRepresentationIdentifier() {
+        return "Placed " + ItemUtils.getItemName(getVehicle());
+    }
+
+}
