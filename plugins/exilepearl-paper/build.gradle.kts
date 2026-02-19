86c6b344db8ccae8b3604c2f38a711594c246d67
okx-code <okx@okx.sh>
2026-02-19 01:23:06 +0000

    fix dep

--- plugins/exilepearl-paper/build.gradle.kts
+++ plugins/exilepearl-paper/build.gradle.kts
@@ -18,5 +18,5 @@ dependencies {
     compileOnly(project(":plugins:banstick-paper"))
     compileOnly(project(":plugins:randomspawn-paper"))
 
-    compileOnly(files("../../ansible/src/paper-plugins/BreweryX-3.6.2.jar"))
+    compileOnly(files("../../ansible/src/paper-plugins/BreweryX-3.6.3.jar"))
 }
