8198fbd163cca132f7ab654fca6e529c94ccbf5a
okx-code <okx@okx.sh>
2026-02-22 16:41:18 +0000

    create civproxy database + add ajqueue

--- containers/provisioning/mariadb/docker-entrypoint-initdb.d/0100_create_databases.sql
+++ containers/provisioning/mariadb/docker-entrypoint-initdb.d/0100_create_databases.sql
@@ -13,6 +13,7 @@ CREATE DATABASE exilepearl;
 CREATE DATABASE realisticbiomes;
 CREATE DATABASE heliodor;
 CREATE DATABASE nameapi;
+CREATE DATABASE civproxy;
 
 GRANT ALL PRIVILEGES ON `donum`.* TO 'mariadb'@'%';
 GRANT ALL PRIVILEGES ON `banstick`.* TO 'mariadb'@'%';
@@ -29,6 +30,7 @@ GRANT ALL PRIVILEGES ON `exilepearl`.* TO 'mariadb'@'%';
 GRANT ALL PRIVILEGES ON `realisticbiomes`.* TO 'mariadb'@'%';
 GRANT ALL PRIVILEGES ON `heliodor`.* TO 'mariadb'@'%';
 GRANT ALL PRIVILEGES ON `nameapi`.* TO 'mariadb'@'%';
+GRANT ALL PRIVILEGES ON `civproxy`.* TO 'mariadb'@'%';
 
 CREATE DATABASE pvp_civduties;
 CREATE DATABASE pvp_civmodcore;
