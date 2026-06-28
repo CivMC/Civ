0513c869b888f3e1499ce700bd3f4331f83ddfe8
okx-code <okx@okx.sh>
2026-06-28 12:58:01 +0100

    fix decompacting

--- plugins/factorymod-paper/src/main/java/com/github/igotyou/FactoryMod/recipes/DecompactingRecipe.java
+++ plugins/factorymod-paper/src/main/java/com/github/igotyou/FactoryMod/recipes/DecompactingRecipe.java
@@ -171,9 +171,6 @@ public class DecompactingRecipe extends InputRecipe {
 
     private void removeCompactLore(ItemStack is) {
         is.editMeta(meta -> {
-            if (!meta.hasLore()) {
-                return;
-            }
             List<Component> lore = meta.lore();
             lore.remove(Component.empty().append(Component.text(compactedLore)));
             meta.lore(lore);
