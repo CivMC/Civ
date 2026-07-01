8ae9b0e77dd10169d1971b3dd4fd78d34aed2b26
okx-code <okx@okx.sh>
2026-07-01 04:28:19 +0100

    actually do ignore cancelled

--- plugins/zorweth-paper/src/main/java/net/civmc/zorweth/oxygen/ActivityManager.java
+++ plugins/zorweth-paper/src/main/java/net/civmc/zorweth/oxygen/ActivityManager.java
@@ -65,12 +65,12 @@ public class ActivityManager implements Listener {
         }
     }
 
-    @EventHandler(priority = EventPriority.MONITOR)
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void on(BlockBreakEvent event) {
         recordActivity(event.getPlayer(), Activity.MINING);
     }
 
-    @EventHandler(priority = EventPriority.MONITOR)
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void on(BlockPlaceEvent event) {
         recordActivity(event.getPlayer(), Activity.MINING);
     }
